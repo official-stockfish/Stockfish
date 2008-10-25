4f14bd50326d058fe14ae1dd51c57fac99b74153
Marco Costalba <mcostalba@gmail.com>
2008-10-25 15:57:19 +0100

    Try to cleanup movepick
    Hopefully without regressions.
    
    Signed-off-by: Marco Costalba <mcostalba@gmail.com>

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -61,9 +61,8 @@ namespace {
 /// search captures, promotions and some checks) and about how important good
 /// move ordering is at the current node.
 
-MovePicker::MovePicker(Position& p, bool pvnode, Move ttm, Move mk,
-                       Move k1, Move k2, Depth d) {
-  pos = &p;
+MovePicker::MovePicker(const Position& p, bool pvnode, Move ttm, Move mk,
+                       Move k1, Move k2, Depth d) : pos(p) {  
   pvNode = pvnode;
   ttMove = ttm;
   mateKiller = (mk == ttm)? MOVE_NONE : mk;
@@ -112,16 +111,13 @@ Move MovePicker::get_next_move() {
     // Next phase
     phaseIndex++;
     switch (PhaseTable[phaseIndex]) {
+
     case PH_TT_MOVE:
         if (ttMove != MOVE_NONE)
         {
             assert(move_is_ok(ttMove));
-            Move m = generate_move_if_legal(*pos, ttMove, pinned);
-            if (m != MOVE_NONE)
-            {
-                assert(m == ttMove);
-                return m;
-            }
+            if (generate_move_if_legal(pos, ttMove, pinned) != MOVE_NONE)
+                return ttMove;
         }
         break;
 
@@ -129,17 +125,13 @@ Move MovePicker::get_next_move() {
         if (mateKiller != MOVE_NONE)
         {
             assert(move_is_ok(mateKiller));
-            Move m = generate_move_if_legal(*pos, mateKiller, pinned);
-            if (m != MOVE_NONE)
-            {
-                assert(m == mateKiller);
-                return m;
-            }
+            if (generate_move_if_legal(pos, mateKiller, pinned) != MOVE_NONE)
+                return mateKiller;
        }
        break;
 
     case PH_GOOD_CAPTURES:
-        numOfMoves = generate_captures(*pos, moves);
+        numOfMoves = generate_captures(pos, moves);
         score_captures();
         movesPicked = 0;
         break;
@@ -149,26 +141,26 @@ Move MovePicker::get_next_move() {
         break;
 
     case PH_NONCAPTURES:
-        numOfMoves = generate_noncaptures(*pos, moves);
+        numOfMoves = generate_noncaptures(pos, moves);
         score_noncaptures();
         movesPicked = 0;
         break;
 
     case PH_EVASIONS:
-        assert(pos->is_check());      
-        numOfMoves = generate_evasions(*pos, moves);
+        assert(pos.is_check());      
+        numOfMoves = generate_evasions(pos, moves);
         score_evasions();
         movesPicked = 0;
         break;
 
     case PH_QCAPTURES:      
-        numOfMoves = generate_captures(*pos, moves);
+        numOfMoves = generate_captures(pos, moves);
         score_qcaptures();
         movesPicked = 0;
         break;
 
     case PH_QCHECKS:
-        numOfMoves = generate_checks(*pos, moves, dc);
+        numOfMoves = generate_checks(pos, moves, dc);
         movesPicked = 0;
         break;
 
@@ -190,15 +182,13 @@ Move MovePicker::get_next_move() {
 
 Move MovePicker::get_next_move(Lock &lock) {
 
-   Move m;
-
    lock_grab(&lock);
    if (finished)
    {
        lock_release(&lock);
        return MOVE_NONE;
    }
-   m = get_next_move();
+   Move m = get_next_move();
    if (m == MOVE_NONE)
        finished = true;
 
@@ -224,14 +214,14 @@ void MovePicker::score_captures() {
   // the subtree size.
   for (int i = 0; i < numOfMoves; i++)
   {
-      int seeValue = pos->see(moves[i].move);
+      int seeValue = pos.see(moves[i].move);
       if (seeValue >= 0)
       {
           if (move_promotion(moves[i].move))
               moves[i].score = QueenValueMidgame;
           else
-              moves[i].score = int(pos->midgame_value_of_piece_on(move_to(moves[i].move)))
-                              -int(pos->type_of_piece_on(move_from(moves[i].move)));
+              moves[i].score = int(pos.midgame_value_of_piece_on(move_to(moves[i].move)))
+                              -int(pos.type_of_piece_on(move_from(moves[i].move)));
       } else
           moves[i].score = seeValue;
   }
@@ -247,7 +237,7 @@ void MovePicker::score_noncaptures() {
       else if (m == killer2)
           moves[i].score = HistoryMax + 1;
       else
-         moves[i].score = H.move_ordering_score(pos->piece_on(move_from(m)), m);
+         moves[i].score = H.move_ordering_score(pos.piece_on(move_from(m)), m);
   }
 }
 
@@ -258,12 +248,12 @@ void MovePicker::score_evasions() {
       Move m = moves[i].move;
       if (m == ttMove)
           moves[i].score = 2*HistoryMax;
-      else if(!pos->square_is_empty(move_to(m)))
+      else if (!pos.square_is_empty(move_to(m)))
       {
-          int seeScore = pos->see(m);
+          int seeScore = pos.see(m);
           moves[i].score = (seeScore >= 0)? seeScore + HistoryMax : seeScore;
       } else
-          moves[i].score = H.move_ordering_score(pos->piece_on(move_from(m)), m);
+          moves[i].score = H.move_ordering_score(pos.piece_on(move_from(m)), m);
   }
   // FIXME try psqt also here
 }
@@ -277,9 +267,26 @@ void MovePicker::score_qcaptures() {
       if (move_promotion(m))
           moves[i].score = QueenValueMidgame;
       else
-          moves[i].score = int(pos->midgame_value_of_piece_on(move_to(m)))
-                          -int(pos->midgame_value_of_piece_on(move_to(m))) / 64;
+          moves[i].score = int(pos.midgame_value_of_piece_on(move_to(m)))
+                          -int(pos.midgame_value_of_piece_on(move_to(m))) / 64;
   }
+}
+
+
+/// find_best_index() loops across the moves and returns index of
+/// the highest scored one.
+
+int MovePicker::find_best_index() {
+
+  int bestScore = -10000000, bestIndex = -1;
+
+  for (int i = movesPicked; i < numOfMoves; i++)
+      if (moves[i].score > bestScore)
+      {
+          bestIndex = i;
+          bestScore = moves[i].score;
+      }
+  return bestIndex;
 }
 
 
@@ -293,18 +300,17 @@ void MovePicker::score_qcaptures() {
 
 Move MovePicker::pick_move_from_list() {
 
-  int bestScore = -10000000;
   int bestIndex;
   Move move;
 
   switch (PhaseTable[phaseIndex]) {
   case PH_GOOD_CAPTURES:
-      assert(!pos->is_check());
+      assert(!pos.is_check());
       assert(movesPicked >= 0);
 
       while (movesPicked < numOfMoves)
       {
-          bestScore = -10000000;
+          int bestScore = -10000000;
           bestIndex = -1;
           for (int i = movesPicked; i < numOfMoves; i++)
           {
@@ -327,35 +333,23 @@ Move MovePicker::pick_move_from_list() {
               moves[bestIndex] = moves[movesPicked++];
               if (   move != ttMove
                   && move != mateKiller
-                  && pos->move_is_legal(move, pinned))
+                  && pos.move_is_legal(move, pinned))
                   return move;
           }
       }
       break;
 
   case PH_NONCAPTURES:
-      assert(!pos->is_check());
+      assert(!pos.is_check());
       assert(movesPicked >= 0);
 
       while (movesPicked < numOfMoves)
       {
-          bestScore = -10000000;
-
           // If this is a PV node or we have only picked a few moves, scan
           // the entire move list for the best move.  If many moves have already
           // been searched and it is not a PV node, we are probably failing low
           // anyway, so we just pick the first move from the list.
-          if (pvNode || movesPicked < 12)
-          {
-              bestIndex = -1;
-              for (int i = movesPicked; i < numOfMoves; i++)
-                  if (moves[i].score > bestScore)
-                  {
-                      bestIndex = i;
-                      bestScore = moves[i].score;
-                  }
-          } else
-              bestIndex = movesPicked;
+          bestIndex = (pvNode || movesPicked < 12) ? find_best_index() : movesPicked;
 
           if (bestIndex != -1)
           {
@@ -363,26 +357,19 @@ Move MovePicker::pick_move_from_list() {
               moves[bestIndex] = moves[movesPicked++];
               if (   move != ttMove
                   && move != mateKiller
-                  && pos->move_is_legal(move, pinned))
+                  && pos.move_is_legal(move, pinned))
                   return move;
           }
       }
       break;
 
   case PH_EVASIONS:
-      assert(pos->is_check());
+      assert(pos.is_check());
       assert(movesPicked >= 0);
 
       while (movesPicked < numOfMoves)
       {
-          bestScore = -10000000;
-          bestIndex = -1;
-          for (int i = movesPicked; i < numOfMoves; i++)
-              if(moves[i].score > bestScore)
-              {
-                  bestIndex = i;
-                  bestScore = moves[i].score;
-              }
+          bestIndex = find_best_index();
 
           if (bestIndex != -1)
           {
@@ -394,7 +381,7 @@ Move MovePicker::pick_move_from_list() {
     break;
 
   case PH_BAD_CAPTURES:
-      assert(!pos->is_check());
+      assert(!pos.is_check());
       assert(badCapturesPicked >= 0);
       // It's probably a good idea to use SEE move ordering here, instead
       // of just picking the first move.  FIXME
@@ -403,28 +390,17 @@ Move MovePicker::pick_move_from_list() {
           move = badCaptures[badCapturesPicked++].move;
           if (   move != ttMove
               && move != mateKiller
-              && pos->move_is_legal(move, pinned))
+              && pos.move_is_legal(move, pinned))
               return move;
       }
       break;
 
   case PH_QCAPTURES:
-      assert(!pos->is_check());
+      assert(!pos.is_check());
       assert(movesPicked >= 0);
       while (movesPicked < numOfMoves)
       {
-          bestScore = -10000000;
-          if (movesPicked < 4)
-          {
-              bestIndex = -1;
-              for (int i = movesPicked; i < numOfMoves; i++)
-                  if(moves[i].score > bestScore)
-                  {
-                      bestIndex = i;
-                      bestScore = moves[i].score;
-                  }
-          } else
-              bestIndex = movesPicked;
+          bestIndex = (movesPicked < 4 ? find_best_index() : movesPicked);
 
           if (bestIndex != -1)
           {
@@ -432,14 +408,14 @@ Move MovePicker::pick_move_from_list() {
               moves[bestIndex] = moves[movesPicked++];
               // Remember to change the line below if we decide to hash the qsearch!
               // Maybe also postpone the legality check until after futility pruning?
-              if (/* move != ttMove && */ pos->move_is_legal(move, pinned))
+              if (/* move != ttMove && */ pos.move_is_legal(move, pinned))
                   return move;
           }
       }
       break;
 
   case PH_QCHECKS:
-      assert(!pos->is_check());
+      assert(!pos.is_check());
       assert(movesPicked >= 0);
       // Perhaps we should do something better than just picking the first
       // move here?  FIXME
@@ -447,7 +423,7 @@ Move MovePicker::pick_move_from_list() {
       {
           move = moves[movesPicked++].move;
           // Remember to change the line below if we decide to hash the qsearch!
-          if (/* move != ttMove && */ pos->move_is_legal(move, pinned))
+          if (/* move != ttMove && */ pos.move_is_legal(move, pinned))
               return move;
       }
       break;
@@ -458,10 +434,15 @@ Move MovePicker::pick_move_from_list() {
   return MOVE_NONE;
 }
 
+
+/// MovePicker::current_move_type() returns the type of the just
+/// picked next move. It can be used in search to further differentiate
+/// according to the current move type: capture, non capture, escape, etc.
 MovePicker::MovegenPhase MovePicker::current_move_type() const {
 
   return PhaseTable[phaseIndex];
 }
+
 
 /// MovePicker::init_phase_table() initializes the PhaseTable[],
 /// MainSearchPhaseIndex, EvasionPhaseIndex, QsearchWithChecksPhaseIndex
--- src/movepick.h
+++ src/movepick.h
@@ -2,17 +2,17 @@
   Stockfish, a UCI chess playing engine derived from Glaurung 2.1
   Copyright (C) 2004-2008 Tord Romstad (Glaurung author)
   Copyright (C) 2008 Marco Costalba
- 
+
   Stockfish is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-   
+
   Stockfish is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -60,8 +60,7 @@ public:
     PH_STOP
   };
 
-  MovePicker(Position &p, bool pvnode, Move ttm, Move mk, Move k1, Move k2,
-             Depth dpth);
+  MovePicker(const Position& p, bool pvnode, Move ttm, Move mk, Move k1, Move k2, Depth d);
   Move get_next_move();
   Move get_next_move(Lock &lock);
   int number_of_moves() const;
@@ -77,8 +76,9 @@ private:
   void score_evasions();
   void score_qcaptures();
   Move pick_move_from_list();
-  
-  Position *pos;
+  int find_best_index();
+
+  const Position& pos;
   Move ttMove, mateKiller, killer1, killer2;
   Bitboard pinned, dc;
   MoveStack moves[256], badCaptures[64];
