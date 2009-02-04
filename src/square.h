cc769514837494e2690fffa487a86377c5bb6d50
Marco Costalba <mcostalba@gmail.com>
2009-02-04 17:51:23 +0100

    Remove square.cpp
    Move the few stuff in square.h
    
    Signed-off-by: Marco Costalba <mcostalba@gmail.com>

--- src/square.h
+++ src/square.h
@@ -159,21 +159,43 @@ inline int square_distance(Square s1, Square s2) {
   return Max(file_distance(s1, s2), rank_distance(s1, s2));
 }
 
+inline File file_from_char(char c) {
+  return File(c - 'a') + FILE_A;
+}
 
-////
-//// Prototypes
-////
+inline char file_to_char(File f) {
+  return char(f - FILE_A) + 'a';
+}
 
-extern File file_from_char(char c);
-extern char file_to_char(File f);
-extern Rank rank_from_char(char c);
-extern char rank_to_char(Rank r);
-extern Square square_from_string(const std::string &str);
-extern const std::string square_to_string(Square s);
+inline Rank rank_from_char(char c) {
+  return Rank(c - '1') + RANK_1;
+}
 
-extern bool file_is_ok(File f);
-extern bool rank_is_ok(Rank r);
-extern bool square_is_ok(Square s);
+inline char rank_to_char(Rank r) {
+  return char(r - RANK_1) + '1';
+}
 
+inline Square square_from_string(const std::string& str) {
+  return make_square(file_from_char(str[0]), rank_from_char(str[1]));
+}
+
+inline const std::string square_to_string(Square s) {
+  std::string str;
+  str += file_to_char(square_file(s));
+  str += rank_to_char(square_rank(s));
+  return str;
+}
+
+inline bool file_is_ok(File f) {
+  return f >= FILE_A && f <= FILE_H;
+}
+
+inline bool rank_is_ok(Rank r) {
+  return r >= RANK_1 && r <= RANK_8;
+}
+
+inline bool square_is_ok(Square s) {
+  return file_is_ok(square_file(s)) && rank_is_ok(square_rank(s));
+}
 
 #endif // !defined(SQUARE_H_INCLUDED)
