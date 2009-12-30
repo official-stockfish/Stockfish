0f39e5c4ff0805b6aeed74aed75cc58eed1bf240
Marco Costalba <mcostalba@gmail.com>
2009-12-30 11:09:27 +0100

    Fix a little warning under gcc compiler
    Signed-off-by: Marco Costalba <mcostalba@gmail.com>

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -297,8 +297,8 @@ void init_bitboards() {
 
 #if defined(IS_64BIT) && !defined(USE_BSFQ)
 
-CACHE_LINE_ALIGNMENT
-static const int BitTable[64] = {
+static CACHE_LINE_ALIGNMENT
+const int BitTable[64] = {
   0, 1, 2, 7, 3, 13, 8, 19, 4, 25, 14, 28, 9, 34, 20, 40, 5, 17, 26, 38, 15,
   46, 29, 48, 10, 31, 35, 54, 21, 50, 41, 57, 63, 6, 12, 18, 24, 27, 33, 39,
   16, 37, 45, 47, 30, 53, 49, 56, 62, 11, 23, 32, 36, 44, 52, 55, 61, 22, 43,
