e81108a85519dedcb6d0ed8574f35a0b1c90ae97
Marco Costalba <mcostalba@gmail.com>
2010-04-10 20:42:31 +0100

    Restore development version
    Signed-off-by: Marco Costalba <mcostalba@gmail.com>

--- src/misc.cpp
+++ src/misc.cpp
@@ -54,7 +54,7 @@ using namespace std;
 /// Version number. If this is left empty, the current date (in the format
 /// YYMMDD) is used as a version number.
 
-static const string EngineVersion = "1.7.1";
+static const string EngineVersion = "";
 static const string AppName = "Stockfish";
 static const string AppTag  = "";
 
