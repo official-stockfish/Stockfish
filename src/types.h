85559cc5972f93a2e7618d29b4b2cdcb79c55f98
Joona Kiiski <joona.kiiski@gmail.com>
2010-05-21 22:17:59 +0300

    Add some automatic detection for Windows
    Signed-off-by: Marco Costalba <mcostalba@gmail.com>

--- src/types.h
+++ src/types.h
@@ -52,26 +52,31 @@ typedef uint64_t Bitboard;
 //// Configuration
 ////
 
-//// For Linux configuration is done using Makefile. To get started type "make help".
+//// For Linux and OSX configuration is done automatically using Makefile.
+//// To get started type "make help".
 ////
-//// For windows you need to set the right compiler switches manually:
+//// For windows part of the configuration is detected automatically, but
+//// some switches need to be set manually:
 ////
 //// -DNDEBUG       | Disable debugging mode. Use always.
 ////
-//// -DIS_64BIT     | Compile in 64-bit mode. Use on 64-bit systems.
-////
-//// -DBIGENDIAN    | Should not be used on Windows
-////
 //// -DNO_PREFETCH  | Disable use of prefetch asm-instruction. A must if you want the
 ////                | executable to run on some very old machines.
-////
-//// -DUSE_BSFQ     | Use bsfq asm-instruction. Works only in 64-bit mode.
-////                | Works with ICC and GCC, not with MSVC. Gives a small speed up.
 ////
 //// -DUSE_POPCNT   | Add runtime support for use of popcnt asm-instruction.
 ////                | Works only in 64-bit mode. For compiling requires hardware
 ////                | with popcnt support. Around 4% speed-up.
 
+// Automatic detection for 64-bit under Windows
+#if defined(_WIN64)
+#define IS_64BIT
+#endif
+
+// Automatic detection for use of bsfq asm-instruction under Windows.
+// Works only in 64-bit mode. Does not work with MSVC.
+#if defined(_WIN64) && defined(__INTEL_COMPILER)
+#define USE_BSFQ
+#endif
 
 // Cache line alignment specification
 #if defined(_MSC_VER) || defined(__INTEL_COMPILER)
