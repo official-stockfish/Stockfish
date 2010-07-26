f40e481fd63d056cbdad78c2c3fcdad0ff3556d9
Joona Kiiski <joona.kiiski@gmail.com>
2010-07-26 16:39:10 +0300

    Tweak default values for ucioptions
    I created three different systems, tested them all separately and
    attached one did best:
    
    1/40: Orig - Mod: 841 - 850 (+2 elo)
    1+1 : Orig - Mod: 474 - 498 (+9 elo)
    1+0 : Orig - Mod: 455 - 495 (+15 elo)
    
    Because such testing system is not statistically reliable, I made a
    confirmation test:
    
    1/40: Orig - Mod: 502 - 543 (+14 elo)
    1+1: Orig - Mod: 447 - 489 (+16 elo)
    1+0: Orig - Mod: 641 - 656 (+4 elo)
    
    All tests show positive score :-)
    
    Signed-off-by: Marco Costalba <mcostalba@gmail.com>

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -128,9 +128,9 @@ namespace {
     o["OwnBook"] = Option(true);
     o["MultiPV"] = Option(1, 1, 500);
     o["Emergency Move Horizon"] = Option(40, 0, 50);
-    o["Emergency Base Time"] = Option(300, 0, 60000);
-    o["Emergency Move Time"] = Option(100, 0, 5000);
-    o["Minimum Thinking Time"] = Option(30, 0, 5000);
+    o["Emergency Base Time"] = Option(200, 0, 60000);
+    o["Emergency Move Time"] = Option(70, 0, 5000);
+    o["Minimum Thinking Time"] = Option(20, 0, 5000);
     o["UCI_Chess960"] = Option(false);
     o["UCI_AnalyseMode"] = Option(false);
 
