ea2b8a93eb3cf2765e51d33ecf37c3994eacb84d
Marco Costalba <mcostalba@gmail.com>
2010-08-18 14:55:12 +0100

    Retire some unused Depth operator() functions
    No functional change.
    
    Signed-off-by: Marco Costalba <mcostalba@gmail.com>

--- src/depth.h
+++ src/depth.h
@@ -36,19 +36,14 @@ enum Depth {
 //// Inline functions
 ////
 
-inline Depth operator+ (Depth d, int i) { return Depth(int(d) + i); }
-inline Depth operator+ (Depth d1, Depth d2) { return Depth(int(d1) + int(d2)); }
-inline void operator+= (Depth &d, int i) { d = Depth(int(d) + i); }
-inline void operator+= (Depth &d1, Depth d2) { d1 += int(d2); }
-inline Depth operator- (Depth d, int i) { return Depth(int(d) - i); }
-inline Depth operator- (Depth d1, Depth d2) { return Depth(int(d1) - int(d2)); }
-inline void operator-= (Depth &d, int i) { d = Depth(int(d) - i); }
-inline void operator-= (Depth &d1, Depth d2) { d1 -= int(d2); }
-inline Depth operator* (Depth d, int i) { return Depth(int(d) * i); }
-inline Depth operator* (int i, Depth d) { return Depth(int(d) * i); }
+inline void operator+= (Depth &d1, Depth d2) { d1 = Depth(int(d1) + int(d2)); }
 inline void operator*= (Depth &d, int i) { d = Depth(int(d) * i); }
-inline Depth operator/ (Depth d, int i) { return Depth(int(d) / i); }
 inline void operator/= (Depth &d, int i) { d = Depth(int(d) / i); }
+
+inline Depth operator+ (Depth d1, Depth d2) { return Depth(int(d1) + int(d2)); }
+inline Depth operator- (Depth d1, Depth d2) { return Depth(int(d1) - int(d2)); }
+inline Depth operator* (int i, Depth d) { return Depth(int(d) * i); }
+inline Depth operator/ (Depth d, int i) { return Depth(int(d) / i); }
 
 
 #endif // !defined(DEPTH_H_INCLUDED)
