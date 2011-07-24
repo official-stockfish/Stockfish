5b0c6b9bc0687c2f3618fb588b5dc3617f6e2a29
Marco Costalba <mcostalba@gmail.com>
2011-07-24 07:22:37 +0100

    Unhide the istringstream behind UCIParser
    It is misnamed because it is not a parser, perhaps a
    tokenizer, anyhow better call it for what it is, an
    input string stream.
    
    No functional change.
    
    Signed-off-by: Marco Costalba <mcostalba@gmail.com>

--- src/uci.cpp
+++ src/uci.cpp
@@ -42,36 +42,31 @@ namespace {
   // where, due to 50 moves rule, we need to check at most 100 plies back.
   StateInfo StateRingBuf[102], *SetupState = StateRingBuf;
 
-  // UCIParser is a class for parsing UCI input. The class
-  // is actually a string stream built on a given input string.
-  typedef istringstream UCIParser;
-
-  void set_option(UCIParser& up);
-  void set_position(Position& pos, UCIParser& up);
-  bool go(Position& pos, UCIParser& up);
-  void perft(Position& pos, UCIParser& up);
+  void set_option(istringstream& up);
+  void set_position(Position& pos, istringstream& up);
+  bool go(Position& pos, istringstream& up);
+  void perft(Position& pos, istringstream& up);
 }
 
 
-/// execute_uci_command() takes a string as input, uses a UCIParser
-/// object to parse this text string as a UCI command, and calls
-/// the appropriate functions. In addition to the UCI commands,
-/// the function also supports a few debug commands.
+/// execute_uci_command() takes a string as input, parses this text string as
+/// an UCI command, and calls the appropriate functions. In addition to the
+/// UCI commands, the function also supports a few debug commands.
 
 bool execute_uci_command(const string& cmd) {
 
   static Position pos(StartPositionFEN, false, 0); // The root position
 
-  UCIParser up(cmd);
+  istringstream is(cmd);
   string token;
 
-  up >> skipws >> token;
+  is >> skipws >> token;
 
   if (token == "quit")
       return false;
 
   if (token == "go")
-      return go(pos, up);
+      return go(pos, is);
 
   if (token == "ucinewgame")
       pos.from_fen(StartPositionFEN, false);
@@ -80,13 +75,13 @@ bool execute_uci_command(const string& cmd) {
       cout << "readyok" << endl;
 
   else if (token == "position")
-      set_position(pos, up);
+      set_position(pos, is);
 
   else if (token == "setoption")
-      set_option(up);
+      set_option(is);
 
   else if (token == "perft")
-      perft(pos, up);
+      perft(pos, is);
 
   else if (token == "d")
       pos.print();
@@ -124,29 +119,28 @@ namespace {
   // fen string ("fen") or the starting position ("startpos") and then
   // makes the moves given in the following move list ("moves").
 
-  void set_position(Position& pos, UCIParser& up) {
+  void set_position(Position& pos, istringstream& is) {
 
     Move m;
     string token, fen;
 
-    up >> token;
+    is >> token;
 
     if (token == "startpos")
     {
-        pos.from_fen(StartPositionFEN, false);
-        up >> token; // Consume "moves" token if any
+        fen = StartPositionFEN;
+        is >> token; // Consume "moves" token if any
     }
     else if (token == "fen")
-    {
-        while (up >> token && token != "moves")
+        while (is >> token && token != "moves")
             fen += token + " ";
+    else
+        return;
 
-        pos.from_fen(fen, Options["UCI_Chess960"].value<bool>());
-    }
-    else return;
+    pos.from_fen(fen, Options["UCI_Chess960"].value<bool>());
 
     // Parse move list (if any)
-    while (up >> token && (m = move_from_uci(pos, token)) != MOVE_NONE)
+    while (is >> token && (m = move_from_uci(pos, token)) != MOVE_NONE)
     {
         pos.do_move(m, *SetupState);
 
@@ -161,18 +155,18 @@ namespace {
   // command. The function updates the corresponding UCI option ("name")
   // to the given value ("value").
 
-  void set_option(UCIParser& up) {
+  void set_option(istringstream& is) {
 
     string token, name, value;
 
-    up >> token; // Consume "name" token
+    is >> token; // Consume "name" token
 
     // Read option name (can contain spaces)
-    while (up >> token && token != "value")
+    while (is >> token && token != "value")
         name += string(" ", !name.empty()) + token;
 
     // Read option value (can contain spaces)
-    while (up >> token)
+    while (is >> token)
         value += string(" ", !value.empty()) + token;
 
     if (Options.find(name) != Options.end())
@@ -187,37 +181,37 @@ namespace {
   // string, and then calls think(). Returns false if a quit command
   // is received while thinking, true otherwise.
 
-  bool go(Position& pos, UCIParser& up) {
+  bool go(Position& pos, istringstream& is) {
 
     string token;
     SearchLimits limits;
     std::vector<Move> searchMoves;
     int time[] = { 0, 0 }, inc[] = { 0, 0 };
 
-    while (up >> token)
+    while (is >> token)
     {
         if (token == "infinite")
             limits.infinite = true;
         else if (token == "ponder")
             limits.ponder = true;
         else if (token == "wtime")
-            up >> time[WHITE];
+            is >> time[WHITE];
         else if (token == "btime")
-            up >> time[BLACK];
+            is >> time[BLACK];
         else if (token == "winc")
-            up >> inc[WHITE];
+            is >> inc[WHITE];
         else if (token == "binc")
-            up >> inc[BLACK];
+            is >> inc[BLACK];
         else if (token == "movestogo")
-            up >> limits.movesToGo;
+            is >> limits.movesToGo;
         else if (token == "depth")
-            up >> limits.maxDepth;
+            is >> limits.maxDepth;
         else if (token == "nodes")
-            up >> limits.maxNodes;
+            is >> limits.maxNodes;
         else if (token == "movetime")
-            up >> limits.maxTime;
+            is >> limits.maxTime;
         else if (token == "searchmoves")
-            while (up >> token)
+            while (is >> token)
                 searchMoves.push_back(move_from_uci(pos, token));
     }
 
@@ -233,12 +227,12 @@ namespace {
   // The function calls perft() passing the required search depth
   // then prints counted leaf nodes and elapsed time.
 
-  void perft(Position& pos, UCIParser& up) {
+  void perft(Position& pos, istringstream& is) {
 
     int depth, time;
     int64_t n;
 
-    if (!(up >> depth))
+    if (!(is >> depth))
         return;
 
     time = get_system_time();
