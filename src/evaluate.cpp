36cc214703e4c8fe91917b603f7eb476b403f0e4
Marco Costalba <mcostalba@gmail.com>
2011-10-20 07:06:05 +0100

    Increase Mobility
    First tuning with CLOP against a pool of 3 engines. Result
    verified with a direct match:
    
    After 8736 games at 10"+0.1
    Mod vs Orig 1470 - 1496 - 5770 ELO -1 (+-4.3)
    
    So no change in self match but if CLOP is right it should
    be a bit better against an engine pool.
    
    Signed-off-by: Marco Costalba <mcostalba@gmail.com>

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -88,7 +88,7 @@ namespace {
   //
   // Values modified by Joona Kiiski
   const Score WeightsInternal[] = {
-      S(248, 271), S(252, 259), S(46, 0), S(247, 0), S(259, 0)
+      S(252, 344), S(252, 259), S(46, 0), S(247, 0), S(259, 0)
   };
 
   // MobilityBonus[PieceType][attacked] contains mobility bonuses for middle and
