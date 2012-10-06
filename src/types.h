d777c4d78983ee631a90eb5b98a6608f728bc552
Marco Costalba <mcostalba@gmail.com>
2012-10-06 12:25:13 +0200

    Fix mingw compile with ARCH=x86-64
    When using the Makefile (as for the mingw case),
    IS_64BIT and USE_BSFQ are already set with
    ARCH=x86-64 and do not need to be redefined.
    
    No functional change.
    
    Signed-off-by: Marco Costalba <mcostalba@gmail.com>

--- src/types.h
+++ src/types.h
@@ -41,7 +41,7 @@
 
 #include "platform.h"
 
-#if defined(_WIN64)
+#if defined(_WIN64) && !defined(IS_64BIT)
 #  include <intrin.h> // MSVC popcnt and bsfq instrinsics
 #  define IS_64BIT
 #  define USE_BSFQ
