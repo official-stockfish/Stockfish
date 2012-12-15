3ddf91d9d1e397c93c8d0f261e8c35b6c90b7528
Marco Costalba <mcostalba@gmail.com>
2012-12-15 11:20:04 +0100

    Remove an extra semicolon
    No functional change.

--- src/thread.cpp
+++ src/thread.cpp
@@ -143,7 +143,7 @@ void Thread::wait_for_stop_or_ponderhit() {
   Signals.stopOnPonderhit = true;
 
   mutex.lock();
-  while (!Signals.stop) sleepCondition.wait(mutex);;
+  while (!Signals.stop) sleepCondition.wait(mutex);
   mutex.unlock();
 }
 
