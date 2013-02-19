c5ec94d0f1b128fc2c691c7231663a345409d5cc
Marco Costalba <mcostalba@gmail.com>
2013-02-19 07:54:14 +0100

    Update copyright year
    No functional change.

--- src/position.h
+++ src/position.h
@@ -1,7 +1,7 @@
 /*
   Stockfish, a UCI chess playing engine derived from Glaurung 2.1
   Copyright (C) 2004-2008 Tord Romstad (Glaurung author)
-  Copyright (C) 2008-2012 Marco Costalba, Joona Kiiski, Tord Romstad
+  Copyright (C) 2008-2013 Marco Costalba, Joona Kiiski, Tord Romstad
 
   Stockfish is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
