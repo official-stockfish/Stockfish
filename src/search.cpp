36c82b751ce227c05bfb0dc74c311a469f7f8ec4
Joona Kiiski <joona.kiiski@gmail.com>
2013-03-31 18:35:45 +0100

    Double Impact of Gain tables
    Very unorthodox idea.
    
    After 16000 games at 60"+0.05
    ELO: 3.14 +-3.4 (95%) LOS: 96.6%
    Total: 13407 W: 2278 L: 2157 D: 8972
    
    bench: 4705335

--- src/search.cpp
+++ src/search.cpp
@@ -875,7 +875,7 @@ split_point_start: // At split points actual search starts from here
           // but fixing this made program slightly weaker.
           Depth predictedDepth = newDepth - reduction<PvNode>(depth, moveCount);
           futilityValue =  ss->staticEval + ss->evalMargin + futility_margin(predictedDepth, moveCount)
-                         + Gain[pos.piece_moved(move)][to_sq(move)];
+                         + 2 * Gain[pos.piece_moved(move)][to_sq(move)];
 
           if (futilityValue < beta)
           {
