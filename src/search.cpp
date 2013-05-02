11d30b629814a2de446bb1e92b58276556cef29e
Gary Linscott <glinscott@gmail.com>
2013-05-02 14:37:55 -0400

    Fix rounding issue

--- src/search.cpp
+++ src/search.cpp
@@ -145,7 +145,7 @@ void Search::init() {
 
   // Init futility move count array
   for (d = 0; d < 32; d++)
-      FutilityMoveCounts[d] = int(3.0 + 0.3 * pow(double(d), 1.8));
+      FutilityMoveCounts[d] = int(3.001 + 0.3 * pow(double(d), 1.8));
 }
 
 
