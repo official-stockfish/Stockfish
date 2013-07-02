13ffb08136947bc65f79a29d4cf92740324a7136
Marco Costalba <mcostalba@gmail.com>
2013-07-02 07:25:39 +0200

    Revert "Increase earlier aspiration window size"
    This reverts commit b88bc7b7667fc6ddbfc7412fb2c0bfc13e3bf11c.

--- src/search.cpp
+++ src/search.cpp
@@ -364,8 +364,6 @@ namespace {
                 if (Signals.stop)
                     return;
 
-                delta += delta / 2;
-
                 // In case of failing low/high increase aspiration window and
                 // research, otherwise exit the loop.
                 if (bestValue <= alpha)
@@ -380,6 +378,8 @@ namespace {
 
                 else
                     break;
+
+                delta += delta / 2;
 
                 assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
 
