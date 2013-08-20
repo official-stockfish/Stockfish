f39cf1b00801c1d2688fa2ba1375d211e205b13a
Joona Kiiski <joona.kiiski@gmail.com>
2013-08-20 23:26:16 +0100

    Use null move when depth >= 2 plies
    Passed both short TC:
    LLR: 2.96 (-2.94,2.94)
    Total: 23725 W: 5031 L: 4855 D: 13839
    
    And long TC:
    LLR: 2.96 (-2.94,2.94)
    Total: 15730 W: 2939 L: 2754 D: 10037
    
    bench: 4729333

--- src/search.cpp
+++ src/search.cpp
@@ -663,7 +663,7 @@ namespace {
     // Step 8. Null move search with verification search (is omitted in PV nodes)
     if (   !PvNode
         && !ss->skipNullMove
-        &&  depth > ONE_PLY
+        &&  depth >= 2 * ONE_PLY
         &&  eval >= beta
         &&  abs(beta) < VALUE_MATE_IN_MAX_PLY
         &&  pos.non_pawn_material(pos.side_to_move()))
