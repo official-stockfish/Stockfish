59702aca0d0aa322a3260e31feeae22a1a8f8341
Lucas Braesch <lucas.braesch@gmail.com>
2013-09-07 09:32:01 +0200

    Singular extension at 8 plies also for PV nodes
    Passed both short TC:
    LLR: 2.96 (-2.94,2.94)
    Total: 11451 W: 2455 L: 2282 D: 6714
    
    And long TC
    LLR: 2.96 (-2.94,2.94)
    Total: 15813 W: 2907 L: 2723 D: 10183
    
    bench: 3864419

--- src/search.cpp
+++ src/search.cpp
@@ -780,7 +780,7 @@ moves_loop: // When in check and at SpNode search starts from here
 
     singularExtensionNode =   !RootNode
                            && !SpNode
-                           &&  depth >= (PvNode ? 6 * ONE_PLY : 8 * ONE_PLY)
+                           &&  depth >= 8 * ONE_PLY
                            &&  ttMove != MOVE_NONE
                            && !excludedMove // Recursive singular search is not allowed
                            && (tte->bound() & BOUND_LOWER)
