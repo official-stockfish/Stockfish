77b5ee0117e86736817cf90f64055dce1ddbc55e
Joona Kiiski <joona.kiiski@gmail.com>
2013-09-15 21:54:26 +0200

    Fix time parameters for blitz games
    The ideal setting for super-blitz might be something like:
    
        "Emergency Base Time" = 50
        "Emergency Move Time" = 5
    
    This would give a total emergency time buffer of:
    
        50 + 40 * 5 = 250 ms
    
    This setup replaces the previous half cooked hack
    "Don't blunder under extreme time pressure".
    
    Test results are very good at super blitz, but keep good even
    at 60 secs.
    
    At 5+0.05
    ELO: 24.30 +-2.4 (95%) LOS: 100.0%
    Total: 37802 W: 10060 L: 7420 D: 20322
    
    At 15+0.05
    ELO: 13.41 +-2.9 (95%) LOS: 100.0%
    Total: 22271 W: 4853 L: 3994 D: 13424
    
    At 60+0.05
    ELO: 5.30 +-3.2 (95%) LOS: 99.9%
    Total: 16000 W: 2897 L: 2653 D: 10450
    
    No functional change.

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -110,12 +110,7 @@ void TimeManager::init(const Search::LimitsType& limits, int currentPly, Color u
 
   // Initialize to maximum values but unstablePVExtraTime that is reset
   unstablePVExtraTime = 0;
-  optimumSearchTime = maximumSearchTime = limits.time[us]; // In msec
-
-  // Scale down emergencyBaseTime if we are under very high time pressure to
-  // avoid moving immediately and so blundering.
-  if (maximumSearchTime)
-      emergencyBaseTime /= std::max(emergencyBaseTime * 100 / maximumSearchTime, 1);
+  optimumSearchTime = maximumSearchTime = limits.time[us];
 
   // We calculate optimum time usage for different hypothetic "moves to go"-values and choose the
   // minimum of calculated search time values. Usually the greatest hypMTG gives the minimum values.
