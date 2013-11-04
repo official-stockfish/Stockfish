1064288b38d8b870601e1cd7393358db6bbb3a0e
Chris Caino <chricainogithub@gmail.com>
2013-11-04 12:44:42 +0000

    Zero redundant material imbalance terms
    First coefficient in the SameColor array does an
    equivalent job when folded into the LinearCoefficients
    array.
    
    All of the diagonal terms in the OppositeColor array
    are redundant due to cancellation.
    
    No functional change.

--- src/material.cpp
+++ src/material.cpp
@@ -39,11 +39,11 @@ namespace {
   const Value RedundantRook  = Value(554);
 
   //                                  pair  pawn knight bishop rook queen
-  const int LinearCoefficients[6] = { 1817, -162, -1122, -190,  105,  26 };
+  const int LinearCoefficients[6] = { 1824, -162, -1122, -190,  105,  26 };
 
   const int QuadraticCoefficientsSameColor[][PIECE_TYPE_NB] = {
     // pair pawn knight bishop rook queen
-    {   7                               }, // Bishop pair
+    {   0                               }, // Bishop pair
     {  39,    2                         }, // Pawn
     {  35,  271,  -4                    }, // Knight
     {   7,  105,   4,    7              }, // Bishop
@@ -54,12 +54,12 @@ namespace {
   const int QuadraticCoefficientsOppositeColor[][PIECE_TYPE_NB] = {
     //           THEIR PIECES
     // pair pawn knight bishop rook queen
-    {  41                               }, // Bishop pair
-    {  37,   41                         }, // Pawn
-    {  10,   62,  41                    }, // Knight      OUR PIECES
-    {  57,   64,  39,    41             }, // Bishop
-    {  50,   40,  23,   -22,   41       }, // Rook
-    { 106,  101,   3,   151,  171,   41 }  // Queen
+    {   0                               }, // Bishop pair
+    {  37,    0                         }, // Pawn
+    {  10,   62,   0                    }, // Knight      OUR PIECES
+    {  57,   64,  39,     0             }, // Bishop
+    {  50,   40,  23,   -22,    0       }, // Rook
+    { 106,  101,   3,   151,  171,    0 }  // Queen
   };
 
   // Endgame evaluation and scaling functions accessed direcly and not through
