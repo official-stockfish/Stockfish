c5bb9b9da943c49fbead1507ba7213bd7fb5e415
Marco Costalba <mcostalba@gmail.com>
2013-11-29 10:23:14 +0100

    Stockfish DD
    Stockfish bench signature is: 8596156

--- src/misc.cpp
+++ src/misc.cpp
@@ -28,7 +28,7 @@ using namespace std;
 
 /// Version number. If Version is left empty, then compile date, in the
 /// format DD-MM-YY, is shown in engine_info.
-static const string Version = "";
+static const string Version = "DD";
 
 
 /// engine_info() returns the full name of the current Stockfish version. This
