13b9e1e09811560d0120a8586c7418fb3640d626
Stephane Nicolet <cassio@free.fr>
2014-03-05 08:51:02 +0100

    Fix dbg_mean_of() for negative numbers
    Type should be int64_t instead of uint64_t
    
    No functional change.

--- src/misc.cpp
+++ src/misc.cpp
@@ -61,7 +61,7 @@ const string engine_info(bool to_uci) {
 
 /// Debug functions used mainly to collect run-time statistics
 
-static uint64_t hits[2], means[2];
+static int64_t hits[2], means[2];
 
 void dbg_hit_on(bool b) { ++hits[0]; if (b) ++hits[1]; }
 void dbg_hit_on_c(bool c, bool b) { if (c) dbg_hit_on(b); }
