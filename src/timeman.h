17cb7e7fa3cbc707be1df6793de1928fcb0c6219
Marco Costalba <mcostalba@gmail.com>
2014-04-12 12:00:37 +0200

    Reshuffle in timeman.cpp
    Move template definitions before call site.
    
    No functional change.

--- src/timeman.h
+++ src/timeman.h
@@ -26,7 +26,7 @@
 class TimeManager {
 public:
   void init(const Search::LimitsType& limits, int currentPly, Color us);
-  void pv_instability(double bestMoveChanges);
+  void pv_instability(double bestMoveChanges) { unstablePvFactor = 1 + bestMoveChanges; }
   int available_time() const { return int(optimumSearchTime * unstablePvFactor * 0.71); }
   int maximum_time() const { return maximumSearchTime; }
 
