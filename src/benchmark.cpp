e49eb67119f772ef6fcecb736cf136b851ee72cc
Marco Costalba <mcostalba@gmail.com>
2014-05-24 04:31:36 +0200

    Add perft 'divide' command
    To show perft numbers for each move. Just
    use 'divide' instead of 'perft', for instance:
    
    position startpos moves e2e4 e7e5
    divide 4
    
    Inspired by Ronald de Man.
    
    No functional change.

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -23,6 +23,7 @@
 #include <vector>
 
 #include "misc.h"
+#include "notation.h"
 #include "position.h"
 #include "search.h"
 #include "thread.h"
@@ -136,7 +137,17 @@ void benchmark(const Position& current, istream& is) {
 
       cerr << "\nPosition: " << i + 1 << '/' << fens.size() << endl;
 
-      if (limitType == "perft")
+      if (limitType == "divide")
+          for (MoveList<LEGAL> it(pos); *it; ++it)
+          {
+              StateInfo si;
+              pos.do_move(*it, si);
+              uint64_t cnt = limits.depth > 1 ? Search::perft(pos, (limits.depth - 1) * ONE_PLY) : 1;
+              pos.undo_move(*it);
+              cerr << move_to_uci(*it, pos.is_chess960()) << ": " << cnt << endl;
+              nodes += cnt;
+          }
+      else if (limitType == "perft")
       {
           uint64_t cnt = Search::perft(pos, limits.depth * ONE_PLY);
           cerr << "\nPerft " << limits.depth  << " leaf nodes: " << cnt << endl;
