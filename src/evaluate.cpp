98513798f8a029bf439f867690f33ec10298a60c
hxim <hxim.jo@gmail.com>
2014-07-26 10:52:15 +0200

    fix alginment

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -468,7 +468,7 @@ namespace {
         
         // Compute the king danger score and subtract from evaluation.
         const int mbonus = attackUnits <= 0   ? 0
-                         : attackUnits <= 370  ? attackUnits * attackUnits / 250
+                         : attackUnits <= 370 ? attackUnits * attackUnits / 250
                          : attackUnits <= 614 ? 547 + 3 * (attackUnits - 370)
                                               : 1280;
 
