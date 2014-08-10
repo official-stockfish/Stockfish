d8d76bbc173ae9e70d72c3a1ff4ea7d71150671c
lucasart <lucas.braesch@gmail.com>
2014-08-10 10:32:57 +0800

    Retire Search Log
    Seems quite useless now. Instead Debug Log is more useful.
    
    No functional change.

--- src/notation.h
+++ src/notation.h
@@ -29,7 +29,6 @@ class Position;
 std::string score_to_uci(Value v, Value alpha = -VALUE_INFINITE, Value beta = VALUE_INFINITE);
 Move move_from_uci(const Position& pos, std::string& str);
 const std::string move_to_uci(Move m, bool chess960);
-std::string pretty_pv(const Position& pos, int depth, Value score, int64_t msecs, Move pv[]);
 
 inline char to_char(File f, bool tolower = true) {
   return char(f - FILE_A + (tolower ? 'a' : 'A'));
--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -55,8 +55,6 @@ bool CaseInsensitiveLess::operator() (const string& s1, const string& s2) const 
 void init(OptionsMap& o) {
 
   o["Write Debug Log"]          << Option(false, on_logger);
-  o["Write Search Log"]         << Option(false);
-  o["Search Log Filename"]      << Option("SearchLog.txt");
   o["Contempt Factor"]          << Option(0, -100,  100);
   o["Min Split Depth"]          << Option(0, 0, 12, on_threads);
   o["Threads"]                  << Option(1, 1, MAX_THREADS, on_threads);
