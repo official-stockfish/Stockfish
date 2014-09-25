d6613b758954fac5c8ac16a5352f4a85848a3d6f
Uri Blass <uriblass@gmail.com>
2014-09-26 02:22:39 +0800

    Change history reduction in LMR to be a full ply.
    STC:
    LLR: 2.96 (-2.94,2.94) [-3.00,1.00]
    Total: 9829 W: 2142 L: 1998 D: 5689
    
    LTC:
    LLR: 2.95 (-2.94,2.94) [-3.00,1.00]
    Total: 27162 W: 4802 L: 4692 D: 17668
    
    Bench: 7284120
    
    Resolves #53

--- src/search.cpp
+++ src/search.cpp
@@ -823,11 +823,9 @@ moves_loop: // When in check and at SpNode search starts from here
       {
           ss->reduction = reduction<PvNode>(improving, depth, moveCount);
 
-          if (!PvNode && cutNode)
+          if (   (!PvNode && cutNode)
+              ||  History[pos.piece_on(to_sq(move))][to_sq(move)] < 0)
               ss->reduction += ONE_PLY;
-
-          else if (History[pos.piece_on(to_sq(move))][to_sq(move)] < 0)
-              ss->reduction += ONE_PLY / 2;
 
           if (move == countermoves[0] || move == countermoves[1])
               ss->reduction = std::max(DEPTH_ZERO, ss->reduction - ONE_PLY);
