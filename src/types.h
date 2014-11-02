ab516b27cb96a9f003a1891af60a9dd20e24dc7a
lucasart <lucas.braesch@gmail.com>
2014-11-03 07:31:57 +0800

    MAX_PLY = 8 (155227)

--- src/types.h
+++ src/types.h
@@ -101,7 +101,7 @@ typedef uint64_t Key;
 typedef uint64_t Bitboard;
 
 const int MAX_MOVES = 256;
-const int MAX_PLY   = 128;
+const int MAX_PLY   = 8;
 
 /// A move needs 16 bits to be stored
 ///
