88e81fbe78412214b01424374df4b9765487d18e
lucasart <lucas.braesch@gmail.com>
2014-12-12 21:10:31 +0800

    astyle -A1f syzygy/*

--- src/syzygy/tbcore.h
+++ src/syzygy/tbcore.h
@@ -55,112 +55,122 @@ struct TBHashEntry;
 
 typedef uint64 base_t;
 
-struct PairsData {
-  char *indextable;
-  ushort *sizetable;
-  ubyte *data;
-  ushort *offset;
-  ubyte *symlen;
-  ubyte *sympat;
-  int blocksize;
-  int idxbits;
-  int min_len;
-  base_t base[1]; // C++ complains about base[]...
+struct PairsData
+{
+    char *indextable;
+    ushort *sizetable;
+    ubyte *data;
+    ushort *offset;
+    ubyte *symlen;
+    ubyte *sympat;
+    int blocksize;
+    int idxbits;
+    int min_len;
+    base_t base[1]; // C++ complains about base[]...
 };
 
-struct TBEntry {
-  char *data;
-  uint64 key;
-  uint64 mapping;
-  ubyte ready;
-  ubyte num;
-  ubyte symmetric;
-  ubyte has_pawns;
+struct TBEntry
+{
+    char *data;
+    uint64 key;
+    uint64 mapping;
+    ubyte ready;
+    ubyte num;
+    ubyte symmetric;
+    ubyte has_pawns;
 }
 #ifndef _WIN32
 __attribute__((__may_alias__))
 #endif
 ;
 
-struct TBEntry_piece {
-  char *data;
-  uint64 key;
-  uint64 mapping;
-  ubyte ready;
-  ubyte num;
-  ubyte symmetric;
-  ubyte has_pawns;
-  ubyte enc_type;
-  struct PairsData *precomp[2];
-  int factor[2][TBPIECES];
-  ubyte pieces[2][TBPIECES];
-  ubyte norm[2][TBPIECES];
-};
-
-struct TBEntry_pawn {
-  char *data;
-  uint64 key;
-  uint64 mapping;
-  ubyte ready;
-  ubyte num;
-  ubyte symmetric;
-  ubyte has_pawns;
-  ubyte pawns[2];
-  struct {
+struct TBEntry_piece
+{
+    char *data;
+    uint64 key;
+    uint64 mapping;
+    ubyte ready;
+    ubyte num;
+    ubyte symmetric;
+    ubyte has_pawns;
+    ubyte enc_type;
     struct PairsData *precomp[2];
     int factor[2][TBPIECES];
     ubyte pieces[2][TBPIECES];
     ubyte norm[2][TBPIECES];
-  } file[4];
 };
 
-struct DTZEntry_piece {
-  char *data;
-  uint64 key;
-  uint64 mapping;
-  ubyte ready;
-  ubyte num;
-  ubyte symmetric;
-  ubyte has_pawns;
-  ubyte enc_type;
-  struct PairsData *precomp;
-  int factor[TBPIECES];
-  ubyte pieces[TBPIECES];
-  ubyte norm[TBPIECES];
-  ubyte flags; // accurate, mapped, side
-  ushort map_idx[4];
-  ubyte *map;
+struct TBEntry_pawn
+{
+    char *data;
+    uint64 key;
+    uint64 mapping;
+    ubyte ready;
+    ubyte num;
+    ubyte symmetric;
+    ubyte has_pawns;
+    ubyte pawns[2];
+    struct
+    {
+        struct PairsData *precomp[2];
+        int factor[2][TBPIECES];
+        ubyte pieces[2][TBPIECES];
+        ubyte norm[2][TBPIECES];
+    } file[4];
 };
 
-struct DTZEntry_pawn {
-  char *data;
-  uint64 key;
-  uint64 mapping;
-  ubyte ready;
-  ubyte num;
-  ubyte symmetric;
-  ubyte has_pawns;
-  ubyte pawns[2];
-  struct {
+struct DTZEntry_piece
+{
+    char *data;
+    uint64 key;
+    uint64 mapping;
+    ubyte ready;
+    ubyte num;
+    ubyte symmetric;
+    ubyte has_pawns;
+    ubyte enc_type;
     struct PairsData *precomp;
     int factor[TBPIECES];
     ubyte pieces[TBPIECES];
     ubyte norm[TBPIECES];
-  } file[4];
-  ubyte flags[4];
-  ushort map_idx[4][4];
-  ubyte *map;
+    ubyte flags; // accurate, mapped, side
+    ushort map_idx[4];
+    ubyte *map;
 };
 
-struct TBHashEntry {
-  uint64 key;
-  struct TBEntry *ptr;
+struct DTZEntry_pawn
+{
+    char *data;
+    uint64 key;
+    uint64 mapping;
+    ubyte ready;
+    ubyte num;
+    ubyte symmetric;
+    ubyte has_pawns;
+    ubyte pawns[2];
+    struct
+    {
+        struct PairsData *precomp;
+        int factor[TBPIECES];
+        ubyte pieces[TBPIECES];
+        ubyte norm[TBPIECES];
+    } file[4];
+    ubyte flags[4];
+    ushort map_idx[4][4];
+    ubyte *map;
 };
 
-struct DTZTableEntry {
-  uint64 key1;
-  uint64 key2;
-  struct TBEntry *entry;
+struct TBHashEntry
+{
+    uint64 key;
+    struct TBEntry *ptr;
+};
+
+struct DTZTableEntry
+{
+    uint64 key1;
+    uint64 key2;
+    struct TBEntry *entry;
 };
 
 #endif
