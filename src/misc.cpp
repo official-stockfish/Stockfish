97a034ad3e8b25f0e8e1dd03d31f4689180f7547
Joona Kiiski <joona.kiiski@gmail.com>
2015-01-17 23:19:18 +0000

    Stockfish 6 Release Candidate 1
    Bench: 8080602
    
    No functional change
    
    Resolves #218

--- src/misc.cpp
+++ src/misc.cpp
@@ -31,7 +31,7 @@ namespace {
 
 /// Version number. If Version is left empty, then compile date in the format
 /// DD-MM-YY and show in engine_info.
-const string Version = "";
+const string Version = "6 RC1";
 
 /// Debug counters
 int64_t hits[2], means[2];
