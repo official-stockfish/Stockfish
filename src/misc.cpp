9f0d5241bfcf454d4b21594b0af239bbe5990b84
Joona Kiiski <joona.kiiski@gmail.com>
2015-01-28 20:53:50 +0000

    Restore development version
    No functional change

--- src/misc.cpp
+++ src/misc.cpp
@@ -31,7 +31,7 @@ namespace {
 
 /// Version number. If Version is left empty, then compile date in the format
 /// DD-MM-YY and show in engine_info.
-const string Version = "6";
+const string Version = "";
 
 /// Debug counters
 int64_t hits[2], means[2];
