09b367231ac20e30ca692039fcab57340b8b8e86
hxim <hxim.jo@gmail.com>
2015-03-02 22:46:03 +0100

    fix

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -294,7 +294,6 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
 
   kingSquares[Us] = ksq;
   castlingRights[Us] = pos.can_castle(Us);
-  
   int minKingPawnDistance = 0;
 
   Bitboard pawns = pos.pieces(Us, PAWN);
@@ -307,14 +306,11 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
   Value bonus = shelter_storm<Us>(pos, ksq);
 
   // If we can castle use the bonus after the castling if it is bigger
-  if (castlingRights[Us])
-  {
-      if (pos.can_castle(MakeCastling<Us, KING_SIDE>::right))
-          bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_G1)));
+  if (pos.can_castle(MakeCastling<Us, KING_SIDE>::right))
+     bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_G1)));
 
-      if (pos.can_castle(MakeCastling<Us, QUEEN_SIDE>::right))
-          bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_C1)));
-  }
+  if (pos.can_castle(MakeCastling<Us, QUEEN_SIDE>::right))
+     bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_C1)));
 
   return make_score(bonus, -16 * minKingPawnDistance);
 }
