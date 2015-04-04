fdbfe097736058cd2cb80c71c21735e9d78cb211
Stéphane Nicolet <cassio@free.fr>
2015-04-04 18:07:45 +0200

    Use minimumSplitDepth = 5
    Using minimumSplitDepth = 5 seems to be the best compromise in the
    current SMP implementation

--- src/thread.cpp
+++ src/thread.cpp
@@ -323,7 +323,7 @@ void ThreadPool::read_uci_options() {
 
   // If zero (default) then set best minimum split depth automatically
   if (!minimumSplitDepth)
-      minimumSplitDepth = requested < 8 ? 4 * ONE_PLY : 7 * ONE_PLY;
+      minimumSplitDepth =  5 * ONE_PLY ;
 
   while (size() < requested)
       push_back(new_thread<Thread>());
