078625e33389536addc9f94b7fc197230ef2f0fb
Alain SAVARD <support@multicim.com>
2015-04-18 22:12:56 +0100

    Exclude queen from Rook Contact Check computation
    In ei.attackedBy, Queen does not x-ray through Rook, but the Rook does
    X-ray through the Queen.
    
    So most of the rook contact checks supported by queen are, in fact,
    Queen Contact Checks and they are already scored separately.
    
    Bench: 7762189
    
    Resolves #338

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -438,7 +438,7 @@ namespace {
         {
             // ...and then remove squares not supported by another enemy piece
             b &= (  ei.attackedBy[Them][PAWN]   | ei.attackedBy[Them][KNIGHT]
-                  | ei.attackedBy[Them][BISHOP] | ei.attackedBy[Them][QUEEN]);
+                  | ei.attackedBy[Them][BISHOP]);
 
             if (b)
                 attackUnits += RookContactCheck * popcount<Max15>(b);
