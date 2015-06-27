7e9f776fcafe0121c7449f4339b26d77c14adf50
VoyagerOne <excelgeek@gmail.com>
2015-06-27 09:41:17 +0100

    LMR Tweak: Decrease reduction if cmh>0 && history>0.
    STC:
    LLR: 2.96 (-2.94,2.94) [-1.50,4.50]
    Total: 9627 W: 1879 L: 1748 D: 6000
    
    LTC:
    LLR: 2.97 (-2.94,2.94) [0.00,6.00]
    Total: 21543 W: 3433 L: 3234 D: 14876
    
    Bench: 8646407
    
    Resolves #370 #371

--- src/search.cpp
+++ src/search.cpp
@@ -959,7 +959,10 @@ moves_loop: // When in check and at SpNode search starts from here
                                         [pos.piece_on(to_sq(move))][to_sq(move)] <= VALUE_ZERO))
               ss->reduction += ONE_PLY;
 
-          if (move == countermove)
+          if (    move == countermove
+              || (   History[pos.piece_on(to_sq(move))][to_sq(move)] > VALUE_ZERO
+                  && CounterMovesHistory[pos.piece_on(prevMoveSq)][prevMoveSq]
+                                        [pos.piece_on(to_sq(move))][to_sq(move)] > VALUE_ZERO))
               ss->reduction = std::max(DEPTH_ZERO, ss->reduction - ONE_PLY);
 
           // Decrease reduction for moves that escape a capture
