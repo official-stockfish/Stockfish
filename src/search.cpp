f57ef20240a0187828e7b993e787dcfbe85dd88a
Giorgi Guliashvili <vinmesmiti@gmail.com>
2015-08-26 22:47:36 -0700

    obfuscated code

--- src/search.cpp
+++ src/search.cpp
@@ -341,7 +341,7 @@ namespace {
     Move easyMove = EasyMove.get(pos.key());
     EasyMove.clear();
 
-    std::memset(ss-2, 0, 5 * sizeof(Stack));
+    std::memset(stack, 0, 5 * sizeof(Stack));
 
     depth = DEPTH_ZERO;
     BestMoveChanges = 0;
