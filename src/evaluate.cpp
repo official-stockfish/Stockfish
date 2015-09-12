c575aabfbfb8c03b3f3c171d4e25f8cda255f7ee
Stefan Geschwentner <stgeschwentner@gmail.com>
2015-09-12 12:00:09 +0200

    pawn scaling take 5

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -799,7 +799,7 @@ Value Eval::evaluate(const Position& pos) {
   // Scale down endgame scaling factor with number of pawns
   int p = pos.count<PAWN>(WHITE) + pos.count<PAWN>(BLACK);
   int v_eg = 1 + abs(int(eg_value(score)));
-  sf = ScaleFactor(std::max(sf / 2, sf - 7 * SCALE_FACTOR_NORMAL * (14 - p) / v_eg));
+  sf = ScaleFactor(std::max(sf / 2, sf - 11 * SCALE_FACTOR_NORMAL * (14 - p) / v_eg));
 
   // Interpolate between a middlegame and a (scaled by 'sf') endgame score
   Value v =  mg_value(score) * int(me->game_phase())
