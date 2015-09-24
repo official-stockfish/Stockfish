fc75a35b14c656f507c15392585b61ceeb2dcb56
Stefan Geschwentner <stgeschwentner@gmail.com>
2015-09-24 11:19:44 +0200

    Double bonus. Take 3

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -172,8 +172,8 @@ namespace {
 
   // PassedFile[File] contains a bonus according to the file of a passed pawn.
   const Score PassedFile[] = {
-    S(-15, -15), S(-5, -5), S( 5,  5), S( 15,  15),
-    S( 15,  15), S( 5,  5), S(-5, -5), S(-15, -15)
+    S( 30,  30), S( 10,  10), S(-10, -10), S(-30, -30),
+    S(-30, -30), S(-10, -10), S( 10,  10), S( 30,  30)
   };
 
   const Score ThreatenedByHangingPawn = S(40, 60);
