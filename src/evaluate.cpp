df74f692f86057129cc7438fb67d8707f07702b2
ElbertoOne <lubvdberg@gmail.com>
2015-09-28 10:33:24 +0200

    Simplify outposts
    This outpost simplification removes a number of unnecessary if statements. No functional change, but maybe I want to test it anyway.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -141,12 +141,17 @@ namespace {
       S( 94, 99), S( 96,100), S(99,111), S(99,112) }
   };
 
-  // Outpost[knight/bishop][supported by pawn] contains bonuses for knights and
-  // bishops outposts, bigger if outpost piece is supported by a pawn.
-  const Score Outpost[][2] = {
-    { S(42,11), S(63,17) }, // Knights
-    { S(18, 5), S(27, 8) }  // Bishops
+  // KnightOutpost[supported by pawn] contains bonuses for
+  // knight outposts, bigger if outpost piece is supported by a pawn.
+  const Score KnightOutpost[2] = {
+    { S(42,11), S(63,17) }
   };
+  
+  // BishopOutpost[supported by pawn] contains bonuses for
+  // bishops outposts, bigger if outpost piece is supported by a pawn.
+  const Score BishopOutpost[2] = {
+    { S(18, 5), S(27, 8) }
+  }
 
   // Threat[defended/weak][minor/major attacking][attacked PieceType] contains
   // bonuses according to which piece type attacks which one.
@@ -292,13 +297,13 @@ namespace {
 
         mobility[Us] += MobilityBonus[Pt][mob];
 
-        if (Pt == BISHOP || Pt == KNIGHT)
+        if (Pt == BISHOP)
         {
             // Bonus for outpost square
             if (   relative_rank(Us, s) >= RANK_4
                 && relative_rank(Us, s) <= RANK_6
                 && !(pos.pieces(Them, PAWN) & pawn_attack_span(Us, s)))
-                score += Outpost[Pt == BISHOP][!!(ei.attackedBy[Us][PAWN] & s)];
+                score += BishopOutpost[!!(ei.attackedBy[Us][PAWN] & s)];
 
             // Bonus when behind a pawn
             if (    relative_rank(Us, s) < RANK_5
@@ -306,14 +311,12 @@ namespace {
                 score += MinorBehindPawn;
 
             // Penalty for pawns on same color square of bishop
-            if (Pt == BISHOP)
-                score -= BishopPawns * ei.pi->pawns_on_same_color_squares(Us, s);
+            score -= BishopPawns * ei.pi->pawns_on_same_color_squares(Us, s);
 
             // An important Chess960 pattern: A cornered bishop blocked by a friendly
             // pawn diagonally in front of it is a very serious problem, especially
             // when that pawn is also blocked.
-            if (   Pt == BISHOP
-                && pos.is_chess960()
+            if (   pos.is_chess960()
                 && (s == relative_square(Us, SQ_A1) || s == relative_square(Us, SQ_H1)))
             {
                 Square d = pawn_push(Us) + (file_of(s) == FILE_A ? DELTA_E : DELTA_W);
@@ -323,8 +326,22 @@ namespace {
                                                                               : TrappedBishopA1H1;
             }
         }
+        
+        else if (Pt == KNIGHT)
+        {
+          // Bonus for outpost square
+            if (   relative_rank(Us, s) >= RANK_4
+                && relative_rank(Us, s) <= RANK_6
+                && !(pos.pieces(Them, PAWN) & pawn_attack_span(Us, s)))
+                score += KnightOutpost[!!(ei.attackedBy[Us][PAWN] & s)];
 
-        if (Pt == ROOK)
+            // Bonus when behind a pawn
+            if (    relative_rank(Us, s) < RANK_5
+                && (pos.pieces(PAWN) & (s + pawn_push(Us))))
+                score += MinorBehindPawn;
+        }
+
+        else if (Pt == ROOK)
         {
             // Bonus for aligning with enemy pawns on the same rank/file
             if (relative_rank(Us, s) >= RANK_5)
