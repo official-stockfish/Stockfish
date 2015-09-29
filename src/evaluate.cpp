5fc8b965ac9e66a8c2dd5e46d08a1102b6ee3d70
Jonathan Calovski <jonoface@live.com.au>
2015-09-29 18:02:55 +1000

    Check score bonus

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -184,7 +184,7 @@ namespace {
   const Score Unstoppable        = S( 0, 20);
   const Score Hanging            = S(31, 26);
   const Score PawnAttackThreat   = S(20, 20);
-  const Score Checked		    = S(20, 20);	 
+  const Score Checked		   = S(20, 20);	 
 
   // Penalty for a bishop on a1/h1 (a8/h8 for black) which is trapped by
   // a friendly pawn on b2/g2 (b7/g7 for black). This can obviously only
