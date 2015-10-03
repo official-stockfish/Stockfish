83e19fbed539fc05626d82afefde730bdcb344ab
Stefan Geschwentner <stgeschwentner@gmail.com>
2015-10-03 03:46:53 -0700

    File based passed pawn bonus
    Add file based bonus for passed pawns. Values tuned by SPSA.
    
    STC:
    LLR: 3.33 (-2.94,2.94) [0.00,5.00]
    Total: 36889 W: 6805 L: 6507 D: 23577
    
    LTC:
    LLR: 2.97 (-2.94,2.94) [0.00,5.00]
    Total: 32301 W: 5101 L: 4858 D: 22342
    
    Bench: 8073614
    
    Resolves #436

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -170,6 +170,12 @@ namespace {
     { V(7), V(14), V(37), V(63), V(134), V(189) }
   };
 
+  // PassedFile[File] contains a bonus according to the file of a passed pawn.
+  const Score PassedFile[] = {
+    S( 14,  13), S( 2,  5), S(-3, -4), S(-19, -14),
+    S(-19, -14), S(-3, -4), S( 2,  5), S( 14,  13)
+  };
+
   const Score ThreatenedByHangingPawn = S(40, 60);
 
   // Assorted bonuses and penalties used by evaluation
@@ -652,7 +658,7 @@ namespace {
         if (pos.count<PAWN>(Us) < pos.count<PAWN>(Them))
             ebonus += ebonus / 4;
 
-        score += make_score(mbonus, ebonus);
+        score += make_score(mbonus, ebonus) + PassedFile[file_of(s)];
     }
 
     if (DoTrace)
