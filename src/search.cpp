23ad2afaf34ede90ce15e06c1db15acdf92fc018
Ajith <achajo@yahoo.co.in>
2015-10-06 21:45:04 +0530

    widen search at multiple threads(crude attempt)

--- src/search.cpp
+++ src/search.cpp
@@ -174,8 +174,8 @@ void Search::init() {
 
   for (int d = 0; d < 16; ++d)
   {
-    FutilityMoveCounts[0][d] = int(2.4  + (Options["Threads"] - 1) + 0.773 * pow(d + 0.00, 1.8) );
-    FutilityMoveCounts[1][d] = int(2.9  + (Options["Threads"] - 1) + 1.045 * pow(d + 0.49, 1.8) );
+    FutilityMoveCounts[0][d] = int(2.4  + (2*Options["Threads"] - 2) + 0.773 * pow(d + 0.00, 1.8) );
+    FutilityMoveCounts[1][d] = int(2.9  + (2*Options["Threads"] - 2) + 1.045 * pow(d + 0.49, 1.8) );
   }
 }
 
