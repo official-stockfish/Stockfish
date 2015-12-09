dba9ee76c414dd2ee32207dfd38105f8f4ed7707
Leonid Pechenik <lp@home>
2015-12-09 02:07:34 -0500

    best version

--- src/timeman.h
+++ src/timeman.h
@@ -31,7 +31,7 @@ class TimeManagement {
 public:
   void init(Search::LimitsType& limits, Color us, int ply);
   void pv_instability(double bestMoveChanges) { unstablePvFactor = 1 + bestMoveChanges; }
-  int available() const { return int(optimumTime * unstablePvFactor * 0.76); }
+  int available() const { return int(optimumTime * unstablePvFactor * 1.016); }
   int maximum() const { return maximumTime; }
   int elapsed() const { return int(Search::Limits.npmsec ? Threads.nodes_searched() : now() - startTime); }
 
