1aaf780a139758b3a179522b495d9e6c86988d07
stockfishdeveloper <joeblow@gmail.com>
2015-12-14 14:12:23 -0600

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -111,7 +111,7 @@ namespace {
   enum { Mobility, PawnStructure, PassedPawns, Space, KingSafety, Threats };
 
   const struct Weight { int mg, eg; } Weights[] = {
-    {266, 404}, {214, 230}, {193, 300}, {140, 50}, {330, -35}, {404, 100}
+    {266, 334}, {214, 203}, {193, 262}, {47, 0}, {330, 0}, {404, 241}
   };
 
   Score operator*(Score s, const Weight& w) {
