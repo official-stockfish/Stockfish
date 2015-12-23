f50a62926207584a2e0edfc487fcf3086988d47b
Leonid Pechenik <lp@home>
2015-12-23 03:41:32 -0500

    best values

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -32,11 +32,11 @@ namespace {
   enum TimeType { OptimumTime, MaxTime };
 
   const int MoveHorizon   = 50;   // Plan time management at most this many moves ahead
-  const double MaxRatio   = 7.48;  // When in trouble, we can step over reserved time with this ratio
-  const double StealRatio = 0.36; // However we must not steal time from remaining moves over this ratio
+  const double MaxRatio   = 7.09;  // When in trouble, we can step over reserved time with this ratio
+  const double StealRatio = 0.35; // However we must not steal time from remaining moves over this ratio
 
   // Easy move, fail low + eval drop, no fail low, no eval drop, no fail low + no eval drop 
-  const double searchFactor[5] = {0.12, 0.968, 0.743, 0.791, 0.383};
+  const double searchFactor[5] = {0.117, 0.968, 0.726, 0.777, 0.348};
 
   // move_importance() is a skew-logistic function based on naive statistical
   // analysis of "how many games are still undecided after n half-moves". Game
@@ -45,9 +45,9 @@ namespace {
 
   double move_importance(int ply) {
 
-    const double XScale = 7.28;
-    const double XShift = 60.5;
-    const double Skew   = 0.188;
+    const double XScale = 7.64;
+    const double XShift = 58.4;
+    const double Skew   = 0.183;
 
     return pow((1 + exp((ply - XShift) / XScale)), -Skew) + DBL_MIN; // Ensure non-zero
   }
