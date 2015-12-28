9c53863a0f25c298211fa1e5a69437c798131f47
Marco Costalba <mcostalba@gmail.com>
2015-12-28 13:14:49 +0100

    Fix assert with very high score position
    In case of a very high material score, we can
    overflow VALUE_INFINITE.
    
    This patch fixes an assert with:
    
    position fen 7k/QQQQR3/2B5/4KN1Q/3QQ3/8/8/4R3 b - - 0 1
    go depth 1
    
    No functional change.

--- src/endgame.cpp
+++ src/endgame.cpp
@@ -163,7 +163,7 @@ Value Endgame<KXK>::operator()(const Position& pos) const {
       ||(pos.count<BISHOP>(strongSide) && pos.count<KNIGHT>(strongSide))
       ||(pos.count<BISHOP>(strongSide) > 1 && opposite_colors(pos.squares<BISHOP>(strongSide)[0],
                                                               pos.squares<BISHOP>(strongSide)[1])))
-      result += VALUE_KNOWN_WIN;
+      result = std::min(result + VALUE_KNOWN_WIN, VALUE_MATE_IN_MAX_PLY - 1);
 
   return strongSide == pos.side_to_move() ? result : -result;
 }
