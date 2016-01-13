bea3c2e701401017fb22b1118f2b52e4e96772a0
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2016-01-13 14:24:52 +0100

    depth tolerance tweak in TT

--- src/tt.h
+++ src/tt.h
@@ -50,7 +50,7 @@ struct TTEntry {
 
     // Don't overwrite more valuable entries
     if (  (k >> 48) != key16
-        || d > depth8 - 2
+        || d > depth8 - 4
      /* || g != (genBound8 & 0xFC) // Matching non-zero keys are already refreshed by probe() */
         || b == BOUND_EXACT)
     {
