744ed85a4dd3f478c4ce26e4257e64bf5c9abbdd
VoyagerOne <excelgeek@gmail.com>
2016-02-14 19:45:10 +0000

    Fix futility pruning bug
    PredictedDepth can be negative, causing the futility_margin to be negative.
    It will be very difficult to tweak moveCount pruning and reduction formula, as they are tuned to prevent this behavior.
    
    No functional change
    
    Resolves #587

--- src/search.cpp
+++ src/search.cpp
@@ -969,7 +969,7 @@ moves_loop: // When in check search starts from here
               && cmh[pos.moved_piece(move)][to_sq(move)] < VALUE_ZERO)
               continue;
 
-          predictedDepth = newDepth - reduction<PvNode>(improving, depth, moveCount);
+          predictedDepth = std::max(newDepth - reduction<PvNode>(improving, depth, moveCount), DEPTH_ZERO);
 
           // Futility pruning: parent node
           if (predictedDepth < 7 * ONE_PLY)
