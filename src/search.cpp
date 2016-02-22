e31f41c3a1c1cb748988661f8655ed3361289786
VoyagerOne <excelgeek@gmail.com>
2016-02-22 18:56:38 -0500

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -179,22 +179,21 @@ namespace {
 
 void Search::init() {
 
-  const double K[][2] = {{ 0.799, 2.281 }, { 0.484, 3.023 }};
+  for (int imp = 0; imp <= 1; ++imp)
+      for (int d = 1; d < 64; ++d)
+          for (int mc = 1; mc < 64; ++mc)
+          {
+              double r = log(d) * log(mc) / 2;
+              if (r < 0.80)
+                continue;
 
-  for (int pv = 0; pv <= 1; ++pv)
-      for (int imp = 0; imp <= 1; ++imp)
-          for (int d = 1; d < 64; ++d)
-              for (int mc = 1; mc < 64; ++mc)
-              {
-                  double r = K[pv][0] + log(d) * log(mc) / K[pv][1];
+              Reductions[0][imp][d][mc] = int(std::round(r)) * ONE_PLY;
+              Reductions[1][imp][d][mc] = std::max(Reductions[0][imp][d][mc] - 1, 0) * ONE_PLY;
 
-                  if (r >= 1.5)
-                      Reductions[pv][imp][d][mc] = int(r) * ONE_PLY;
-
-                  // Increase reduction when eval is not improving
-                  if (!pv && !imp && Reductions[pv][imp][d][mc] >= 2 * ONE_PLY)
-                      Reductions[pv][imp][d][mc] += ONE_PLY;
-              }
+              // Increase reduction when eval is not improving
+              if (!imp && Reductions[0][imp][d][mc] >= 2 * ONE_PLY)
+                Reductions[0][imp][d][mc] += ONE_PLY;
+          }
 
   for (int d = 0; d < 16; ++d)
   {
