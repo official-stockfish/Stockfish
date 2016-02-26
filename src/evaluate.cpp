c3a393504c02f681447122f32f56084bcd13a602
Stéphane Nicolet <cassio@free.fr>
2016-02-26 09:39:55 +0100

    Change pawn factor in initiative formula
    Change pawn factor in initiative formula
    bench : 7183534

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -683,7 +683,7 @@ namespace {
     int pawns = pos.count<PAWN>(WHITE) + pos.count<PAWN>(BLACK);
 
     // Compute the initiative bonus for the attacking side
-    int initiative = 8 * (pawns + asymmetry + kingDistance - 15);
+    int initiative = 8 * (asymmetry + kingDistance) + 12 * pawns - 136;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
