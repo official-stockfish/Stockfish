7ed152de6ff029cc4a5c24c172a25ec3b83fe623
Stéphane Nicolet <cassio@free.fr>
2016-02-26 10:23:58 +0100

    Take 2
    Take 2
    bench : 7483251

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -683,7 +683,7 @@ namespace {
     int pawns = pos.count<PAWN>(WHITE) + pos.count<PAWN>(BLACK);
 
     // Compute the initiative bonus for the attacking side
-    int initiative = 8 * (asymmetry + kingDistance) + 12 * pawns - 136;
+    int initiative = 8 * (asymmetry + kingDistance) + 12 * pawns - 156;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
