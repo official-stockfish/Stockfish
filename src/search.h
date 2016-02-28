352d5a3330f467c2eb310011b7e2adcb1edb4d50
JUBIN CHHEDA <jubinchheda@gmail.com>
2016-02-28 23:29:23 +0000

    Fixing some warnings/errors flagged by cppcheck static code analysis tool.

--- src/search.h
+++ src/search.h
@@ -5,7 +5,7 @@
   Copyright (C) 2015-2016 Marco Costalba, Joona Kiiski, Gary Linscott, Tord Romstad
 
   Stockfish is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published byrotmove
+  it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
