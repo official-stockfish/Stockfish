f446475b26255cf12ee45e2e0c1643d42ad47cad
ElbertoOne <ElbertoOne@users.noreply.github.com>
2016-06-06 16:59:47 +0200

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1010,7 +1010,7 @@ moves_loop: // When in check search starts from here
 
           // Increase reduction for cut nodes
           if (!PvNode && cutNode)
-              r += ONE_PLY;
+              r += 2 * ONE_PLY;
 
           // Decrease reduction for moves that escape a capture. Filter out
           // castling moves, because they are coded as "king captures rook" and
@@ -1019,7 +1019,7 @@ moves_loop: // When in check search starts from here
           else if (   type_of(move) == NORMAL
                    && type_of(pos.piece_on(to_sq(move))) != PAWN
                    && pos.see(make_move(to_sq(move), from_sq(move))) < VALUE_ZERO)
-              r -= ONE_PLY;
+              r -= 2 * ONE_PLY;
 
           // Decrease/increase reduction for moves with a good/bad history
           int rHist = (val - 10000) / 20000;
