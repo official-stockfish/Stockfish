5828a9e40ff3e206414a025338e8d5b682fc9e60
ajithcj <achajo@yahoo.co.in>
2016-07-14 08:14:32 +0000

    Gradually relax the NMP staticEval check for higher depths

--- src/search.cpp
+++ src/search.cpp
@@ -731,7 +731,7 @@ namespace {
     if (   !PvNode
         &&  depth >= 2 * ONE_PLY
         &&  eval >= beta
-        && (ss->staticEval >= beta || depth >= 12 * ONE_PLY)
+        && ((ss->staticEval >= beta - ((int) depth - 6) * 35) || depth >= 13 * ONE_PLY)
         &&  pos.non_pawn_material(pos.side_to_move()))
     {
         ss->currentMove = MOVE_NULL;
