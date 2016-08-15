5596492f6ed72ebc52a4d0f65bed483568e48918
VoyagerOne <excelgeek@gmail.com>
2016-08-15 11:19:21 -0400

    Simplify IID
    STC:
    LLR: 2.96 (-2.94,2.94) [-3.00,1.00]
    Total: 30468 W: 5687 L: 5582 D: 19199
    http://tests.stockfishchess.org/tests/view/57b1ddd80ebc591c761f63e2
    
    LTC:
    LLR: 2.95 (-2.94,2.94) [-3.00,1.00]
    Total: 87406 W: 11756 L: 11725 D: 63925
    http://tests.stockfishchess.org/tests/view/57b212590ebc591c761f63f9
    
    bench: 6554900

--- src/search.cpp
+++ src/search.cpp
@@ -801,13 +801,12 @@ namespace {
     }
 
     // Step 10. Internal iterative deepening (skipped when in check)
-    if (    depth >= (PvNode ? 5 * ONE_PLY : 8 * ONE_PLY)
+    if (    depth >= 6 * ONE_PLY
         && !ttMove
         && (PvNode || ss->staticEval + 256 >= beta))
     {
-        Depth d = depth - 2 * ONE_PLY - (PvNode ? DEPTH_ZERO : depth / 4);
         ss->skipEarlyPruning = true;
-        search<NT>(pos, ss, alpha, beta, d, cutNode);
+        search<NT>(pos, ss, alpha, beta, 3 * depth / 4 - 2 * ONE_PLY, cutNode);
         ss->skipEarlyPruning = false;
 
         tte = TT.probe(posKey, ttHit);
