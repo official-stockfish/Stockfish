91d3dcf1e257933c18dadd73586f7e281b4fabf2
Stefan Geschwentner <stgeschwentner@gmail.com>
2016-08-28 12:39:49 +0200

    try rbeta - static eval as probcut threshold

--- src/search.cpp
+++ src/search.cpp
@@ -802,7 +802,7 @@ namespace {
         assert((ss-1)->currentMove != MOVE_NONE);
         assert((ss-1)->currentMove != MOVE_NULL);
 
-        MovePicker mp(pos, ttMove, PieceValue[MG][pos.captured_piece_type()]);
+        MovePicker mp(pos, ttMove, rbeta - ss->staticEval);
 
         while ((move = mp.next_move()) != MOVE_NONE)
             if (pos.legal(move))
