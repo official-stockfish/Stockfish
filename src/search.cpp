5c216727128cb453c31045279c2d1535e72aaf5d
Stefan Geschwentner <stgeschwentner@gmail.com>
2016-08-29 17:50:03 +0200

    Change comment to match new implementation

--- src/search.cpp
+++ src/search.cpp
@@ -788,9 +788,8 @@ namespace {
     }
 
     // Step 9. ProbCut (skipped when in check)
-    // If we have a very good capture (i.e. SEE > seeValues[captured_piece_type])
-    // and a reduced search returns a value much above beta, we can (almost)
-    // safely prune the previous move.
+    // If we have a good enough capture and a reduced search returns a value
+    // much above beta, we can (almost) safely prune the previous move.
     if (   !PvNode
         &&  depth >= 5 * ONE_PLY
         &&  abs(beta) < VALUE_MATE_IN_MAX_PLY)
