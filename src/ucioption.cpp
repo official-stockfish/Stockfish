205098e8c8de78c0cf80ac281e9bd5a81dcdb39f
IIvec <ivan.ivec@gmail.com>
2016-10-07 18:12:19 +0200

    Time management simplification
    Bench 5608839

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -66,8 +66,6 @@ void init(OptionsMap& o) {
   o["MultiPV"]               << Option(1, 1, 500);
   o["Skill Level"]           << Option(20, 0, 20);
   o["Move Overhead"]         << Option(30, 0, 5000);
-  o["Minimum Thinking Time"] << Option(20, 0, 5000);
-  o["Slow Mover"]            << Option(89, 10, 1000);
   o["nodestime"]             << Option(0, 0, 10000);
   o["UCI_Chess960"]          << Option(false);
   o["SyzygyPath"]            << Option("<empty>", on_tb_path);
