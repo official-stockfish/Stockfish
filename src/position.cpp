0fa80c9ba3b766c4d99ea06d4bc52eb33c28ff47
atumanian <1>
2016-11-08 00:34:11 +0200

    Update comments related after new see_ge()
    Update comments according to changes from my patch: #822
    
    No functional change.

--- src/position.cpp
+++ src/position.cpp
@@ -52,7 +52,7 @@ namespace {
 
 const string PieceToChar(" PNBRQK  pnbrqk");
 
-// min_attacker() is a helper function used by see() to locate the least
+// min_attacker() is a helper function used by see_ge() to locate the least
 // valuable attacker for the side to move, remove the attacker we just found
 // from the bitboards and scan for new X-ray attacks behind it.
 
--- src/search.cpp
+++ src/search.cpp
@@ -984,8 +984,7 @@ moves_loop: // When in check search starts from here
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
-              // hence break make_move(). Also use see() instead of see_sign(),
-              // because the destination square is empty.
+              // hence break make_move().
               else if (   type_of(move) == NORMAL
                        && type_of(pos.piece_on(to_sq(move))) != PAWN
                        && !pos.see_ge(make_move(to_sq(move), from_sq(move)),  VALUE_ZERO))
