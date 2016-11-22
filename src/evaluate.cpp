d6a070f1e29b4cb068099b4b8d6cb8a94987b030
ElbertoOne <ElbertoOne@users.noreply.github.com>
2016-11-22 09:11:00 +0100

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -349,7 +349,6 @@ namespace {
                 Square ksq = pos.square<KING>(Us);
 
                 if (   ((file_of(ksq) < FILE_E) == (file_of(s) < file_of(ksq)))
-                    && (rank_of(ksq) == rank_of(s) || relative_rank(Us, ksq) == RANK_1)
                     && !ei.pi->semiopen_side(Us, file_of(ksq), file_of(s) < file_of(ksq)))
                     score -= (TrappedRook - make_score(mob * 22, 0)) * (1 + !pos.can_castle(Us));
             }
