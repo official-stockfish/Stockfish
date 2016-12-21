e5fb94b7ec1e4ce129f923800cf60a87072cef4b
Jonathan Calovski <jonoface@live.com.au>
2016-12-21 15:16:20 +1100

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1452,7 +1452,7 @@ moves_loop: // When in check search starts from here
     // RootMoves are already sorted by score in descending order
     Value topScore = rootMoves[0].score;
     int delta = std::min(topScore - rootMoves[multiPV - 1].score, PawnValueMg);
-    int weakness = 120 - 4 * level - 1;
+    int weakness = 120 - 4 * level + 3;
     int maxScore = -VALUE_INFINITE;
 
     // Choose best move. For each move score we add two terms, both dependent on
