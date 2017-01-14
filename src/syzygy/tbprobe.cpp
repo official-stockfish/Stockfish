40d14181a98e1a6ac444e6376ba8f795d578841d
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2017-01-14 16:13:11 +0100

    Fixup.

--- src/syzygy/tbprobe.cpp
+++ src/syzygy/tbprobe.cpp
@@ -17,10 +17,7 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <algorithm>
 #include <atomic>
-#include <cstdint>
-#include <cstring>   // For memset
 #include <deque>
 #include <fstream>
 #include <iostream>
