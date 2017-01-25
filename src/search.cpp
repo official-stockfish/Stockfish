3f43ca3d33bdf18682878aa909d41b254962eb52
VoyagerOne <excelgeek@gmail.com>
2017-01-25 14:54:00 -0500

    Update Bench 6936343

--- src/search.cpp
+++ src/search.cpp
@@ -998,7 +998,7 @@ moves_loop: // When in check search starts from here
                            + (fmh  ? (*fmh )[moved_piece][to_sq(move)] : VALUE_ZERO)
                            + (fmh2 ? (*fmh2)[moved_piece][to_sq(move)] : VALUE_ZERO)
                            + thisThread->history.get(~pos.side_to_move(), move)
-                           - 8000; // Correction factor
+                           - 4000; // Correction factor
 
               // Decrease/increase reduction by comparing opponent's stat score
               if (ss->history > VALUE_ZERO && (ss-1)->history < VALUE_ZERO)
