4eb17dbb71ae8b8c8fc76371f722470660931eec
VoyagerOne <excelgeek@gmail.com>
2017-02-01 22:18:56 -0500

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -292,11 +292,6 @@ namespace {
             ei.kingAdjacentZoneAttacksCount[Us] += popcount(b & ei.attackedBy[Them][KING]);
         }
 
-        if (Pt == QUEEN)
-            b &= ~(  ei.attackedBy[Them][KNIGHT]
-                   | ei.attackedBy[Them][BISHOP]
-                   | ei.attackedBy[Them][ROOK]);
-
         int mob = popcount(b & ei.mobilityArea[Us]);
 
         mobility[Us] += MobilityBonus[Pt][mob];
