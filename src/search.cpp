d4dd8805a5fd573ef78aa2f1402995a54032b34b
VoyagerOne <excelgeek@gmail.com>
2017-03-07 12:05:06 -0500

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -172,7 +172,7 @@ void Search::init() {
       for (int d = 1; d < 64; ++d)
           for (int mc = 1; mc < 64; ++mc)
           {
-              double r = log(d) * log(mc) / 2;
+              double r = log(d) * log(mc) / 1.95;
 
               Reductions[NonPV][imp][d][mc] = int(std::round(r));
               Reductions[PV][imp][d][mc] = std::max(Reductions[NonPV][imp][d][mc] - 1, 0);
