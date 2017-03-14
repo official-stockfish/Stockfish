8fe4fd31a7fe4bee0b85a0c1988a520c17527c42
IIvec <ivan.ivec@gmail.com>
2017-03-14 20:43:20 +0100

    For CorChess

--- src/search.cpp
+++ src/search.cpp
@@ -159,7 +159,7 @@ void Search::init() {
       for (int d = 1; d < 64; ++d)
           for (int mc = 1; mc < 64; ++mc)
           {
-              double r = log(d) * log(mc) / 1.95;
+              double r = log(d) * log(mc) / 2;
 
               Reductions[NonPV][imp][d][mc] = int(std::round(r));
               Reductions[PV][imp][d][mc] = std::max(Reductions[NonPV][imp][d][mc] - 1, 0);
