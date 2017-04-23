73abc2760f69700131b1f8abdb6d9c9f7f02eec0
Stéphane Nicolet <cassio@free.fr>
2017-04-23 08:19:11 +0200

    Avoid misuse of StepAttacksBB for pawns
    Make it explicit that first index of StepAttacksBB is a piece, not a
    piece type.
    
    Non functional change.

--- src/position.h
+++ src/position.h
@@ -273,6 +273,7 @@ inline Square Position::castling_rook_square(CastlingRight cr) const {
 
 template<PieceType Pt>
 inline Bitboard Position::attacks_from(Square s) const {
+  assert(Pt != PAWN);
   return  Pt == BISHOP || Pt == ROOK ? attacks_bb<Pt>(s, byTypeBB[ALL_PIECES])
         : Pt == QUEEN  ? attacks_from<ROOK>(s) | attacks_from<BISHOP>(s)
         : StepAttacksBB[Pt][s];
