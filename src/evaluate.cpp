7887d3578668f1b69dd803825c01c1ebe0af38c6
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-04-29 23:39:21 +0200

    correct assert because its triggered

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -618,7 +618,7 @@ namespace {
     {
         Square s = pop_lsb(&b);
 
-        assert(!(pos.pieces(PAWN) & forward_bb(Us, s + pawn_push(Us))));
+        assert(!(pos.pieces(Them, PAWN) & forward_bb(Us, s + pawn_push(Us))));
 
         bb = forward_bb(Us, s) & (ei.attackedBy[Them][ALL_PIECES] | pos.pieces(Them));
         score -= HinderPassedPawn * popcount(bb);
