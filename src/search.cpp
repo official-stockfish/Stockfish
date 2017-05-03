dedba8c4b5c8d6635e7b0abc6a1f7f1ff0f1f457
Guenther Demetz <guenther.demetz@wuerth-phoenix.com>
2017-05-03 08:43:23 +0200

    Simplification of checkmate detection in quiescence search.
    Implies storing checkmate positions in TT too.
    Tested for no regression:
    
    STC 
    http://tests.stockfishchess.org/tests/view/590337e00ebc59035df34086
    LLR: 2.96 (-2.94,2.94) [-3.00,1.00]
    Total: 19067 W: 3451 L: 3327 D: 12289
    
    LTC
    http://tests.stockfishchess.org/tests/view/590357c60ebc59035df34093
    LLR: 2.96 (-2.94,2.94) [-3.00,1.00]
    Total: 120659 W: 15383 L: 15390 D: 89886
    
    bench: 5954618

--- src/search.cpp
+++ src/search.cpp
@@ -1207,7 +1207,8 @@ moves_loop: // When in check search starts from here
     if (InCheck)
     {
         ss->staticEval = VALUE_NONE;
-        bestValue = futilityBase = -VALUE_INFINITE;
+        // Start from the worst case which is checkmate
+        bestValue = futilityBase = mated_in(ss->ply);  // Plies to mate from the root
     }
     else
     {
@@ -1335,11 +1336,6 @@ moves_loop: // When in check search starts from here
           }
        }
     }
-
-    // All legal moves have been searched. A special case: If we're in check
-    // and no legal moves were found, it is checkmate.
-    if (InCheck && bestValue == -VALUE_INFINITE)
-        return mated_in(ss->ply); // Plies to mate from the root
 
     tte->save(posKey, value_to_tt(bestValue, ss->ply),
               PvNode && bestValue > oldAlpha ? BOUND_EXACT : BOUND_UPPER,
