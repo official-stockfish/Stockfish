c843352092f0de725ec107f77beb4003c72972c3
VoyagerOne <excelgeek@gmail.com>
2017-07-15 14:22:19 -0400

    Remove Redundant IFs:  Bench- 6258930
    Remove Redundant IF Statements
    Non-functional Change

--- src/search.cpp
+++ src/search.cpp
@@ -685,9 +685,8 @@ namespace {
             eval = ss->staticEval = evaluate(pos);
 
         // Can ttValue be used as a better position evaluation?
-        if (ttValue != VALUE_NONE)
-            if (tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER))
-                eval = ttValue;
+        if (ttValue != VALUE_NONE && tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER))
+            eval = ttValue;
     }
     else
     {
@@ -1215,9 +1214,8 @@ moves_loop: // When in check search starts from here
                 ss->staticEval = bestValue = evaluate(pos);
 
             // Can ttValue be used as a better position evaluation?
-            if (ttValue != VALUE_NONE)
-                if (tte->bound() & (ttValue > bestValue ? BOUND_LOWER : BOUND_UPPER))
-                    bestValue = ttValue;
+            if (ttValue != VALUE_NONE && tte->bound() & (ttValue > bestValue ? BOUND_LOWER : BOUND_UPPER))
+                bestValue = ttValue;
         }
         else
             ss->staticEval = bestValue =
