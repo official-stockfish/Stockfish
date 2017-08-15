fa5b0936eef8e647df99f424e454510839d0a344
Marco Costalba <mcostalba@gmail.com>
2017-08-15 02:51:14 -0700

    Wide bench coverage
    Add tests for:
    
    - Positions with move list
    - Chess960 positions
    
    Now bench covers almost all cases, only few endgames
    are still out of reach (verified with lcov)
    
    It is a non functionality patch, but bench
    changed because we added new test positions.
    
    bench: 5479946

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -24,19 +24,19 @@
 #include <vector>
 
 #include "position.h"
-#include "uci.h"
 
 using namespace std;
 
 namespace {
 
 const vector<string> Defaults = {
+  "setoption name UCI_Chess960 value false",
   "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
   "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 10",
   "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 11",
   "4rrk1/pp1n3p/3q2pQ/2p1pb2/2PP4/2P3N1/P2B2PP/4RRK1 b - - 7 19",
-  "rq3rk1/ppp2ppp/1bnpb3/3N2B1/3NP3/7P/PPPQ1PP1/2KR3R w - - 7 14",
-  "r1bq1r1k/1pp1n1pp/1p1p4/4p2Q/4Pp2/1BNP4/PPP2PPP/3R1RK1 w - - 2 14",
+  "rq3rk1/ppp2ppp/1bnpb3/3N2B1/3NP3/7P/PPPQ1PP1/2KR3R w - - 7 14 moves d4e6",
+  "r1bq1r1k/1pp1n1pp/1p1p4/4p2Q/4Pp2/1BNP4/PPP2PPP/3R1RK1 w - - 2 14 moves g2g4",
   "r3r1k1/2p2ppp/p1p1bn2/8/1q2P3/2NPQN2/PPP3PP/R4RK1 b - - 2 15",
   "r1bbk1nr/pp3p1p/2n5/1N4p1/2Np1B2/8/PPP2PPP/2KR1B1R w kq - 0 13",
   "r1bq1rk1/ppp1nppp/4n3/3p3Q/3P4/1BP1B3/PP1N2PP/R4RK1 w - - 1 16",
@@ -49,7 +49,7 @@ const vector<string> Defaults = {
   "3q2k1/pb3p1p/4pbp1/2r5/PpN2N2/1P2P2P/5PP1/Q2R2K1 b - - 4 26",
   "6k1/6p1/6Pp/ppp5/3pn2P/1P3K2/1PP2P2/3N4 b - - 0 1",
   "3b4/5kp1/1p1p1p1p/pP1PpP1P/P1P1P3/3KN3/8/8 w - - 0 1",
-  "2K5/p7/7P/5pR1/8/5k2/r7/8 w - - 0 1",
+  "2K5/p7/7P/5pR1/8/5k2/r7/8 w - - 0 1 moves g5g6 f3e3 g6g5 e3f3",
   "8/6pk/1p6/8/PP3p1p/5P2/4KP1q/3Q4 w - - 0 1",
   "7k/3p2pp/4q3/8/4Q3/5Kp1/P6b/8 w - - 0 1",
   "8/2p5/8/2kPKp1p/2p4P/2P5/3P4/8 w - - 0 1",
@@ -76,11 +76,15 @@ const vector<string> Defaults = {
   "8/R7/2q5/8/6k1/8/1P5p/K6R w - - 0 124", // Draw
 
   // Mate and stalemate positions
+  "6k1/3b3r/1p1p4/p1n2p2/1PPNpP1q/P3Q1p1/1R1RB1P1/5K2 b - - 0 1",
+  "r2r1n2/pp2bk2/2p1p2p/3q4/3PN1QP/2P3R1/P4PP1/5RK1 w - - 0 1",
   "8/8/8/8/8/6k1/6p1/6K1 w - -",
-  "5k2/5P2/5K2/8/8/8/8/8 b - -",
-  "8/8/8/8/8/4k3/4p3/4K3 w - -",
-  "8/8/8/8/8/5K2/8/3Q1k2 b - -",
-  "7k/7P/6K1/8/3B4/8/8/8 b - -"
+  "7k/7P/6K1/8/3B4/8/8/8 b - -",
+
+  // Chess 960
+  "setoption name UCI_Chess960 value true",
+  "bbqnnrkr/pppppppp/8/8/8/8/PPPPPPPP/BBQNNRKR w KQkq - 0 1 moves g2g3 d7d5 d2d4 c8h3 c1g5 e8d6 g5e7 f7f6",
+  "setoption name UCI_Chess960 value false"
 };
 
 } // namespace
@@ -97,7 +101,7 @@ const vector<string> Defaults = {
 /// bench 64 1 100000 default nodes -> search default positions for 100K nodes each
 /// bench 16 1 5 default perft -> run a perft 5 on default positions
 
-std::vector<string> setup_bench(const Position& current , istream& is) {
+vector<string> setup_bench(const Position& current, istream& is) {
 
   vector<string> fens, list;
   string go, token;
@@ -109,10 +113,8 @@ std::vector<string> setup_bench(const Position& current , istream& is) {
   string fenFile   = (is >> token) ? token : "default";
   string limitType = (is >> token) ? token : "depth";
 
-  // Build 'go' string (movetime is in millisecs)
   go = "go " + limitType + " " + limit;
 
-  // Get test positions fens
   if (fenFile == "default")
       fens = Defaults;
 
@@ -137,16 +139,18 @@ std::vector<string> setup_bench(const Position& current , istream& is) {
       file.close();
   }
 
-  // Build UCI command list
   list.emplace_back("ucinewgame");
   list.emplace_back("setoption name Threads value " + threads);
   list.emplace_back("setoption name Hash value " + ttSize);
 
   for (const string& fen : fens)
-  {
-      list.emplace_back("position fen " + fen);
-      list.emplace_back(go);
-  }
+      if (fen.find("setoption") != string::npos)
+          list.emplace_back(fen);
+      else
+      {
+          list.emplace_back("position fen " + fen);
+          list.emplace_back(go);
+      }
 
   return list;
 }
