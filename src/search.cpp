19b50476ebdbb30b01f4a083ea07702b36a7db74
GuardianRM <strimr25@mail.ru>
2017-09-21 09:58:29 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -979,10 +979,10 @@ moves_loop: // When in check search starts from here
                              - 4000;
 
               // Decrease/increase reduction by comparing opponent's stat score
-              if (ss->statScore > 0 && (ss-1)->statScore < 0)
+              if (ss->statScore >= 0 && (ss-1)->statScore < 0)
                   r -= ONE_PLY;
 
-              else if (ss->statScore < 0 && (ss-1)->statScore > 0)
+              else if (ss->statScore < 0 && (ss-1)->statScore >= 0)
                   r += ONE_PLY;
 
               // Decrease/increase reduction for moves with a good/bad history
