e46a621563d09f0f17055bd4347d7d36252866dd
Stéphane Nicolet <cassio@free.fr>
2017-10-02 00:41:06 +0200

    Good bishops on the main diagonals
    Bonus in midgame for bishops on long diagonals when the central squares are not occupied by pawns.
    
    Author: ElbertoOne
    
    Passed STC:
    LLR: 2.96 (-2.94,2.94) [0.00,5.00]
    Total: 10801 W: 1955 L: 1786 D: 7060
    http://tests.stockfishchess.org/tests/view/59cf5c1d0ebc5916ff64b9da
    
    and LTC:
    LLR: 2.97 (-2.94,2.94) [0.00,5.00]
    Total: 83978 W: 10685 L: 10303 D: 62990
    http://tests.stockfishchess.org/tests/view/59cf6f6c0ebc5916ff64b9e4
    
    Bench: 5620312

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -204,6 +204,7 @@ namespace {
   // Assorted bonuses and penalties used by evaluation
   const Score MinorBehindPawn     = S( 16,  0);
   const Score BishopPawns         = S(  8, 12);
+  const Score LongRangedBishop    = S( 22,  0);
   const Score RookOnPawn          = S(  8, 24);
   const Score TrappedRook         = S( 92,  0);
   const Score WeakQueen           = S( 50, 10);
@@ -338,9 +339,17 @@ namespace {
                 && (pos.pieces(PAWN) & (s + pawn_push(Us))))
                 score += MinorBehindPawn;
 
-            // Penalty for pawns on the same color square as the bishop
             if (Pt == BISHOP)
+            {
+                // Penalty for pawns on the same color square as the bishop
                 score -= BishopPawns * pe->pawns_on_same_color_squares(Us, s);
+
+                // Bonus for bishop on a long diagonal without pawns in the center
+                if (    (LongDiagonals & s)
+                    && !(attackedBy[Them][PAWN] & s)
+                    && !(Center & PseudoAttacks[BISHOP][s] & pos.pieces(PAWN)))
+                    score += LongRangedBishop;
+            }
 
             // An important Chess960 pattern: A cornered bishop blocked by a friendly
             // pawn diagonally in front of it is a very serious problem, especially
