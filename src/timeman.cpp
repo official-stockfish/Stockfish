2dc9ec1e81129a9afb28b0d2566be8d6ecc2a308
IIvec <ivan.ivec@gmail.com>
2017-10-14 20:52:51 +0200

    Only 5% of time in sudden death after move 40!
    Bench 5234652

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -59,7 +59,8 @@ namespace {
     {
         double k = 1 + 20 * moveNum / (500.0 + moveNum);
         double l = (type == OptimumTime ? 0.017 : 0.07);
-        double sratio = std::min(0.1, l * k); // Ratio of myTime for sudden death case
+        double sratio = k * l; // Ratio of myTime for sudden death case
+        if (moveNum > 40) sratio = std::min(0.05, sratio);
         ratio = sratio + l * inc / myTime;
     }
 
