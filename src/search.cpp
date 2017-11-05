997a958fcd7d8ff0ede0367da092acb56db25baa
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2017-11-05 21:06:47 +0100

    Simplify NMP condition.
    Removes depth condition, adjust parameters.
    
    passed STC:
    http://tests.stockfishchess.org/tests/view/5a008cbc0ebc590ccbb8a512
    LLR: 2.96 (-2.94,2.94) [-3.00,1.00]
    Total: 29282 W: 5317 L: 5210 D: 18755
    
    passed LTC:
    http://tests.stockfishchess.org/tests/view/5a00d8530ebc590ccbb8a541
    LLR: 2.96 (-2.94,2.94) [-3.00,1.00]
    Total: 26893 W: 3458 L: 3345 D: 20090
    
    Bench: 5015773

--- src/search.cpp
+++ src/search.cpp
@@ -686,7 +686,7 @@ namespace {
     // Step 8. Null move search with verification search (is omitted in PV nodes)
     if (   !PvNode
         &&  eval >= beta
-        && (ss->staticEval >= beta - 35 * (depth / ONE_PLY - 6) || depth >= 13 * ONE_PLY)
+        &&  ss->staticEval >= beta - 36 * depth / ONE_PLY + 225
         &&  pos.non_pawn_material(pos.side_to_move()))
     {
 
