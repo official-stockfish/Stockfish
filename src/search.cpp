432b21622ade545c09cf748d4668ab0f0f3e58b3
VoyagerOne <excelgeek@gmail.com>
2017-12-09 12:44:36 -0500

    Bench: 4744425

--- src/search.cpp
+++ src/search.cpp
@@ -920,7 +920,7 @@ moves_loop: // When in check search starts from here
               if ((ss-1)->moveCount > 15)
                   r -= ONE_PLY;
 
-              // Decrease reduction for exact PV nodes
+              // Decrease reduction for exact TT nodes
               if (ttExact)
                   r -= ONE_PLY;
 
