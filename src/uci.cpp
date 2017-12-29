182de211c8796b0fa44c32188f52d2949e3af6e1
Stéphane Nicolet <cassio@free.fr>
2017-12-29 12:25:47 +0100

    Avoid draw and win adjudication in fishtest

--- src/uci.cpp
+++ src/uci.cpp
@@ -256,6 +256,9 @@ string UCI::value(Value v) {
 
   stringstream ss;
 
+  // Avoid draw and win adjudication in fishtest
+  v = PawnValueEg;
+
   if (abs(v) < VALUE_MATE - MAX_PLY)
       ss << "cp " << v * 100 / PawnValueEg;
   else
