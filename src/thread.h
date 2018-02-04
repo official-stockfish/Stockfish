c29a97ec1c6e4069d01f335dc75007b837003ca6
syzygy1 <3028851+syzygy1@users.noreply.github.com>
2018-02-04 20:05:14 +0100

    Revert "Implement 'old' multipv search."
    This reverts commit 44a7db0f9ac02d2461aff39e25f1ac9107ffbfac.
    The revert fixes https://github.com/official-stockfish/Stockfish/issues/1392

--- src/thread.h
+++ src/thread.h
@@ -60,7 +60,7 @@ public:
   Pawns::Table pawnsTable;
   Material::Table materialTable;
   Endgames endgames;
-  size_t PVIdx, multiPV;
+  size_t PVIdx;
   int selDepth, nmp_ply, nmp_odd;
   std::atomic<uint64_t> nodes, tbHits;
 
