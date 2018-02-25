2ec36f8ae8089bf1d687951c1d26c3a6a9e9eb01
Stéphane Nicolet <cassio@free.fr>
2018-02-25 15:12:19 +0100

    Revert "Count passed pawns in asymmetry measure"

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -164,9 +164,6 @@ namespace {
                     e->passedPawns[Us] |= s;
         }
 
-        if (e->passedPawns[Us] & s)
-            e->asymmetry++;
-
         // Score this pawn
         if (supported | phalanx)
             score += Connected[opposed][bool(phalanx)][popcount(supported)][relative_rank(Us, s)];
@@ -223,11 +220,10 @@ Entry* probe(const Position& pos) {
       return e;
 
   e->key = key;
-  e->asymmetry = 0;
   e->scores[WHITE] = evaluate<WHITE>(pos, e);
   e->scores[BLACK] = evaluate<BLACK>(pos, e);
-  e->asymmetry += popcount(e->semiopenFiles[WHITE] ^ e->semiopenFiles[BLACK]);
-  e->openFiles  = popcount(e->semiopenFiles[WHITE] & e->semiopenFiles[BLACK]);
+  e->asymmetry = popcount(e->semiopenFiles[WHITE] ^ e->semiopenFiles[BLACK]);
+  e->openFiles = popcount(e->semiopenFiles[WHITE] & e->semiopenFiles[BLACK]);
 
   return e;
 }
