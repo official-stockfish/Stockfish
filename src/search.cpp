bca62090801923f30c6fbfc05c2aa458e219579c
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2018-03-08 08:04:04 +0100

    Remove wrong comment (at this location ss->staticEval is different from VALUE_NONE (TT collisions excluded))

--- src/search.cpp
+++ src/search.cpp
@@ -679,7 +679,6 @@ namespace {
     }
 
     improving =   ss->staticEval >= (ss-2)->staticEval
-            /* || ss->staticEval == VALUE_NONE Already implicit in the previous condition */
                ||(ss-2)->staticEval == VALUE_NONE;
 
     if (skipEarlyPruning || !pos.non_pawn_material(pos.side_to_move()))
