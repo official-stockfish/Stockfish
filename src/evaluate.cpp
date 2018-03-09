877a9da95ccecfd44a33319207a4d71136161721
protonspring <mike@whiteley.org>
2018-03-09 07:20:05 -0700

    introduct connectedness.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -126,6 +126,9 @@ namespace {
     { S( 9, 2), S(15, 5) }  // Bishop
   };
 
+  // Connectedness
+  const Score Connected = S(2,2);
+
   // RookOnFile[semiopen/open] contains bonuses for each rook when there is
   // no (friendly) pawn on the rook file.
   const Score RookOnFile[] = { S(20, 7), S(45, 20) };
@@ -606,6 +609,12 @@ namespace {
 
         score += KnightOnQueen * popcount(b);
     }
+
+    // Connectedness
+    b = pos.pieces(Us) & attackedBy[Us][ALL_PIECES] 
+                       & ~attackedBy[Us][PAWN]
+                       & ~pos.pieces(Us, KING);
+    score += Connected * popcount(b);
 
     if (T)
         Trace::add(THREAT, Us, score);
