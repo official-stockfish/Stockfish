c8d9cb87d620e1f8d6ef0b71ac53f16539b8d791
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-03-27 20:47:17 +0200

    Rook EG -1.5%

--- src/types.h
+++ src/types.h
@@ -186,7 +186,7 @@ enum Value : int {
   PawnValueMg   = 171,   PawnValueEg   = 240,
   KnightValueMg = 764,   KnightValueEg = 848,
   BishopValueMg = 826,   BishopValueEg = 891,
-  RookValueMg   = 1282,  RookValueEg   = 1400,
+  RookValueMg   = 1282,  RookValueEg   = 1350,
   QueenValueMg  = 2500,  QueenValueEg  = 2670,
 
   MidgameLimit  = 15258, EndgameLimit  = 3915
