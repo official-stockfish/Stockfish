18254624905241fada78e0638f88b4451c334ebc
31m059 <37052095+31m059@users.noreply.github.com>
2018-04-05 15:10:25 -0400

    S(10, 0). 5365004

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -171,7 +171,7 @@ namespace {
   constexpr Score KnightOnQueen      = S( 21, 11);
   constexpr Score LongDiagonalBishop = S( 22,  0);
   constexpr Score MinorBehindPawn    = S( 16,  0);
-  constexpr Score Overload           = S( 10,  5);
+  constexpr Score Overload           = S( 10,  0);
   constexpr Score PawnlessFlank      = S( 20, 80);
   constexpr Score RookOnPawn         = S(  8, 24);
   constexpr Score SliderOnQueen      = S( 42, 21);
