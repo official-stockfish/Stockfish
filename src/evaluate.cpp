23b64cb2e642748354fcad804c77f7a71404b6a3
SFisGOD <38142170+SFisGOD@users.noreply.github.com>
2018-04-07 05:16:41 +0800

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -177,7 +177,7 @@ namespace {
   constexpr Score ThreatByPawnPush   = S( 47, 26);
   constexpr Score ThreatByRank       = S( 16,  3);
   constexpr Score ThreatBySafePawn   = S(175,168);
-  constexpr Score TrappedRook        = S( 92,  0);
+  constexpr Score TrappedRook        = S( 90,  2);
   constexpr Score WeakQueen          = S( 50, 10);
   constexpr Score WeakUnopposedPawn  = S(  5, 25);
 
