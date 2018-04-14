966690ccb56851bb52fa8f70ac8740457a75faff
protonspring <mike@whiteley.org>
2018-04-13 18:18:09 -0600

    still same bench.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -172,7 +172,7 @@ namespace {
   constexpr Score LongDiagonalBishop = S( 22,  0);
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score Overload           = S( 10,  5);
-  constexpr Score PawnlessFlank      = S( 20, 80);
+  constexpr Score PawnlessFlank      = S( 20,  0);
   constexpr Score RookOnPawn         = S(  8, 24);
   constexpr Score SliderOnQueen      = S( 42, 21);
   constexpr Score ThreatByPawnPush   = S( 47, 26);
--- src/pawns.cpp
+++ src/pawns.cpp
@@ -31,6 +31,17 @@ namespace {
   #define V Value
   #define S(mg, eg) make_score(mg, eg)
 
+  constexpr Bitboard QueenSide   = FileABB | FileBBB | FileCBB | FileDBB;
+  constexpr Bitboard CenterFiles = FileCBB | FileDBB | FileEBB | FileFBB;
+  constexpr Bitboard KingSide    = FileEBB | FileFBB | FileGBB | FileHBB;
+  constexpr Bitboard Center      = (FileDBB | FileEBB) & (Rank4BB | Rank5BB);
+  constexpr Bitboard KingFlank[FILE_NB] = {
+    QueenSide,   QueenSide, QueenSide,
+    CenterFiles, CenterFiles,
+    KingSide,    KingSide,  KingSide
+  };
+  constexpr Score PawnlessFlank  = S( 0, 80);
+
   // Isolated pawn penalty
   constexpr Score Isolated = S(13, 18);
 
@@ -291,6 +302,11 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
 
   if (pos.can_castle(MakeCastling<Us, QUEEN_SIDE>::right))
       bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_C1)));
+
+  // Penalty when our king is on a pawnless flank
+  Bitboard kf = KingFlank[file_of(ksq)];
+  if (!(pos.pieces(PAWN) & kf))
+      bonus -= PawnlessFlank;
 
   return make_score(bonus, -16 * minKingPawnDistance);
 }
