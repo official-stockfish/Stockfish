74892b931f09f6a9d1cd9617a40b0d21e8fd00a3
protonspring <mike@whiteley.org>
2018-05-29 17:39:21 -0600

    restructing, reverting to passed version for unblocked.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -43,27 +43,34 @@ namespace {
   // Doubled pawn penalty
   constexpr Score Doubled = S(13, 40);
 
-  // Penalty for no pawns in shelter on a file by [file]
-  constexpr Value PawnlessFile[FILE_NB/2] = { V(25), V(  5), V(-20), V(-27)};
-
   // Strength of pawn shelter for our king by [distance from edge][rank].
   // RANK_1 = 0 is used for files where we have no pawn, or pawn is behind our king.
   constexpr Value ShelterStrength[int(FILE_NB) / 2][RANK_NB] = {
     { V(  7), V(76), V( 84), V( 38), V(  7), V( 30), V(-19) },
-    { V(-13-10), V(83-10), V( 42-10), V(-27-10), V(  2-10), V(-32-10), V(-45-10) },
-    { V(-26-20), V(63-20), V(  5-20), V(-44-20), V( -5-20), V(  2-20), V(-59-20) },
-    { V(-19-30), V(53-30), V(-11-30), V(-22-30), V(-12-30), V(-51-30), V(-60-30) }
+    { V(-13), V(83), V( 42), V(-27), V(  2), V(-32), V(-45) },
+    { V(-26), V(63), V(  5), V(-44), V( -5), V(  2), V(-59) },
+    { V(-19), V(53), V(-11), V(-22), V(-12), V(-51), V(-60) }
   };
+
+  // Storm value for no enemy pawn on a file by [distance from edge]
+  constexpr Value PawnlessFile[FILE_NB/2] = { V(25), V( 5), V(-20), V(-27)};
 
   // Danger of enemy pawns moving toward our king by [distance from edge][rank].
   // RANK_1 = 0 is used for files where the enemy has no pawn, or their pawn 
   // is behind our king.
-  constexpr Value UnblockedStorm[RANK_NB] =
-    { V(  0), V( 48), V( 96), V( 45), V( 27), V(  0), V(  0) };
+  constexpr Value UnblockedStorm[int(FILE_NB) / 2][RANK_NB] = {
+    { V(  0), V( 49), V( 96), V( 45), V( 27), V(  0), V(  0) },
+    { V(  0), V( 39), V( 86), V( 35), V( 17), V(-10), V(-10) },
+    { V(  0), V( 29), V( 76), V( 25), V(  7), V(-20), V(-20) },
+    { V(  0), V( 19), V( 66), V( 15), V( -3), V(-30), V(-30) }
+  };
 
   // Danger of blocked enemy pawns storming our king, by rank
-  constexpr Value BlockedStorm[RANK_NB] =
-    { V(  0), V(  0), V( 95), V( 10), V(  0), V(  0), V(  0) };
+  constexpr Value BlockedStorm[FILE_NB/2][RANK_NB] = {
+    { V(  0), V(  0), V( 75), V(-10), V(-20), V(-20), V(-20) },
+    { V(  0), V(  0), V( 75), V(-10), V(-20), V(-20), V(-20) },
+    { V(  0), V(  0), V( 75), V(-10), V(-20), V(-20), V(-20) },
+    { V(  0), V(  0), V( 75), V(-10), V(-20), V(-20), V(-20) } };
 
   #undef S
   #undef V
@@ -224,15 +231,16 @@ Value Entry::evaluate_shelter(const Position& pos, Square ksq) {
   for (File f = File(center - 1); f <= File(center + 1); ++f)
   {
       int d = std::min(f, ~f);
+
       b = ourPawns & file_bb(f);
       int ourRank = b ? relative_rank(Us, backmost_sq(Us, b)) : RANK_1;
       safety += ShelterStrength[d][ourRank];
 
       b = theirPawns & file_bb(f);
       int theirRank = b ? relative_rank(Us, frontmost_sq(Them, b)) : RANK_1;
-      safety -= theirRank ? ((shift<Down>(b) & ourPawns) ? BlockedStorm[theirRank]
-                                                       : UnblockedStorm[theirRank])
-                                                       : PawnlessFile[d];
+      safety -= theirRank ? ((ourRank && (ourRank == theirRank - 1)) ? 
+                     BlockedStorm[d][theirRank] : UnblockedStorm[d][theirRank]) :
+                     PawnlessFile[d];
   }
 
   return safety;
