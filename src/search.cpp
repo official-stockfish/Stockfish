c5d6ae8c9615dd510b99c2d6b0138ac58aece2e1
pb00068 <guenther.demetz@wuerth-phoenix.com>
2018-06-02 18:01:11 +0200

    Extra bonus for capture creating a huge fail high
    STC:
    http://tests.stockfishchess.org/tests/view/5b114f3d0ebc596e9e0881f6
    LLR: 2.95 (-2.94,2.94) [0.00,5.00]
    Total: 13007 W: 2730 L: 2541 D: 7736
    
    LTC:
    http://tests.stockfishchess.org/tests/view/5b1176740ebc59033d2d52c6
    LLR: 2.96 (-2.94,2.94) [0.00,5.00]
    Total: 15594 W: 2417 L: 2239 D: 10938
    
    See https://github.com/official-stockfish/Stockfish/pull/1627
    
    Bench: 4790240

--- src/search.cpp
+++ src/search.cpp
@@ -1166,7 +1166,8 @@ moves_loop: // When in check, search starts from here
         if (!pos.capture_or_promotion(bestMove))
             update_quiet_stats(pos, ss, bestMove, quietsSearched, quietCount, stat_bonus(depth));
         else
-            update_capture_stats(pos, bestMove, capturesSearched, captureCount, stat_bonus(depth));
+            update_capture_stats(pos, bestMove, capturesSearched, captureCount,
+                                 stat_bonus(depth + bool(bestValue > beta + KnightValueMg) * ONE_PLY));
 
         // Extra penalty for a quiet TT move in previous ply when it gets refuted
         if ((ss-1)->moveCount == 1 && !pos.captured_piece())
