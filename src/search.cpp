8ef9bc5a9588c49a9c32f87b025980aed794ecd6
Stéphane Nicolet <cassio@free.fr>
2018-06-03 09:18:18 +0200

    Fix overload ambiguity
    Fix an error when compiling current master with MSVC due to the
    ambiguity of which operator* overload was intended (reported by
    Jarrod Torriero).
    
    No functional change.

--- src/search.cpp
+++ src/search.cpp
@@ -1167,7 +1167,7 @@ moves_loop: // When in check, search starts from here
             update_quiet_stats(pos, ss, bestMove, quietsSearched, quietCount, stat_bonus(depth));
         else
             update_capture_stats(pos, bestMove, capturesSearched, captureCount,
-                                 stat_bonus(depth + bool(bestValue > beta + KnightValueMg) * ONE_PLY));
+                                 stat_bonus(depth + (bestValue > beta + KnightValueMg ? ONE_PLY : DEPTH_ZERO)));
 
         // Extra penalty for a quiet TT move in previous ply when it gets refuted
         if ((ss-1)->moveCount == 1 && !pos.captured_piece())
