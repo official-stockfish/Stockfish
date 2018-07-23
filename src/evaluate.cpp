356d2cdb8277b228ae306fcc68ee7e469d74a740
Goodkov Vasiliy Aleksandrovich <qwegood@mail.ru>
2018-07-23 07:15:28 +0400

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -557,7 +557,7 @@ namespace {
 
         score += Hanging * popcount(weak & ~attackedBy[Them][ALL_PIECES]);
 
-        b = weak & nonPawnEnemies & attackedBy[Them][ALL_PIECES];
+        b = weak & attackedBy[Them][ALL_PIECES];
         score += Overload * popcount(b);
     }
 
