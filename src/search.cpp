423a6998ec7f26a1cc1f611a44624001f6295b59
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-07-24 12:16:49 +0200

    Revert "Tweak reductions formula: 0.88 * depth + 0.12". bench 4491691

--- src/search.cpp
+++ src/search.cpp
@@ -153,8 +153,7 @@ void Search::init() {
       for (int d = 1; d < 64; ++d)
           for (int mc = 1; mc < 64; ++mc)
           {
-              double slope = d > 2 ? 0.88 * d + 0.36 : d;
-              double r = log(slope) * log(mc) / 1.95;
+              double r = log(d) * log(mc) / 1.95;
 
               Reductions[NonPV][imp][d][mc] = int(std::round(r));
               Reductions[PV][imp][d][mc] = std::max(Reductions[NonPV][imp][d][mc] - 1, 0);
