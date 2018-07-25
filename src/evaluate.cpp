6e5ea079843dc01a380e5fe04b7b513d4c342ce7
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-07-25 11:00:58 +0200

    Consider pawns if attacked by rook from forward. bench 5096100

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -540,8 +540,6 @@ namespace {
             score += ThreatByMinor[type_of(pos.piece_on(s))];
             if (type_of(pos.piece_on(s)) != PAWN)
                 score += ThreatByRank * (int)relative_rank(Them, s);
-            else
-                score += ThreatByRank * (int)relative_rank(Them, s) / 2;
         }
 
         b = weak & attackedBy[Us][ROOK];
@@ -549,10 +547,9 @@ namespace {
         {
             Square s = pop_lsb(&b);
             score += ThreatByRook[type_of(pos.piece_on(s))];
-            if (type_of(pos.piece_on(s)) != PAWN)
+            if (    type_of(pos.piece_on(s)) != PAWN
+                || (forward_file_bb(Them, s) & pos.pieces(Us, ROOK)))
                 score += ThreatByRank * (int)relative_rank(Them, s);
-            else
-                score += ThreatByRank * (int)relative_rank(Them, s) / 2;
         }
 
         // Bonus for king attacks on pawns or pieces which are not pawn-defended
