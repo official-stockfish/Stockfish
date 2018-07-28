fae57273b20468f534cce5843152a21214b5da05
Marco Costalba <mcostalba@gmail.com>
2018-07-28 10:38:36 +0200

    Small tweaks to recent code changes
    As a note, current 2 LMR conditions on stat score
    could be simplified in a single line:
    
    r -= ((ss->statScore >= 0) - ((ss-1)->statScore >= 0)) * ONE_PLY;
    
    We keep them splitted in 2 "if" statements because are easier
    to (immediately) read.
    
    No functional change.

--- src/search.cpp
+++ src/search.cpp
@@ -935,7 +935,7 @@ moves_loop: // When in check, search starts from here
               int lmrDepth = std::max(newDepth - reduction<PvNode>(improving, depth, moveCount), DEPTH_ZERO) / ONE_PLY;
 
               // Countermoves based pruning (~20 Elo)
-              if (   lmrDepth <= ((ss-1)->statScore > 0 ? 3 : 2)
+              if (   lmrDepth < 3 + ((ss-1)->statScore > 0)
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
