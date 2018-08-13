5476f98b397d622d44efa26276b9980c2cc374c5
Rocky640 <support@multicim.com>
2018-08-13 10:43:18 -0400

    QT_2
    Clean up
    bench: 4738555

--- src/types.h
+++ src/types.h
@@ -182,7 +182,7 @@ enum Value : int {
   VALUE_MATE_IN_MAX_PLY  =  VALUE_MATE - 2 * MAX_PLY,
   VALUE_MATED_IN_MAX_PLY = -VALUE_MATE + 2 * MAX_PLY,
 
-  PawnValueMg   = 172-30,   PawnValueEg   = 237-30,
+  PawnValueMg   = 142,   PawnValueEg   = 207,
   KnightValueMg = 784,   KnightValueEg = 868,
   BishopValueMg = 828,   BishopValueEg = 916,
   RookValueMg   = 1286,  RookValueEg   = 1378,
