51a5bba45c0a048c85bd9fb2277931f449b93a52
protonspring <mike@whiteley.org>
2018-08-14 10:40:49 -0600

    +10 in default safety.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -212,7 +212,7 @@ Value Entry::evaluate_shelter(const Position& pos, Square ksq) {
   Bitboard theirPawns = b & pos.pieces(Them);
 
   Value safety = (shift<Down>(theirPawns) & (FileABB | FileHBB) & BlockRanks & ksq) ?
-      Value(374) : Value(0);
+      Value(374) : Value(10);
 
   File center = std::max(FILE_B, std::min(FILE_G, file_of(ksq)));
   for (File f = File(center - 1); f <= File(center + 1); ++f)
