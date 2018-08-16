77a60c061f5c0d383f8d35935a24536a5c1a34f1
VoyagerOne <excelgeek@gmail.com>
2018-08-16 16:14:02 -0400

    Bench: 4409220

--- src/search.cpp
+++ src/search.cpp
@@ -82,7 +82,7 @@ namespace {
   // History and stats update bonus, based on depth
   int stat_bonus(Depth depth) {
     int d = depth / ONE_PLY;
-    return d >= 17 ? 0 : 29 * d * d + 138 * d - 134;
+    return d > 17 ? 0 : 29 * d * d + 138 * d - 134;
   }
 
   // Skill structure is used to implement strength limit
