fb513423fd974e57a39cacd38fb4ab3c71b1aa8d
Rocky640 <support@multicim.com>
2018-08-31 15:09:03 -0400

    B2
    Even more psqt penalty when some bloking pawns along the long diagonal
    and close to master if not.
    
    bench: 4693901

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -162,7 +162,7 @@ namespace {
   constexpr Score HinderPassedPawn   = S(  8,  0);
   constexpr Score KingProtector      = S(  6,  6);
   constexpr Score KnightOnQueen      = S( 21, 11);
-  constexpr Score LongDiagonalBishop = S( 36,  0);
+  constexpr Score LongDiagonalBishop = S( 46,  0);
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score Overload           = S( 13,  6);
   constexpr Score PawnlessFlank      = S( 19, 84);
