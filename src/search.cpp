4bef7aa5cd682617502cacfa4413c0d2c78401ca
ElbertoOne <lubvdberg@gmail.com>
2018-09-03 12:15:31 +0200

    Parameter tweaks in PSQT and NMP
    This patch is a combinaison of two parameters tweaks patches which
    have failed as strong yellows at LTC recently, by Alain Savard (Rocky640)
    and Fabian Fichter (ianfab):
      http://tests.stockfishchess.org/tests/view/5b8a71e60ebc592cf2749b1d
      http://tests.stockfishchess.org/tests/view/5b81ce3b0ebc5902bdbb6585
    
    Passed STC:
    LLR: 2.95 (-2.94,2.94) [0.00,4.00]
    Total: 57200 W: 12392 L: 12008 D: 32800
    http://tests.stockfishchess.org/tests/view/5b8d0a5a0ebc592cf274c48f
    
    And LTC:
    LLR: 2.96 (-2.94,2.94) [0.00,4.00]
    Total: 37215 W: 6233 L: 5962 D: 25020
    http://tests.stockfishchess.org/tests/view/5b8d56090ebc592cf274cb53
    
    Closes https://github.com/official-stockfish/Stockfish/pull/1764
    
    Bench: 4136116
    
    ---------------
    
    How to continue from there?
    
    The null move reduction formula in line 769 of search.cpp is quite convoluted
    and full of mysterious magic constants at the moment, it would certainly be
    nice to simplify it and/or gain more Elo from it:
    
    ```
    Depth R = (  (823 + 67 * depth / ONE_PLY) / 256
               + std::min(int(eval - beta) / 200, 3)) * ONE_PLY;
    ```

--- src/search.cpp
+++ src/search.cpp
@@ -766,7 +766,7 @@ namespace {
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and value
-        Depth R = ((823 + 67 * depth / ONE_PLY) / 256 + std::min((eval - beta) / PawnValueMg, 3)) * ONE_PLY;
+        Depth R = ((823 + 67 * depth / ONE_PLY) / 256 + std::min(int(eval - beta) / 200, 3)) * ONE_PLY;
 
         ss->currentMove = MOVE_NULL;
         ss->continuationHistory = &thisThread->continuationHistory[NO_PIECE][0];
