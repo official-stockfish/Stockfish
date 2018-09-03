767c4ad1fc487b72240006c888aaf8c00006cd82
Stéphane Nicolet <cassio@free.fr>
2018-09-03 12:46:05 +0200

    Update list of authors
    And also fix some spaces and formatting oddities in the code.
    
    No functional change

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -32,9 +32,9 @@ namespace {
   #define S(mg, eg) make_score(mg, eg)
 
   // Pawn penalties
- constexpr Score Isolated = S( 5, 15);
- constexpr Score Backward = S( 9, 24);
- constexpr Score Doubled  = S(11, 56);
+  constexpr Score Backward = S( 9, 24);
+  constexpr Score Doubled  = S(11, 56);
+  constexpr Score Isolated = S( 5, 15);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
   Score Connected[2][2][3][RANK_NB];
