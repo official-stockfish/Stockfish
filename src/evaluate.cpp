29bd1c860de31013801783bcbb50f693d82e56ce
Vizvezdenec <Vizvezdenec@gmail.com>
2018-09-07 15:58:47 +0300

    tweak. bench 4275334

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -787,7 +787,6 @@ namespace {
   ScaleFactor Evaluation<T>::scale_factor(Value eg) const {
 
     Color strongSide = eg > VALUE_DRAW ? WHITE : BLACK;
-    Color weakSide = eg < VALUE_DRAW ? WHITE : BLACK;
     int sf = me->scale_factor(pos, strongSide);
 
     // If scale is not already specific, scale down the endgame via general heuristics
@@ -796,12 +795,9 @@ namespace {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material(WHITE) == BishopValueMg
             && pos.non_pawn_material(BLACK) == BishopValueMg)
-            sf = 31;
+            sf = 15+4*pe->pawn_asymmetry();
         else
             sf = std::min(40 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide), sf);
-        if (   pos.non_pawn_material(strongSide) == RookValueMg
-            && pos.non_pawn_material(weakSide) == BishopValueMg)
-            sf = 30 + 3 * pe->pawn_asymmetry();
     }
 
     return ScaleFactor(sf);
