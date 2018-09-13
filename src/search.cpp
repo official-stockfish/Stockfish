c3b1cb86e16d3f04eb4c95ed34ee6981c7687c72
mstembera <MissingEmail@email>
2018-09-13 03:47:51 -0700

    Dynamic reduction but capped at 4. bench: 3641758

--- src/search.cpp
+++ src/search.cpp
@@ -401,10 +401,10 @@ void Thread::search() {
           // Start with a small aspiration window and, in the case of a fail
           // high/low, re-search with a bigger window until we don't fail
           // high/low anymore.
-          bool failedHigh = false;
+          int failedHighCnt = 0;
           while (true)
           {
-              Depth adjustedDepth = std::max(ONE_PLY, rootDepth - failedHigh * 3 * ONE_PLY);
+              Depth adjustedDepth = std::max(ONE_PLY, rootDepth - std::min(failedHighCnt, 4) * ONE_PLY);
               bestValue = ::search<PV>(rootPos, ss, alpha, beta, adjustedDepth, false);
 
               // Bring the best move to the front. It is critical that sorting
@@ -435,7 +435,7 @@ void Thread::search() {
               {
                   beta = (alpha + beta) / 2;
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
-                  failedHigh = false;
+                  failedHighCnt = 0;
 
                   if (mainThread)
                   {
@@ -446,7 +446,7 @@ void Thread::search() {
               else if (bestValue >= beta)
               {
                   beta = std::min(bestValue + delta, VALUE_INFINITE);
-                  failedHigh = true;
+                  ++failedHighCnt;
               }
               else
                   break;
