542a2b39ed811a3795cdce2416896ffaf2a485cf
mstembera <MissingEmail@email>
2018-10-14 12:50:31 -0700

    Small simplification in castling rights
    There is no need for a special struct with a static member
    to generate castling rights.
    
    No functional change.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -250,10 +250,10 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
   Value bonus = evaluate_shelter<Us>(pos, ksq);
 
   // If we can castle use the bonus after the castling if it is bigger
-  if (pos.can_castle(MakeCastling<Us, KING_SIDE>::right))
+  if (pos.can_castle(Us | KING_SIDE))
       bonus = std::max(bonus, evaluate_shelter<Us>(pos, relative_square(Us, SQ_G1)));
 
-  if (pos.can_castle(MakeCastling<Us, QUEEN_SIDE>::right))
+  if (pos.can_castle(Us | QUEEN_SIDE))
       bonus = std::max(bonus, evaluate_shelter<Us>(pos, relative_square(Us, SQ_C1)));
 
   return make_score(bonus, -16 * minKingPawnDistance);
--- src/types.h
+++ src/types.h
@@ -145,12 +145,6 @@ enum CastlingRight {
   CASTLING_RIGHT_NB = 16
 };
 
-template<Color C, CastlingSide S> struct MakeCastling {
-  static constexpr CastlingRight
-  right = C == WHITE ? S == QUEEN_SIDE ? WHITE_OOO : WHITE_OO
-                     : S == QUEEN_SIDE ? BLACK_OOO : BLACK_OO;
-};
-
 enum Phase {
   PHASE_ENDGAME,
   PHASE_MIDGAME = 128,
