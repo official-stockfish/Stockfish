0a645045f46bac87e5d345a1319f48575c1d0dbd
protonspring <mike@whiteley.org>
2018-10-22 07:30:33 -0600

    back to passing version.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -859,13 +859,13 @@ void init() {
 
   for (int m = 0; m < 32; ++m)
   {
-    MobilityBonus[ QUEEN][m] = make_score(-120*(1-log10(m+2.0)), -270*(1-log10(m+5.0)));
-    MobilityBonus[  ROOK][m] = make_score(-140 + 6.5*m-31,       -220*(1-log10(m+1.0)));
-    MobilityBonus[BISHOP][m] = make_score(-110*(1-log10(m+0.8)), -145*(1-log10(m+1.3)));
-    MobilityBonus[KNIGHT][m] = make_score(-120*(1-log10(m+1.0)), -160*(1-log10(m+2.0)));
+    MobilityBonus[ QUEEN][m] = make_score(52-120*(1-log10(m+2)),   48-270*(1-log10(m+5)));
+    MobilityBonus[  ROOK][m] = make_score(6.5*m-31,               142-220*(1-log10(m+1)));
+    MobilityBonus[BISHOP][m] = make_score(73-110*(1-log10(m+0.8)), 70-145*(1-log10(m+1.3)));
+    MobilityBonus[KNIGHT][m] = make_score(44-120*(1-log10(m+1)),   38-160*(1-log10(m+2)));
   }
 
-  MobilityBonus[ROOK][0] = make_score(-58, -220*(1-log10(1.0)));
+  MobilityBonus[ROOK][0] = make_score(-58, 142-220*(1-log10(1)));
 }
 
 } // namespace
