67e6ed4fb2d4e9a49df4d09a0e92445db80db185
protonspring <mike@whiteley.org>
2018-10-31 13:33:49 -0600

    +2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -634,7 +634,7 @@ namespace {
 
         if (r > RANK_3)
         {
-            int w = (r-2) * (r-2) + 3;
+            int w = (r-2) * (r-2) + 2;
             Square blockSq = s + Up;
 
             // Adjust bonus based on the king's proximity
