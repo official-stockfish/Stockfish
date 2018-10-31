4a0db9ea3c34d7663a039c40ce810ba9cb743cca
SFisGOD <jonathandumale@gmail.com>
2018-11-01 00:48:16 +0800

    Combo
    Combo of two parameter tweaks and tuned values for Queen and ThreatByKing.
    
    STC
    LLR: 2.95 (-2.94,2.94) [0.00,4.00]
    Total: 20180 W: 4439 L: 4198 D: 11543
    http://tests.stockfishchess.org/tests/view/5bd7b8250ebc595e0ae22e97
    
    LTC
    LLR: 2.95 (-2.94,2.94) [0.00,4.00]
    Total: 86312 W: 14106 L: 13685 D: 58521
    http://tests.stockfishchess.org/tests/view/5bd803560ebc595e0ae23213
    
    This combo consists of the following:
    
    Queen Value (tuned values)
    Iter: 72056, A: 5000, alpha 0.602000, gamma 0.101000, clipping old, rounding deterministic
    param: QueenValueMg, best: 2528.91, start: 2528.00
    param: QueenValueEg, best: 2687.12, start: 2698.00
    
    ThreatByKing (tuned values)
    Green STC (50.8k games)
    http://tests.stockfishchess.org/tests/view/5bd1d5a00ebc595e0ae1cbec
    LTC (I stopped this test at 71.2k games. It's likely yellow.)
    http://tests.stockfishchess.org/tests/view/5bd263e70ebc595e0ae1d77e
    
    WeakUnopposedPawn (tweak) by xoto (https://github.com/xoto10)
    Green STC (102.8k games)
    http://tests.stockfishchess.org/tests/view/5bd306bb0ebc595e0ae1e146
    Yellow LTC (90.8k games)
    http://tests.stockfishchess.org/tests/view/5bd3ea660ebc595e0ae1f16b
    
    aspiTune1 (tweak) by vondele (https://github.com/vondele)
    Green STC (125.9k games)
    http://tests.stockfishchess.org/tests/view/5bd2ae100ebc595e0ae1dab0
    Yellow LTC (107.9k games)
    http://tests.stockfishchess.org/tests/view/5bd3eb700ebc595e0ae1f16f
    
    Thank you @31m059 (Mark Tenzer) for helping me! Also, thank you very much
    for recognizing my efforts. I genuinely appreciate it.
    
    Bench: 3556672

--- src/search.cpp
+++ src/search.cpp
@@ -388,7 +388,7 @@ void Thread::search() {
           if (rootDepth >= 5 * ONE_PLY)
           {
               Value previousScore = rootMoves[pvIdx].previousScore;
-              delta = Value(18);
+              delta = Value(20);
               alpha = std::max(previousScore - delta,-VALUE_INFINITE);
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
 
