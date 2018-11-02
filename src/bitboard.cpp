171418b2f0d115b81fb099e48e3ace89762bb101
protonspring <mike@whiteley.org>
2018-11-02 12:50:20 -0600

    remove distance condition.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -113,7 +113,6 @@ void Bitboards::init() {
 
   for (Square s1 = SQ_A1; s1 <= SQ_H8; ++s1)
       for (Square s2 = SQ_A1; s2 <= SQ_H8; ++s2)
-          if (s1 != s2)
           {
               SquareDistance[s1][s2] = std::max(distance<File>(s1, s2), distance<Rank>(s1, s2));
               DistanceRingBB[s1][SquareDistance[s1][s2]] |= s2;
