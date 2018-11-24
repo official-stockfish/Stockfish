6e66e7aae28ab087ad816589c0d54851993f2e4a
31m059 <37052095+31m059@users.noreply.github.com>
2018-11-24 02:09:03 +0100

    Remove the tropism term from kingDanger
    A recent LTC tuning session by @candirufish showed this term decreasing significantly. It appears that it can be removed altogether without significant Elo loss.
    
    I also thank @GuardianRM, whose attempt to remove tropism from king danger inspired this one.
    
    After this PR is merged, my next step will be to attempt to tune the coefficients of this new, simplified kingDanger calculation.
    
    STC:
    LLR: 2.95 (-2.94,2.94) [-3.00,1.00]
    Total: 12518 W: 2795 L: 2656 D: 7067
    http://tests.stockfishchess.org/tests/view/5befadda0ebc595e0ae3a289
    
    LTC:
    LLR: 2.96 (-2.94,2.94) [-3.00,1.00]
    Total: 164771 W: 26463 L: 26566 D: 111742
    http://tests.stockfishchess.org/tests/view/5befcca70ebc595e0ae3a343
    
    LTC 2, rebased on Stockfish 10 beta:
    LLR: 2.96 (-2.94,2.94) [-3.00,1.00]
    Total: 75226 W: 12563 L: 12529 D: 50134
    http://tests.stockfishchess.org/tests/view/5bf2e8910ebc5902bcecb919
    
    Bench: 3412071

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -475,7 +475,6 @@ namespace {
                      +  69 * kingAttacksCount[Them]
                      + 185 * popcount(kingRing[Us] & weak)
                      + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
-                     +   4 * tropism
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
                      +       mg_value(mobility[Them] - mobility[Us])
