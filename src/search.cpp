ef5266733fc662a97fd60dbe32896c5d26cfc334
VoyagerOne <excelgeek@gmail.com>
2018-12-06 18:14:34 -0500

    Tweak CMH pruning - Bench: 3775064

--- src/search.cpp
+++ src/search.cpp
@@ -973,7 +973,7 @@ moves_loop: // When in check, search starts from here
               int lmrDepth = std::max(newDepth - reduction<PvNode>(improving, depth, moveCount), DEPTH_ZERO) / ONE_PLY;
 
               // Countermoves based pruning (~20 Elo)
-              if (   lmrDepth < 3 + ((ss-1)->statScore > 0)
+              if (   lmrDepth < 3 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
