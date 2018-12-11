ac2aad864a4b58d6888264e6efe3c0d8e9719ebb
protonspring <mike@whiteley.org>
2018-12-11 05:47:56 -0700

    Changes identified in RENAME/REFORMATTING thread (#1861)
    I've gone through the RENAME/REFORMATTING thread and changed everything I could find, plus a few more. With this, let's close the previous issue and open another.
    
    No functional change.

--- src/material.h
+++ src/material.h
@@ -58,7 +58,7 @@ struct Entry {
   Key key;
   const EndgameBase<Value>* evaluationFunction;
   const EndgameBase<ScaleFactor>* scalingFunction[COLOR_NB]; // Could be one for each
-                                                             // side (e.g. KPKP, KBPsKs)
+                                                             // side (e.g. KPKP, KBPsK)
   int16_t value;
   uint8_t factor[COLOR_NB];
   Phase gamePhase;
