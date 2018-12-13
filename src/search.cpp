6f7d7afcb68fca12695e9709069fbfc4c6c30bd9
31m059 <37052095+31m059@users.noreply.github.com>
2018-12-13 00:35:00 -0500

    Remove NPM threshold. Bench: 3467183

--- src/search.cpp
+++ src/search.cpp
@@ -960,7 +960,7 @@ moves_loop: // When in check, search starts from here
       {
           if (   !captureOrPromotion
               && !givesCheck
-              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material() >= Value(5000)))
+              && !pos.advanced_pawn_push(move))
           {
               // Move count based pruning (~30 Elo)
               if (moveCountPruning)
