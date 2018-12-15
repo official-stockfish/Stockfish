08d9c0d903b82bbd589e225cbca2422449533da9
Rocky640 <support@multicim.com>
2018-12-15 11:28:52 -0500

    KR2b
    Apply also to the attacking pawns.
    bench 3750757

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -273,8 +273,9 @@ namespace {
         else if (file_of(pos.square<KING>(Us)) == FILE_A)
             kingRing[Us] |= shift<EAST>(kingRing[Us]);
 
-        kingAttackersCount[Them] = popcount(kingRing[Us] & pe->pawn_attacks(Them));
         kingRing[Us] &= ~double_pawn_attacks_bb<Us>(pos.pieces(Us, PAWN));
+
+        kingAttackersCount[Them] = popcount(kingRing[Us] & pe->pawn_attacks(Them));
         kingAttacksCount[Them] = kingAttackersWeight[Them] = 0;
     }
   }
