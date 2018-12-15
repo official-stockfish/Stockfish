1c5c3c34d323a374111e895ed9e1846eb0f48f10
Rocky640 <support@multicim.com>
2018-12-15 08:48:08 -0500

    KRS-2
    Fixed code
    Run as a simplification, since it reduces the number of processor instructions (line 313 is called for each piece)
    
    bench: 3332460

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -274,7 +274,7 @@ namespace {
             kingRing[Us] |= shift<EAST>(kingRing[Us]);
 
         kingAttackersCount[Them] = popcount(kingRing[Us] & pe->pawn_attacks(Them));
-        kingRing[Us] &= ~double_pawn_attacks_bb<Them>(pos.pieces(Them, PAWN))
+        kingRing[Us] &= ~double_pawn_attacks_bb<Us>(pos.pieces(Us, PAWN));
         kingAttacksCount[Them] = kingAttackersWeight[Them] = 0;
     }
   }
