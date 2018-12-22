8fce5e3c61d858a776bb24dddaa45f768f476c70
xoto10 <buylow001@gmail.com>
2018-12-22 03:43:12 +0000

    remove voting constant. On one thread no functional change

--- src/search.cpp
+++ src/search.cpp
@@ -267,8 +267,8 @@ void MainThread::search() {
       // Vote according to score and depth
       auto square = [](int64_t x) { return x * x; };
       for (Thread* th : Threads)
-          votes[th->rootMoves[0].pv[0]] += 200 + (square(th->rootMoves[0].score - minScore + 1)
-                                                  * int64_t(th->completedDepth));
+          votes[th->rootMoves[0].pv[0]] += (square(th->rootMoves[0].score - minScore + 1)
+                                            * int64_t(th->completedDepth));
 
       // Select best thread
       int64_t bestVote = votes[this->rootMoves[0].pv[0]];
