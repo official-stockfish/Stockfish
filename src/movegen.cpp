59d32f8eddf4fc8138f419777fbabf58080f1caa
protonspring <mike@whiteley.org>
2018-12-23 08:05:24 -0700

    Simplify generate_castling (#1885)
    Although this is a compile-time constant, we stick the castlingSide into a CastlingRight, then pull it out again. This seems unecessarily complex.
    
    No functional change.

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -29,7 +29,7 @@ namespace {
   ExtMove* generate_castling(const Position& pos, ExtMove* moveList) {
 
     constexpr CastlingRight Cr = Us | Cs;
-    constexpr bool KingSide = (Cr == WHITE_OO || Cr == BLACK_OO);
+    constexpr bool KingSide = (Cs == KING_SIDE);
 
     if (pos.castling_impeded(Cr) || !pos.can_castle(Cr))
         return moveList;
