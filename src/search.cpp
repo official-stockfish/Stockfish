f013904ac277cdf7fbe39bc92f939cd1f0f23a8c
Marco Costalba <mcostalba@gmail.com>
2019-01-02 10:01:51 +0100

    Revert "Simplify thread scoring"
    This reverts commit ded28a7ab5fda3e4f5975df1f369bd05e5ec3129.
    
    @vondele noted that the precise value of votes is important now.
    Two threads that vote for a move with 10cp advantage is better
    than one vote with 13cp advantage.
    
    Note that we don't need to zero out an already empty map.
    
    No functional change.

--- src/search.cpp
+++ src/search.cpp
@@ -255,11 +255,16 @@ void MainThread::search() {
       &&  rootMoves[0].pv[0] != MOVE_NONE)
   {
       std::map<Move, int64_t> votes;
+      Value minScore = this->rootMoves[0].score;
+
+      // Find out minimum score and reset votes for moves which can be voted
+      for (Thread* th: Threads)
+          minScore = std::min(minScore, th->rootMoves[0].score);
 
       // Vote according to score and depth
       for (Thread* th : Threads)
       {
-          int64_t s = th->rootMoves[0].score + VALUE_INFINITE + 1; // Ensure positive
+          int64_t s = th->rootMoves[0].score - minScore + 1;
           votes[th->rootMoves[0].pv[0]] += 200 + s * s * int(th->completedDepth);
       }
 
