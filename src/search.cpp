83bb5750992a67a29864aca27cc339ebd7bbebaf
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-01-09 14:16:24 +0100

    Remove early pruning.
    Bench 3399866

--- src/search.cpp
+++ src/search.cpp
@@ -776,9 +776,6 @@ namespace {
     improving =   ss->staticEval >= (ss-2)->staticEval
                || (ss-2)->staticEval == VALUE_NONE;
 
-    if (pvHit)
-        goto moves_loop;
-
     // Step 8. Futility pruning: child node (~30 Elo)
     if (   !PvNode
         &&  depth < 7 * ONE_PLY
