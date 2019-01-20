3acacf8471efd018e33e4743cbbb2aed8d59e855
Jonathan D <38142170+SFisGOD@users.noreply.github.com>
2019-01-20 19:20:21 +0800

    Tweak initiative and Pawn PSQT (#1957)
    Small changes in initiative(). For Pawn PSQT, endgame values for d6-e6 and d7-e7 are now symmetric. The MG value of d2 is now smaller than e2 (d2=13, e2=21 now compared to d2=19, e2=16 before). The MG values of h5-h6-h7 also increased so this might encourage stockfish for more h-pawn pushes.
    
    STC
    LLR: -2.96 (-2.94,2.94) [0.00,4.00]
    Total: 81141 W: 17933 L: 17777 D: 45431
    http://tests.stockfishchess.org/tests/view/5c4017350ebc5902bb5cf237
    
    LTC
    LLR: 2.96 (-2.94,2.94) [0.00,4.00]
    Total: 83078 W: 13883 L: 13466 D: 55729
    http://tests.stockfishchess.org/tests/view/5c40763f0ebc5902bb5cff09
    
    Bench: 3266398

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -743,12 +743,12 @@ namespace {
                             && (pos.pieces(PAWN) & KingSide);
 
     // Compute the initiative bonus for the attacking side
-    int complexity =   8 * pe->pawn_asymmetry()
-                    + 12 * pos.count<PAWN>()
-                    + 12 * outflanking
-                    + 16 * pawnsOnBothFlanks
-                    + 48 * !pos.non_pawn_material()
-                    -118 ;
+    int complexity =   9 * pe->pawn_asymmetry()
+                    + 11 * pos.count<PAWN>()
+                    +  9 * outflanking
+                    + 18 * pawnsOnBothFlanks
+                    + 49 * !pos.non_pawn_material()
+                    -121 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
