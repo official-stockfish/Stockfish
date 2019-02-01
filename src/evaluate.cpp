ff97a9fdb936a73de774df062f35d454a4a9af00
Stéphane Nicolet <stephanenicoletsuriphone@gmail.com>
2019-02-01 13:09:17 +0100

    Tweak tropism weight in king danger
    There was a simplification attempt last week for the tropism
    term in king danger, which passed STC but failed LTC. This
    was an indirect sign that maybe the tropism factor was sightly
    untuned in current master, so we tried to change it from 1/4
    to 5/16.
    
    STC:
    LLR: 2.95 (-2.94,2.94) [0.00,4.00]
    Total: 28098 W: 6264 L: 5990 D: 15844
    http://tests.stockfishchess.org/tests/view/5c518db60ebc593af5d4e306
    
    LTC:
    LLR: 2.95 (-2.94,2.94) [0.00,3.50]
    Total: 103709 W: 17387 L: 16923 D: 69399
    http://tests.stockfishchess.org/tests/view/5c52a5510ebc592fc7baea8b
    
    Bench: 4016000

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -483,7 +483,7 @@ namespace {
                  +  69 * kingAttacksCount[Them]
                  + 185 * popcount(kingRing[Us] & weak)
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
-                 +       tropism * tropism / 4
+                 +   5 * tropism * tropism / 16
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
