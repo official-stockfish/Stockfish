76d2f5b94a0df20d84ccf922bd1c0fcf1c779090
protonspring <mike@whiteley.org>
2019-02-14 14:23:24 -0700

    Remove skipQuiets variable in search()
    This is a functional simplification. The moveCountPruning variable and the
    skipQuiets variable are similar enough in function that they can be combined.
    This removes the skipQuiets variable in search.
    
    STC
    LLR: 2.96 (-2.94,2.94) [-3.00,1.00]
    Total: 23278 W: 5210 L: 5091 D: 12977
    http://tests.stockfishchess.org/tests/view/5c65dc490ebc5925cffc12e9
    
    LTC
    LLR: 2.95 (-2.94,2.94) [-3.00,1.00]
    Total: 77107 W: 12792 L: 12761 D: 51554
    http://tests.stockfishchess.org/tests/view/5c65e4360ebc5925cffc1490
    
    Closes https://github.com/official-stockfish/Stockfish/pull/2011
    
    bench 3640330

--- src/search.cpp
+++ src/search.cpp
@@ -570,7 +570,7 @@ namespace {
     Depth extension, newDepth;
     Value bestValue, value, ttValue, eval, maxValue, pureStaticEval;
     bool ttHit, ttPv, inCheck, givesCheck, improving;
-    bool captureOrPromotion, doFullDepthSearch, moveCountPruning, skipQuiets, ttCapture;
+    bool captureOrPromotion, doFullDepthSearch, moveCountPruning, ttCapture;
     Piece movedPiece;
     int moveCount, captureCount, quietCount;
 
@@ -880,12 +880,12 @@ moves_loop: // When in check, search starts from here
                                       ss->killers);
     value = bestValue; // Workaround a bogus 'uninitialized' warning under gcc
 
-    skipQuiets = false;
+    moveCountPruning = false;
     ttCapture = ttMove && pos.capture_or_promotion(ttMove);
 
     // Step 12. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
-    while ((move = mp.next_move(skipQuiets)) != MOVE_NONE)
+    while ((move = mp.next_move(moveCountPruning)) != MOVE_NONE)
     {
       assert(is_ok(move));
 
@@ -914,8 +914,9 @@ moves_loop: // When in check, search starts from here
       movedPiece = pos.moved_piece(move);
       givesCheck = gives_check(pos, move);
 
-      moveCountPruning =   depth < 16 * ONE_PLY
-                        && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY];
+      // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
+      moveCountPruning = depth < 16 * ONE_PLY
+                      && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY];
 
       // Step 13. Extensions (~70 Elo)
 
@@ -973,10 +974,7 @@ moves_loop: // When in check, search starts from here
           {
               // Move count based pruning (~30 Elo)
               if (moveCountPruning)
-              {
-                  skipQuiets = true;
                   continue;
-              }
 
               // Reduced depth of the next LMR search
               int lmrDepth = std::max(newDepth - reduction<PvNode>(improving, depth, moveCount), DEPTH_ZERO) / ONE_PLY;
