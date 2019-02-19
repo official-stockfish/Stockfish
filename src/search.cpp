e51244cb27151075ac7a05282d0ec615cf93f290
MJZ1977 <37274752+MJZ1977@users.noreply.github.com>
2019-02-19 17:48:03 +0100

    ProbCutCount limit dependancy to cutNode
    Use the ProbCutCount limit `2 + 2 * cutNode` instead of constant 3.
    
    STC
    LLR: -2.95 (-2.94,2.94) [0.50,4.50]
    Total: 61812 W: 13599 L: 13459 D: 34754
    http://tests.stockfishchess.org/tests/view/5c6d19240ebc5925cffca07a
    
    LTC
    LLR: 2.96 (-2.94,2.94) [0.00,3.50]
    Total: 27549 W: 4614 L: 4363 D: 18572
    http://tests.stockfishchess.org/tests/view/5c6d45c10ebc5925cffca7a6
    
    Closes https://github.com/official-stockfish/Stockfish/pull/2015
    
    Bench: 3368889

--- src/search.cpp
+++ src/search.cpp
@@ -830,7 +830,7 @@ namespace {
         int probCutCount = 0;
 
         while (  (move = mp.next_move()) != MOVE_NONE
-               && probCutCount < 3)
+                && probCutCount < 2 + 2 * cutNode)
             if (move != excludedMove && pos.legal(move))
             {
                 probCutCount++;
