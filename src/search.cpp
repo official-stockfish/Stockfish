d320de761951687fa55716da1bd786df4c2cb14b
xoto10 <buylow001@gmail.com>
2019-03-10 21:57:48 +0000

    Remove !extension check #2045
    While looking at pruning using see_ge() (which is very valuable)
    it became apparent that the !extension test is not adding any
    value - simplify it away.
    
    STC:
    LLR: 2.96 (-2.94,2.94) [-3.00,1.00]
    Total: 56843 W: 12621 L: 12569 D: 31653
    http://tests.stockfishchess.org/tests/view/5c8588cb0ebc5925cffe77f4
    
    LTC:
    LLR: 2.96 (-2.94,2.94) [-3.00,1.00]
    Total: 78622 W: 13223 L: 13195 D: 52204
    http://tests.stockfishchess.org/tests/view/5c8611cc0ebc5925cffe7f86
    
    Further work could be to optimize the remaining see_ge() test. The idea of less pruning at higher depths is valuable, but perhaps the test (-PawnValueEg * depth) can be improved.
    
    Bench: 3188688

--- src/search.cpp
+++ src/search.cpp
@@ -970,8 +970,7 @@ moves_loop: // When in check, search starts from here
               if (!pos.see_ge(move, Value(-29 * lmrDepth * lmrDepth)))
                   continue;
           }
-          else if (   !extension // (~20 Elo)
-                   && !pos.see_ge(move, -PawnValueEg * (depth / ONE_PLY)))
+          else if (!pos.see_ge(move, -PawnValueEg * (depth / ONE_PLY))) // (~20 Elo)
                   continue;
       }
 
