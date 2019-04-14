b0796d957e13e66e0a4eb3d443d4fa591b1b9669
protonspring <mike@whiteley.org>
2019-04-14 17:30:39 -0600

    remove PawnsOnSquares.

--- src/position.h
+++ src/position.h
@@ -128,6 +128,7 @@ public:
   // Piece specific
   bool pawn_passed(Color c, Square s) const;
   bool opposite_bishops() const;
+  int  pawns_on_same_color_squares(Color c, Square s) const;
 
   // Doing and undoing moves
   void do_move(Move m, StateInfo& newSt);
@@ -311,6 +312,10 @@ inline Bitboard Position::check_squares(PieceType pt) const {
 
 inline bool Position::pawn_passed(Color c, Square s) const {
   return !(pieces(~c, PAWN) & passed_pawn_span(c, s));
+}
+
+inline int Position::pawns_on_same_color_squares(Color c, Square s) const {
+  return popcount(pieces(c, PAWN) & ((DarkSquares & s) ? DarkSquares : ~DarkSquares));
 }
 
 inline bool Position::advanced_pawn_push(Move m) const {
