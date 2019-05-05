8ebe2dfab0cb1c5a0f3673e44b4e70fe13e4dd90
xoto10 <buylow001@gmail.com>
2019-05-05 14:23:37 +0100

    Remove pawn count from space with compensation, -1. Bench 2849754

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -719,7 +719,7 @@ namespace {
     behind |= shift<Down+Down>(behind);
 
     int bonus = popcount(safe) + popcount(behind & safe);
-    int weight =  pos.count<ALL_PIECES>(Us) - 4;
+    int weight =  pos.count<ALL_PIECES>(Us) - 1;
     Score score = make_score(bonus * weight * weight / 16, 0);
 
     if (T)
