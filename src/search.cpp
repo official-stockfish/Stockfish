743d2b1df56be62b80a1a53ef451c3824c7b2ed4
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-04 09:07:34 +0300

    tviig. bench 3584443

--- src/search.cpp
+++ src/search.cpp
@@ -958,7 +958,7 @@ moves_loop: // When in check, search starts from here
 
           if (   !captureOrPromotion
               && !givesCheck
-              && !pos.advanced_pawn_push(move))
+              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material(~us) > BishopValueMg))
           {
               // Move count based pruning (~30 Elo)
               if (moveCountPruning)
