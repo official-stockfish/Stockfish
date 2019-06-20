f3b483b890d75ea55158c125e916971ac90bf8bb
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-21 01:02:19 +0300

    tviick. bench 3541900

--- src/search.cpp
+++ src/search.cpp
@@ -1021,9 +1021,6 @@ moves_loop: // When in check, search starts from here
           if ((ss-1)->moveCount > 15)
               r -= ONE_PLY;
 
-          if (!PvNode && moveCount > 15)
-              r += ONE_PLY;
-
           // Decrease reduction if move has been singularly extended
           r -= singularLMR * ONE_PLY;
 
@@ -1035,7 +1032,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
-                  r += 2 * ONE_PLY;
+                  r += (1 + (moveCount > 15)) * ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
