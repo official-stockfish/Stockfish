653b7c3c83507cc38818e63039a0506efe2052fe
protonspring <mike@whiteley.org>
2019-07-06 08:08:08 -0600

    simplify move_importance.

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -37,18 +37,12 @@ namespace {
   constexpr double StealRatio = 0.34; // However we must not steal time from remaining moves over this ratio
 
 
-  // move_importance() is a skew-logistic function based on naive statistical
-  // analysis of "how many games are still undecided after n half-moves". Game
-  // is considered "undecided" as long as neither side has >275cp advantage.
-  // Data was extracted from the CCRL game database with some simple filtering criteria.
-
+  // move_importance() is a sigmoid for adjusting time allocation for a move.
+  // The aim is to allocate more time for earlier moves.
   double move_importance(int ply) {
 
-    constexpr double XScale = 6.85;
-    constexpr double XShift = 64.5;
-    constexpr double Skew   = 0.171;
-
-    return pow((1 + exp((ply - XShift) / XScale)), -Skew) + DBL_MIN; // Ensure non-zero
+    int p = ply - 88;
+    return 0.6 - 0.6 * p / std::sqrt(p * p + 1936);
   }
 
   template<TimeType T>
