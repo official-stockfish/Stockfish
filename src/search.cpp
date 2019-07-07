5a7827d59de5e3dea0d90bdb3e7c57153c0a9f1f
xoto10 <buylow001@gmail.com>
2019-07-07 02:20:49 +0100

    Combo of statscore divisor and pawn psqt changes
    Passed STC 10+0.1 th 1:
    LLR: 2.96 (-2.94,2.94) [0.00,4.00]
    Total: 13282 W: 3100 L: 2881 D: 7301
    http://tests.stockfishchess.org/tests/view/5d21132e0ebc5925cf0c81f4
    
    Passed LTC 60+0.6 th 1:
    LLR: 2.95 (-2.94,2.94) [0.00,4.00]
    Total: 44243 W: 7768 L: 7468 D: 29007
    http://tests.stockfishchess.org/tests/view/5d2119050ebc5925cf0c832b
    
    Bench 3705891

--- src/search.cpp
+++ src/search.cpp
@@ -1115,7 +1115,7 @@ moves_loop: // When in check, search starts from here
                   r += ONE_PLY;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-              r -= ss->statScore / 20000 * ONE_PLY;
+              r -= ss->statScore / 16384 * ONE_PLY;
           }
 
           Depth d = clamp(newDepth - r, ONE_PLY, newDepth);
