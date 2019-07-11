e2165155d11d5456e6e256d43e00a444a2057246
Hisayori Noda <nodchip@gmail.com>
2019-07-11 22:47:55 +0900

    Enabled halfkp_256x2-32-32.h.

--- src/eval/nnue/nnue_architecture.h
+++ src/eval/nnue/nnue_architecture.h
@@ -6,10 +6,10 @@
 #if defined(EVAL_NNUE)
 
 // 入力特徴量とネットワーク構造が定義されたヘッダをincludeする
-#include "architectures/k-p_256x2-32-32.h"
+//#include "architectures/k-p_256x2-32-32.h"
 //#include "architectures/k-p-cr_256x2-32-32.h"
 //#include "architectures/k-p-cr-ep_256x2-32-32.h"
-//#include "architectures/halfkp_256x2-32-32.h"
+#include "architectures/halfkp_256x2-32-32.h"
 //#include "architectures/halfkp-cr-ep_256x2-32-32.h"
 
 namespace Eval {
