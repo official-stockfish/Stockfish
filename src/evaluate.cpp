0650f46ea709ed45c4070b905a9c245e9cdf478f
protonspring <mike@whiteley.org>
2019-07-19 15:48:39 -0600

    linear version.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -126,12 +126,6 @@ namespace {
     S(0, 0), S(5, 18), S(12, 23), S(10, 31), S(57, 62), S(163, 167), S(271, 250)
   };
 
-  // PassedFile[File] contains a bonus according to the file of a passed pawn
-  constexpr Score PassedFile[FILE_NB] = {
-    S( -1,  7), S( 0,  9), S(-9, -8), S(-30,-14),
-    S(-30,-14), S(-9, -8), S( 0,  9), S( -1,  7)
-  };
-
   // Assorted bonuses and penalties
   constexpr Score AttacksOnSpaceArea = S(  4,  0);
   constexpr Score BishopPawns        = S(  3,  7);
@@ -616,7 +610,8 @@ namespace {
 
         assert(!(pos.pieces(Them, PAWN) & forward_file_bb(Us, s + Up)));
 
-        int r = relative_rank(Us, s);
+        Rank r = relative_rank(Us, s);
+        File f = file_of(s);
 
         Score bonus = PassedRank[r];
 
@@ -666,7 +661,8 @@ namespace {
             || (pos.pieces(PAWN) & (s + Up)))
             bonus = bonus / 2;
 
-        score += bonus + PassedFile[file_of(s)];
+        int d = std::min(f, ~f);  //File based bonus/penalty
+        score += bonus + make_score(5 - 11 * d, 10 - 8 * d);
     }
 
     if (T)
