6c5a7cca7c8959799eed79e1d295bfee6f7301a8
protonspring <mike@whiteley.org>
2019-08-22 08:32:41 -0600

    adjust logic in search.

--- src/search.cpp
+++ src/search.cpp
@@ -1046,7 +1046,7 @@ moves_loop: // When in check, search starts from here
               if (!pos.see_ge(move, Value(-(31 - std::min(lmrDepth, 18)) * lmrDepth * lmrDepth)))
                   continue;
           }
-          else if (  (!givesCheck || !extension)
+          else if (  !(givesCheck && extension)
                    && !pos.see_ge(move, Value(-199) * (depth / ONE_PLY))) // (~20 Elo)
                   continue;
       }
