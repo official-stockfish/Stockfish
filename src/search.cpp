f0795630c73389b53e7456e44e1987abe4899baf
protonspring <mike@whiteley.org>
2019-08-22 08:31:48 -0600

    update comment.

--- src/search.cpp
+++ src/search.cpp
@@ -1091,7 +1091,7 @@ moves_loop: // When in check, search starts from here
           if ((ss-1)->moveCount > 15)
               r -= ONE_PLY;
 
-          // Decrease reduction if move has been singularly extended
+          // Decrease reduction if ttMove has been singularly extended
           r -= singularLMR * ONE_PLY;
 
           if (!captureOrPromotion)
