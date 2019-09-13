e8d84ad830f15b36e032a112906c5b528376e9d0
31m059 <37052095+31m059@users.noreply.github.com>
2019-09-13 15:46:05 -0400

    Use queens of either color in RookOnQueenFile. Bench: 3703909

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -347,8 +347,8 @@ namespace {
             if (relative_rank(Us, s) >= RANK_5)
                 score += RookOnPawn * popcount(pos.pieces(Them, PAWN) & PseudoAttacks[ROOK][s]);
 
-            // Bonus for rook on same file as their queen
-            if (file_bb(s) & pos.pieces(Them, QUEEN))
+            // Bonus for rook on the same file as a queen
+            if (file_bb(s) & pos.pieces(QUEEN))
                 score += RookOnQueenFile;
 
             // Bonus for rook on an open or semi-open file
