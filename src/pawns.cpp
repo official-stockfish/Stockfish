328bdd0947ea7903ee1061c8eb60bc3121a4eb19
Stéphane Nicolet <cassio@free.fr>
2019-10-05 11:15:24 +0200

    Fix compare function in previous patch
    Bench: 4012371

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -231,7 +231,7 @@ Score Entry::do_king_safety(const Position& pos) {
   Square ksq = pos.square<KING>(Us);
   kingSquares[Us] = ksq;
   castlingRights[Us] = pos.castling_rights(Us);
-  auto compare = [](Score a, Score b) { return mg_value(a) <= mg_value(b); };
+  auto compare = [](Score a, Score b) { return mg_value(a) < mg_value(b); };
 
   Score shelter = evaluate_shelter<Us>(pos, ksq);
 
