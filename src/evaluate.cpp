0b0b21c608a9c096af6155e39270a29ebfba240f
SFisGOD <jonathandumale@gmail.com>
2019-10-07 12:30:57 +0800

    Tweak kingFlankAttacks factor in kingDanger
    Increase kingFlankAttacks factor in kingDanger from 5/16 to 6/16.
    
    Failed STC:
    LLR: -2.96 (-2.94,2.94) [0.00,4.00]
    Total: 77947 W: 16989 L: 16848 D: 44110
    http://tests.stockfishchess.org/tests/view/5d9ac0280ebc5902b6cf63cd
    
    Passed LTC 1:
    LLR: 2.96 (-2.94,2.94) [0.00,4.00]
    Total: 13443 W: 2231 L: 2037 D: 9175
    http://tests.stockfishchess.org/tests/view/5d9ac88d0ebc5902b6cf6ffb
    
    Passed LTC 2:
    LLR: 2.96 (-2.94,2.94) [0.00,4.00]
    Total: 23340 W: 3842 L: 3617 D: 15881
    http://tests.stockfishchess.org/tests/view/5d9acf7f0ebc5902b6cf7c27
    
    Closes https://github.com/official-stockfish/Stockfish/pull/2349
    
    Bench: 4042155

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -457,7 +457,7 @@ namespace {
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
-                 +   5 * kingFlankAttacks * kingFlankAttacks / 16
+                 +   3 * kingFlankAttacks * kingFlankAttacks / 8
                  -   7;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
