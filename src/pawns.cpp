30aa4cd0fedff98128b29628fc7f8ab4d3f5fd62
protonspring <mike@whiteley.org>
2019-10-12 10:15:35 -0600

    v3

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -88,10 +88,9 @@ namespace {
 
     e->passedPawns[Us] = 0;
     e->kingSquares[Us] = SQ_NONE;
-    e->pawnAttacks[Us] = pawn_attacks_bb<Us>(ourPawns);
     e->outpostSquares[Them] = TheirOutpostRanks
-                            & pawn_attacks_bb<Them>(theirPawns)
-                            & ~e->pawnAttacks[Us];
+                  & pawn_attacks_bb<Them>(theirPawns)
+                  & ~(e->pawnAttacks[Us] = pawn_attacks_bb<Us>(ourPawns));
 
     // Loop through all pawns of the current color and score each pawn
     while ((s = *pl++) != SQ_NONE)
