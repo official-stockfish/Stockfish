0256416bb7b3ba7e96a487062104a9379c0c3a82
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2019-11-12 16:12:09 +0100

    Remove unneeded & incorrect check.
    the removed line is not needed, since with the conditions on SE, eval
    equals ttValue (except inCheck), which must be larger than beta if the second condition
    is true.
    
    The removed line is also incorrect as eval might be VALUE_NONE at this
    location if inCheck. This removal addresses part of https://github.com/official-stockfish/Stockfish/pull/2406#issuecomment-552642608
    
    No functional change.

--- src/search.cpp
+++ src/search.cpp
@@ -1033,8 +1033,7 @@ moves_loop: // When in check, search starts from here
           // search without the ttMove. So we assume this expected Cut-node is not singular,
           // that multiple moves fail high, and we can prune the whole subtree by returning
           // a soft bound.
-          else if (   eval >= beta
-                   && singularBeta >= beta)
+          else if (singularBeta >= beta)
               return singularBeta;
       }
 
