1fdf1f1ff5800bd8b1b1c33fc281b9731d40583d
SFisGOD <jonathandumale@gmail.com>
2019-11-21 03:31:23 +0800

    Simplify endgame factor for opposite colored bishops
    Stockfish is continually improving. Patches that gain elo in the past may
    no longer be needed as stockfish improved elsewhere. This patch removes
    passed pawns count dependence in opposite colored bishops scale factor.
    We used the mean of passed count pawns (~1.4) to compensate, and changed
    the base value from 16 to 22.
    
    Passed STC:
    LLR: 2.95 (-2.94,2.94) [-3.00,1.00]
    Total: 57879 W: 12657 L: 12607 D: 32615
    http://tests.stockfishchess.org/tests/view/5dd1644f42928ff08153dc1e
    
    Passed LTC:
    LLR: 2.95 (-2.94,2.94) [-3.00,1.00]
    Total: 121648 W: 19622 L: 19659 D: 82367
    http://tests.stockfishchess.org/tests/view/5dd24572ccb823d41d4b47bb
    
    Closes https://github.com/official-stockfish/Stockfish/pull/2419
    
    Bench: 5067864

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -748,7 +748,7 @@ namespace {
     {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material() == 2 * BishopValueMg)
-            sf = 16 + 4 * pe->passed_count();
+            sf = 22 ;
         else
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
 
