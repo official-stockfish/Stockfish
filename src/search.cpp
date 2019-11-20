63d7c0fb2ad1c2bda8bf54605c0bef5df5e5b3cb
Vizvezdenec <Vizvezdenec@gmail.com>
2019-11-20 23:20:20 +0300

    tweak. bench 5183523

--- src/search.cpp
+++ src/search.cpp
@@ -1084,7 +1084,8 @@ moves_loop: // When in check, search starts from here
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
-              || cutNode))
+              || cutNode
+              || thisThread->ttHitAverage < 3 * ttHitAverageResolution * ttHitAverageWindow / 8))
       {
           Depth r = reduction(improving, depth, moveCount);
 
