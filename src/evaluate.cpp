0a4d5bb46f7b83e349584d96a652914b584a71c5
Alain SAVARD <support@multicim.com>
2019-11-20 09:35:19 -0500

    oEG2-b
    cosmetic change after merge with latest master
    bench: 5103360

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -292,7 +292,7 @@ namespace {
         {
             // Bonus if piece is on an outpost square or can reach one
             bb = OutpostRanks & attackedBy[Us][PAWN] & ~pe->pawn_attacks_span(Them);
-            if (s & bb)
+            if (bb & s)
                 score += Outpost * (Pt == KNIGHT ? 2 : 1);
 
             else if (Pt == KNIGHT && bb & b & ~pos.pieces(Us))
