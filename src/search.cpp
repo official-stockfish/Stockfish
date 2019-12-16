c81c098d34ae49cb9421593e581d60ffe2a77d30
pb00067 <pb00067@PHXL0356.wp.lan>
2019-12-16 11:48:02 +0100

    Remove improving parameter from nmp-condition,
    as it is true about 90% of cases when the rest of condition is satisfied
    
    STC:
    https://tests.stockfishchess.org/tests/view/5df34ce36932658fe9b45132
    LLR: 2.96 (-2.94,2.94) [-3.00,1.00]
    Total: 94916 W: 20642 L: 20664 D: 53610
    
    LTC:
    https://tests.stockfishchess.org/tests/view/5df37bb26932658fe9b4513e
    LLR: 2.96 (-2.94,2.94) [-3.00,1.00]
    Total: 53531 W: 8652 L: 8582 D: 36297
    
    bench: 4957524

--- src/search.cpp
+++ src/search.cpp
@@ -828,7 +828,7 @@ namespace {
         && (ss-1)->statScore < 22661
         &&  eval >= beta
         &&  eval >= ss->staticEval
-        &&  ss->staticEval >= beta - 33 * depth + 299 - improving * 30
+        &&  ss->staticEval >= beta - 33 * depth + 272
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))
