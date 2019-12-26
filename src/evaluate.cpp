6f8ad34ef54dfaee216446fb27367a4371b09622
Chess13234 <37609326+Chess13234@users.noreply.github.com>
2019-12-26 18:52:43 +0200

    Fix UCI command eval
    Avoid lazy eval from UCI, which should not be used anyway(lazy eval makes sense only within context of search).
    Bench:5184081

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -786,10 +786,11 @@ namespace {
 
     // Early exit if score is high
     Value v = (mg_value(score) + eg_value(score)) / 2;
+    if(!T){//Fix for UCI command 'eval';
      if ( (pos.this_thread()->bestMoveChanges < 13)
        && (abs(v) > LazyThreshold + pos.non_pawn_material() / 64))
        return pos.side_to_move() == WHITE ? v : -v;
-
+    }
     // Main evaluation begins here
 
     initialize<WHITE>();
