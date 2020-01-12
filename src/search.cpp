4901218d4cc31658942486ccd1dbadf2d2df783a
Vizvezdenec <Vizvezdenec@gmail.com>
2020-01-13 02:59:06 +0300

    Tweak futility pruning constants
    Based on recent improvement of futility pruning by @locutus2 : we lower
    the futility margin to apply it for more nodes but as a compensation
    we also lower the history threshold to apply it to less nodes. Further
    work in tweaking constants can always be done - numbers are guessed
    "by hand" and are not results of some tuning, maybe there is some more
    Elo to squeeze from this part of code.
    
    Passed STC
    LLR: 2.98 (-2.94,2.94) {-1.00,3.00}
    Total: 15300 W: 3081 L: 2936 D: 9283
    Ptnml(0-2): 260, 1816, 3382, 1900, 290
    http://tests.stockfishchess.org/tests/view/5e18da3b27dab692fcf9a158
    
    Passed LTC
    LLR: 2.94 (-2.94,2.94) {0.00,2.00}
    Total: 108670 W: 14509 L: 14070 D: 80091
    Ptnml(0-2): 813, 10259, 31736, 10665, 831
    http://tests.stockfishchess.org/tests/view/5e18fc9627dab692fcf9a180
    
    Bench: 4643972

--- src/search.cpp
+++ src/search.cpp
@@ -1016,11 +1016,11 @@ moves_loop: // When in check, search starts from here
               // Futility pruning: parent node (~5 Elo)
               if (   lmrDepth < 6
                   && !inCheck
-                  && ss->staticEval + 255 + 182 * lmrDepth <= alpha
+                  && ss->staticEval + 235 + 172 * lmrDepth <= alpha
                   &&  thisThread->mainHistory[us][from_to(move)]
                     + (*contHist[0])[movedPiece][to_sq(move)]
                     + (*contHist[1])[movedPiece][to_sq(move)]
-                    + (*contHist[3])[movedPiece][to_sq(move)] < 30000)
+                    + (*contHist[3])[movedPiece][to_sq(move)] < 25000)
                   continue;
 
               // Prune moves with negative SEE (~20 Elo)
