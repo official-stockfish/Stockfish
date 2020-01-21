7f1b08e559b78dd0a54bf65c044d5d7af1e69116
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-01-21 09:28:58 +0100

    Less reduction for esacpe moves at ttPv nodes. Bench: 5223157

--- src/search.cpp
+++ src/search.cpp
@@ -1160,7 +1160,7 @@ moves_loop: // When in check, search starts from here
               // hence break make_move(). (~2 Elo)
               else if (    type_of(move) == NORMAL
                        && !pos.see_ge(reverse_move(move)))
-                  r -= 2;
+                  r -= 2 + ttPv;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
