e67c67cd1ecd73c16a90920c686f1e854505b461
Chess13234 <37609326+Chess13234@users.noreply.github.com>
2020-01-26 18:36:55 +0200

    Macro magic
    Make dbg_hit/mean include exact location and arguments used.

--- src/misc.h
+++ src/misc.h
@@ -26,17 +26,65 @@
 #include <ostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <atomic>
 
 #include "types.h"
+#include "uci.h" // for testing uci variables.
+
+// get number of arguments with __NARG__
+#define __NARG__(...)  __NARG_I_(__VA_ARGS__,__RSEQ_N())
+#define __NARG_I_(...) __ARG_N(__VA_ARGS__)
+#define __ARG_N( \
+      _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
+     _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
+     _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
+     _31,_32,_33,_34,_35,_36,_37,_38,_39,_40, \
+     _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
+     _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
+     _61,_62,_63,N,...) N
+#define __RSEQ_N() \
+     63,62,61,60,                   \
+     59,58,57,56,55,54,53,52,51,50, \
+     49,48,47,46,45,44,43,42,41,40, \
+     39,38,37,36,35,34,33,32,31,30, \
+     29,28,27,26,25,24,23,22,21,20, \
+     19,18,17,16,15,14,13,12,11,10, \
+     9,8,7,6,5,4,3,2,1,0
+
+// general definition for any function name
+#define _VFUNC_(name, n) name##n
+#define _VFUNC(name, n) _VFUNC_(name, n)
+#define VFUNC(func, ...) _VFUNC(func, __NARG__(__VA_ARGS__)) (__VA_ARGS__)
+
+
+#define stringify2(x) #x
+#define stringify(x) stringify2(x)
+#define stringifyany1(a) stringify(a)
+#define stringifyany2(a,b) stringify(a) "," stringify(b) 
+#define stringifyany(...) VFUNC(stringifyany,__VA_ARGS__) 
+  
+typedef const char*    loc_file_line; 
+
+typedef std::atomic<int64_t> int64StoreAtomic[2]; 
+typedef std::pair< loc_file_line , int64StoreAtomic   > Atomic64MapItem;
+typedef std::map < loc_file_line , int64StoreAtomic   > Atomic64Map;
+
+extern Atomic64Map dbg_print_hits;
+extern Atomic64Map dbg_print_means;
+#define dbg_hit_on(...) dbg_hit_on_impl(__FILE__ ":" stringify(__LINE__) " : dbg_hit_on(" stringifyany(__VA_ARGS__)  ")" ,__VA_ARGS__)
+#define dbg_mean_of(...)  dbg_mean_of_impl(__FILE__ ":" stringify(__LINE__) " : dbg_mean_of(" stringify(__VA_ARGS__)  ")" ,__VA_ARGS__)
+
 
 const std::string engine_info(bool to_uci = false);
 const std::string compiler_info();
 void prefetch(void* addr);
 void start_logger(const std::string& fname);
 
-void dbg_hit_on(bool b);
-void dbg_hit_on(bool c, bool b);
-void dbg_mean_of(int v);
+void dbg_hit_on_impl(loc_file_line info_str,bool b);
+void dbg_hit_on_impl(loc_file_line info_str,bool c, bool b);
+void dbg_mean_of_impl(loc_file_line info_str,int v);
+
 void dbg_print();
 
 typedef std::chrono::milliseconds::rep TimePoint; // A value in milliseconds
