58836f55352e1d9a062b7f02ca1dd4451234c5fb
Vizvezdenec <Vizvezdenec@gmail.com>
2020-02-07 23:26:05 +0300

    tweak. bench 5468987

--- src/search.cpp
+++ src/search.cpp
@@ -1008,7 +1008,7 @@ moves_loop: // When in check, search starts from here
               int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), 0);
 
               // Countermoves based pruning (~20 Elo)
-              if (   (lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
+              if (   ((lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1))
                    || depth < 7)
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
