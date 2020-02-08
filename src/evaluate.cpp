0a93a5fc1072577d6d3b0497efcb2b415ac52cec
Vizvezdenec <Vizvezdenec@gmail.com>
2020-02-08 05:06:16 +0300

    tweak. bench 4723903

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -443,7 +443,7 @@ namespace {
     int kingFlankAttack = popcount(b1) + popcount(b2);
     int kingFlankDefense = popcount(b3);
 
-    kingDanger += 50 * bool(attackedBy[Them][ALL_PIECES] & forward_ranks_bb(Them, ksq) & kingRing[Us]);
+    kingDanger += 50 * bool(weak & forward_ranks_bb(Them, ksq) & kingRing[Us]);
 
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  + 185 * popcount(kingRing[Us] & weak)
