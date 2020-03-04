e7dd5a079efef0509c4b56229364f872365c9971
Lyudmil Antonov <lantonov.here@gmail.com>
2020-03-04 13:30:15 +0200

    Tuned histoy reduction; 16630 Bench 5027335

--- src/search.cpp
+++ src/search.cpp
@@ -1190,7 +1190,7 @@ moves_loop: // When in check, search starts from here
                   r++;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-              r -= ss->statScore / 17282;
+              r -= ss->statScore / 16630;
           }
 
           // Increase reduction for captures/promotions if late move and at low depth
