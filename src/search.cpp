c077bfb413fbed8d6fa94459135ca81f9977c2f2
silversolver1 <61594747+silversolver1@users.noreply.github.com>
2020-03-08 14:52:05 -0500

    Remove set statScore to zero
    Simplification. Removes setting statScore to zero if negative.
    
    STC:
    LLR: 2.95 (-2.94,2.94) {-1.50,0.50}
    Total: 84820 W: 16100 L: 16033 D: 52687
    Ptnml(0-2): 1442, 9865, 19723, 9944, 1436
    https://tests.stockfishchess.org/tests/view/5e654fdae42a5c3b3ca2e2f8
    
    LTC:
    LLR: 2.94 (-2.94,2.94) {-1.50,0.50}
    Total: 57658 W: 7435 L: 7391 D: 42832
    Ptnml(0-2): 441, 5397, 17104, 5451, 436
    https://tests.stockfishchess.org/tests/view/5e657ce9e42a5c3b3ca2e307
    
    closes https://github.com/official-stockfish/Stockfish/pull/2578
    
    Bench: 5168890

--- src/search.cpp
+++ src/search.cpp
@@ -1175,13 +1175,6 @@ moves_loop: // When in check, search starts from here
                              + (*contHist[3])[movedPiece][to_sq(move)]
                              - 4926;
 
-              // Reset statScore to zero if negative and most stats shows >= 0
-              if (    ss->statScore < 0
-                  && (*contHist[0])[movedPiece][to_sq(move)] >= 0
-                  && (*contHist[1])[movedPiece][to_sq(move)] >= 0
-                  && thisThread->mainHistory[us][from_to(move)] >= 0)
-                  ss->statScore = 0;
-
               // Decrease/increase reduction by comparing opponent's stat score (~10 Elo)
               if (ss->statScore >= -102 && (ss-1)->statScore < -114)
                   r--;
