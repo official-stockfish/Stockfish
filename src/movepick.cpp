00bdb785e19aa7379d2cce7b4d6ddf75bd97e1f2
protonspring <mike@whiteley.org>
2020-03-27 23:06:32 -0600

    use bool for TT stages.

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -25,10 +25,10 @@
 namespace {
 
   enum Stages {
-    MAIN_TT, CAPTURE_INIT, GOOD_CAPTURE, REFUTATION, QUIET_INIT, QUIET, BAD_CAPTURE,
-    EVASION_TT, EVASION_INIT, EVASION,
-    PROBCUT_TT, PROBCUT_INIT, PROBCUT,
-    QSEARCH_TT, QCAPTURE_INIT, QCAPTURE, QCHECK_INIT, QCHECK
+    CAPTURE_INIT, GOOD_CAPTURE, REFUTATION, QUIET_INIT, QUIET, BAD_CAPTURE,
+    EVASION_INIT, EVASION,
+    PROBCUT_INIT, PROBCUT,
+    QCAPTURE_INIT, QCAPTURE, QCHECK_INIT, QCHECK
   };
 
   // partial_insertion_sort() sorts moves in descending order up to and including
@@ -63,9 +63,8 @@ MovePicker::MovePicker(const Position& p, Move ttm, Depth d, const ButterflyHist
 
   assert(d > 0);
 
-  stage = pos.checkers() ? EVASION_TT : MAIN_TT;
-  ttMove = ttm && pos.pseudo_legal(ttm) ? ttm : MOVE_NONE;
-  stage += (ttMove == MOVE_NONE);
+  stage = pos.checkers() ? EVASION_INIT : CAPTURE_INIT;
+  useTTM = ((ttMove = ttm && pos.pseudo_legal(ttm) ? ttm : MOVE_NONE));
 }
 
 /// MovePicker constructor for quiescence search
@@ -75,11 +74,10 @@ MovePicker::MovePicker(const Position& p, Move ttm, Depth d, const ButterflyHist
 
   assert(d <= 0);
 
-  stage = pos.checkers() ? EVASION_TT : QSEARCH_TT;
-  ttMove =   ttm
+  stage = pos.checkers() ? EVASION_INIT : QCAPTURE_INIT;
+  useTTM = ((ttMove =   (ttm
           && (depth > DEPTH_QS_RECAPTURES || to_sq(ttm) == recaptureSquare)
-          && pos.pseudo_legal(ttm) ? ttm : MOVE_NONE;
-  stage += (ttMove == MOVE_NONE);
+          && pos.pseudo_legal(ttm) ? ttm : MOVE_NONE)));
 }
 
 /// MovePicker constructor for ProbCut: we generate captures with SEE greater
@@ -89,12 +87,11 @@ MovePicker::MovePicker(const Position& p, Move ttm, Value th, const CapturePiece
 
   assert(!pos.checkers());
 
-  stage = PROBCUT_TT;
-  ttMove =   ttm
-          && pos.capture(ttm)
-          && pos.pseudo_legal(ttm)
-          && pos.see_ge(ttm, threshold) ? ttm : MOVE_NONE;
-  stage += (ttMove == MOVE_NONE);
+  stage = PROBCUT_INIT;
+  useTTM = ((ttMove =   ttm
+                      && pos.capture(ttm)
+                      && pos.pseudo_legal(ttm)
+                      && pos.see_ge(ttm, threshold) ? ttm : MOVE_NONE));
 }
 
 /// MovePicker::score() assigns a numerical value to each move in a list, used
@@ -156,16 +153,11 @@ Move MovePicker::next_move(bool skipQuiets) {
 top:
   switch (stage) {
 
-  case MAIN_TT:
-  case EVASION_TT:
-  case QSEARCH_TT:
-  case PROBCUT_TT:
-      ++stage;
-      return ttMove;
-
   case CAPTURE_INIT:
   case PROBCUT_INIT:
   case QCAPTURE_INIT:
+      if (!(useTTM = !useTTM)) return ttMove; //if set, unset and return ttm
+
       cur = endBadCaptures = moves;
       endMoves = generate<CAPTURES>(pos, cur);
 
@@ -231,6 +223,8 @@ top:
       return select<Next>([](){ return true; });
 
   case EVASION_INIT:
+      if (!(useTTM = !useTTM)) return ttMove; //if set, unset and return ttm
+
       cur = moves;
       endMoves = generate<EVASIONS>(pos, cur);
 
