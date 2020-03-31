375e4eeaf5e739c176c38ff05ae954bb60a98987
31m059 <37052095+31m059@users.noreply.github.com>
2020-03-30 21:53:02 -0400

    Simplify a candidate passer condition.
    STC:
    LLR: 2.94 (-2.94,2.94) {-1.50,0.50}
    Total: 31528 W: 6208 L: 6061 D: 19259
    Ptnml(0-2): 541, 3673, 7205, 3788, 557
    https://tests.stockfishchess.org/tests/view/5e825db0e42a5c3b3ca2ee21
    
    LTC:
    LLR: 2.94 (-2.94,2.94) {-1.50,0.50}
    Total: 38546 W: 5083 L: 5009 D: 28454
    Ptnml(0-2): 299, 3628, 11362, 3668, 316
    https://tests.stockfishchess.org/tests/view/5e826ec7e42a5c3b3ca2ee2a
    
    closes https://github.com/official-stockfish/Stockfish/pull/2607
    
    Bench: 5139561

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -639,9 +639,8 @@ namespace {
         } // r > RANK_3
 
         // Scale down bonus for candidate passers which need more than one
-        // pawn push to become passed, or have a pawn in front of them.
-        if (   !pos.pawn_passed(Us, s + Up)
-            || (pos.pieces(PAWN) & (s + Up)))
+        // pawn push to become passed.
+        if (!pos.pawn_passed(Us, s + Up))
             bonus = bonus / 2;
 
         score += bonus - PassedFile * edge_distance(file_of(s));
