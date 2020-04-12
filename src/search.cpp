88fc4fb40046a043f0dc455eadb0a026ce045a8c
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-04-12 18:38:53 +0200

    Fixup: comment

--- src/search.cpp
+++ src/search.cpp
@@ -1040,7 +1040,7 @@ moves_loop: // When in check, search starts from here
           }
           else
           {
-              // Capture history based pruning when not in check
+              // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
                   && lmrDepth < 1
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
