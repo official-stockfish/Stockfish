0573c06f6745b4b36e5c6c5e67709504f994f96a
mstembera <MissingEmail@email>
2020-06-07 23:40:30 -0700

    See how a simple modulo performs. bench: 4213753

--- src/tt.h
+++ src/tt.h
@@ -84,7 +84,7 @@ public:
 
   // The 32 lowest order bits of the key are used to get the index of the cluster
   TTEntry* first_entry(const Key key) const {
-    return &table[(uint32_t(key) * uint64_t(clusterCount)) >> 32].entry[0];
+    return &table[key % clusterCount].entry[0];
   }
 
 private:
--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -56,8 +56,7 @@ bool CaseInsensitiveLess::operator() (const string& s1, const string& s2) const 
 
 void init(OptionsMap& o) {
 
-  // at most 2^32 clusters.
-  constexpr int MaxHashMB = Is64Bit ? 131072 : 2048;
+  constexpr int MaxHashMB = Is64Bit ? 1073741824 : 2048;
 
   o["Debug Log File"]        << Option("", on_logger);
   o["Contempt"]              << Option(24, -100, 100);
