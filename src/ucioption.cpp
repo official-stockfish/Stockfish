d6c9a58c2f36ebeab1f73d3f0925e07d4337f673
mstembera <MissingEmail@email>
2020-06-14 02:52:39 -0700

    Updated contempt. bench: 4245202

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -60,7 +60,7 @@ void init(OptionsMap& o) {
   constexpr int MaxHashMB = Is64Bit ? 33554432 : 2048;
 
   o["Debug Log File"]        << Option("", on_logger);
-  o["Contempt"]              << Option(24, -100, 100);
+  o["Contempt"]              << Option(14, -500, 500);
   o["Analysis Contempt"]     << Option("Both var Off var White var Black var Both", "Both");
   o["Threads"]               << Option(1, 1, 512, on_threads);
   o["Hash"]                  << Option(16, 1, MaxHashMB, on_hash_size);
