bc3c215490edf24cef0ff87d74ab01eeb91ae1bc
Vizvezdenec <Vizvezdenec@gmail.com>
2020-06-17 05:36:30 +0300

    More reduction for evading pawn moves.
    pawn moves are irreversable unlike other evading moves; pawn is the least valuable piece in the game.
    So it makes a lot of sence to assume that evading pawn moves are on average not as good as other evading moves thus can be reduced more.
    
    Passed STC
    https://tests.stockfishchess.org/tests/view/5ee9602e563bc7aa756002dc
    LLR: 2.95 (-2.94,2.94) {-0.50,1.50}
    Total: 94176 W: 17993 L: 17668 D: 58515
    Ptnml(0-2): 1634, 10742, 21989, 11111, 1612
    
    Passed LTC
    https://tests.stockfishchess.org/tests/view/5ee97342563bc7aa75600301
    LLR: 2.94 (-2.94,2.94) {0.25,1.75}
    Total: 20432 W: 2572 L: 2354 D: 15506
    Ptnml(0-2): 146, 1707, 6280, 1949, 134
    
    closes https://github.com/official-stockfish/Stockfish/pull/2749
    
    Bench: 5073064

--- src/search.cpp
+++ src/search.cpp
@@ -1186,7 +1186,7 @@ moves_loop: // When in check, search starts from here
               // hence break make_move(). (~2 Elo)
               else if (    type_of(move) == NORMAL
                        && !pos.see_ge(reverse_move(move)))
-                  r -= 2 + ttPv;
+                  r -= 2 + ttPv - (type_of(movedPiece) == PAWN);
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
