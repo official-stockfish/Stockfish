110068808b51344ac59f8c6a0846f5dfdf670392
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-06-27 21:29:29 +0200

    Provide WDL statistics
    A number of engines, GUIs and tournaments start to report WDL estimates
    along or instead of scores. This patch enables reporting of those stats
    in a more or less standard way (http://www.talkchess.com/forum3/viewtopic.php?t=72140)
    
    The model this reporting uses is based on data derived from a few million fishtest LTC games,
    given a score and a game ply, a win rate is provided that matches rather closely,
    especially in the intermediate range [0.05, 0.95] that data. Some data is shown at
    https://github.com/glinscott/fishtest/wiki/UsefulData#win-loss-draw-statistics-of-ltc-games-on-fishtest
    Making the conversion game ply dependent is important for a good fit, and is in line
    with experience that a +1 score in the early midgame is more likely a win than in the late endgame.
    
    Even when enabled, the printing of the info causes no significant overhead.
    
    Passed STC:
    LLR: 2.94 (-2.94,2.94) {-1.50,0.50}
    Total: 197112 W: 37226 L: 37347 D: 122539
    Ptnml(0-2): 2591, 21025, 51464, 20866, 2610
    https://tests.stockfishchess.org/tests/view/5ef79ef4f993893290cc146b
    
    closes https://github.com/official-stockfish/Stockfish/pull/2778
    
    No functional change

--- src/uci.cpp
+++ src/uci.cpp
@@ -19,6 +19,7 @@
 */
 
 #include <cassert>
+#include <cmath>
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -182,6 +183,28 @@ namespace {
          << "\nNodes/second    : " << 1000 * nodes / elapsed << endl;
   }
 
+  // The win rate model returns the probability (per mille) of winning given an eval
+  // and a game-ply. The model fits rather accurately the LTC fishtest statistics.
+  int win_rate_model(Value v, int ply) {
+
+     // The model captures only up to 240 plies, so limit input (and rescale)
+     double m = std::min(240, ply) / 64.0;
+
+     // Coefficients of a 3rd order polynomial fit based on fishtest data
+     // for two parameters needed to transform eval to the argument of a
+     // logistic function.
+     double as[] = {-8.24404295, 64.23892342, -95.73056462, 153.86478679};
+     double bs[] = {-3.37154371, 28.44489198, -56.67657741,  72.05858751};
+     double a = (((as[0] * m + as[1]) * m + as[2]) * m) + as[3];
+     double b = (((bs[0] * m + bs[1]) * m + bs[2]) * m) + bs[3];
+
+     // Transform eval to centipawns with limited range
+     double x = Utility::clamp(double(100 * v) / PawnValueEg, -1000.0, 1000.0);
+
+     // Return win rate in per mille (rounded to nearest)
+     return int(0.5 + 1000 / (1 + std::exp((a - x) / b)));
+  }
+
 } // namespace
 
 
@@ -264,6 +287,22 @@ string UCI::value(Value v) {
       ss << "cp " << v * 100 / PawnValueEg;
   else
       ss << "mate " << (v > 0 ? VALUE_MATE - v + 1 : -VALUE_MATE - v) / 2;
+
+  return ss.str();
+}
+
+
+/// UCI::wdl() report WDL statistics given an evaluation and a game ply, based on
+/// data gathered for fishtest LTC games.
+
+string UCI::wdl(Value v, int ply) {
+
+  stringstream ss;
+
+  int wdl_w = win_rate_model( v, ply);
+  int wdl_l = win_rate_model(-v, ply);
+  int wdl_d = 1000 - wdl_w - wdl_l;
+  ss << " wdl " << wdl_w << " " << wdl_d << " " << wdl_l;
 
   return ss.str();
 }
