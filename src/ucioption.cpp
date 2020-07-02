fb83da0892c183690ddeb1f7c3dbf6779b12707a
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-07-02 18:58:37 +0200

    Set UCI_ShowWDL by default to false
    UCI_ShowWDL might not be shown by GUIs that don't know the option,
    but crash on the WDL output, effectively making it hard for users to
    turn it off and run the engine. This sets it by default to false.
    
    fixes https://github.com/official-stockfish/Stockfish/issues/2787
    
    closes https://github.com/official-stockfish/Stockfish/pull/2788
    
    No functional change.

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -74,7 +74,7 @@ void init(OptionsMap& o) {
   o["UCI_AnalyseMode"]       << Option(false);
   o["UCI_LimitStrength"]     << Option(false);
   o["UCI_Elo"]               << Option(1350, 1350, 2850);
-  o["UCI_ShowWDL"]           << Option(true);
+  o["UCI_ShowWDL"]           << Option(false);
   o["SyzygyPath"]            << Option("<empty>", on_tb_path);
   o["SyzygyProbeDepth"]      << Option(1, 1, 100);
   o["Syzygy50MoveRule"]      << Option(true);
