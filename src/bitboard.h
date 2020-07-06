6da00a7c36889980dd02d24ac079e03e32fd01ea
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-07-06 18:11:46 +0200

    Fixup: square s1

--- src/bitboard.h
+++ src/bitboard.h
@@ -124,7 +124,7 @@ inline Bitboard  operator&(Square s, Bitboard b) { return b & s; }
 inline Bitboard  operator|(Square s, Bitboard b) { return b | s; }
 inline Bitboard  operator^(Square s, Bitboard b) { return b ^ s; }
 
-inline Bitboard  operator|(Square s, Square s2) { return square_bb(s) | s2; }
+inline Bitboard  operator|(Square s1, Square s2) { return square_bb(s1) | s2; }
 
 constexpr bool more_than_one(Bitboard b) {
   return b & (b - 1);
