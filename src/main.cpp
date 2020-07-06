3525329d2b62d6d0188428408f63870d75d420c2
Unai Corzo <corzounai@gmail.com>
2020-07-06 15:43:21 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -20,14 +20,8 @@
 
 #include <iostream>
 
-#include "bitboard.h"
-#include "endgame.h"
-#include "position.h"
-#include "search.h"
 #include "thread.h"
-#include "tt.h"
 #include "uci.h"
-#include "syzygy/tbprobe.h"
 
 namespace PSQT {
   void init();
