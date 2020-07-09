c94e9d4f20f3060f37c05690708960cb71a8ec7c
SFisGOD <jonathandumale@gmail.com>
2020-07-09 10:56:18 +0800

    Try. Bench: 4561903

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -314,8 +314,7 @@ namespace {
             if (   Pt == KNIGHT 
                 && bb & s & ~CenterFiles
                 && !(b & pos.pieces(Them) & ~pos.pieces(PAWN))
-                && !more_than_one((pos.pieces(Them) & ~pos.pieces(PAWN)) & ((s & QueenSide) ? QueenSide : KingSide))
-                && b & pos.pieces(Them, PAWN) & attackedBy[Them][PAWN] & ~attackedBy[Us][PAWN])
+                && !more_than_one((pos.pieces(Them) & ~pos.pieces(PAWN)) & ((s & QueenSide) ? QueenSide : KingSide)))
                 score += BadOutpost;
             else if (bb & s)
                 score += Outpost[Pt == BISHOP];
