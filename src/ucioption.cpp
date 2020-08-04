e8ad3a2af0f041edfcc2a291d4a4788a59bc1db9
NguyenPham <phhnguyen@gmail.com>
2020-08-04 21:52:37 +1000

    Set option Use NNUE to true

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -80,7 +80,7 @@ void init(OptionsMap& o) {
   o["SyzygyProbeDepth"]      << Option(1, 1, 100);
   o["Syzygy50MoveRule"]      << Option(true);
   o["SyzygyProbeLimit"]      << Option(7, 0, 7);
-  o["Use NNUE"]              << Option(false, on_use_NNUE);
+  o["Use NNUE"]              << Option(true, on_use_NNUE);
   o["EvalFile"]              << Option("nn-97f742aaefcd.nnue", on_eval_file);
 }
 
