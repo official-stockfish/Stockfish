0541ed27f9734f34ff25632bbd0f958831bb457a
Unai Corzo <corzounai@gmail.com>
2020-08-06 10:03:19 +0200

    Search tweak
    bench: 4399403

--- src/search.cpp
+++ src/search.cpp
@@ -1274,10 +1274,9 @@ moves_loop: // When in check, search starts from here
           }
       }
 
-      // For PV nodes only, do a full PV search on the first move or after a fail
-      // high (in the latter case search only if value < beta), otherwise let the
-      // parent node fail low with value <= alpha and try another move.
-      if (PvNode && (moveCount == 1 || (value > alpha && (rootNode || value < beta))))
+      // For PV nodes only, do a full PV search on the first move or after a fail high,
+      // otherwise let the parent node fail low with value <= alpha and try another move.
+      if (PvNode && (moveCount == 1 || value > alpha))
       {
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
