539b35bc87a0302ef3d8d1514111e4a93c766e45
UnaiCorzo <corzounai@gmail.com>
2020-08-07 11:47:48 +0200

    Search tweak

--- src/search.cpp
+++ src/search.cpp
@@ -816,7 +816,7 @@ namespace {
 
     // Step 8. Futility pruning: child node (~50 Elo)
     if (   !PvNode
-        &&  depth < 6
+        &&  depth < 8
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
