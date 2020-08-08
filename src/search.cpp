450b60a303b0c59b0cc5dd22d95b9a983dfc4f96
mckx00 <mckx00@gmail.com>
2020-08-08 03:07:07 -0700

    Remove unnecessay legality check
    Possible after the recent reording pos.legal(move) check
    
    https://github.com/official-stockfish/Stockfish/pull/2941
    
    No functional change.

--- src/search.cpp
+++ src/search.cpp
@@ -1079,8 +1079,7 @@ moves_loop: // When in check, search starts from here
        /* &&  ttValue != VALUE_NONE Already implicit in the next condition */
           &&  abs(ttValue) < VALUE_KNOWN_WIN
           && (tte->bound() & BOUND_LOWER)
-          &&  tte->depth() >= depth - 3
-          &&  pos.legal(move))
+          &&  tte->depth() >= depth - 3)
       {
           Value singularBeta = ttValue - ((formerPv + 4) * depth) / 2;
           Depth singularDepth = (depth - 1 + 3 * formerPv) / 2;
