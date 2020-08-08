ff8a4c83f73e42ba784eed6115a855cc5027183f
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-08 17:43:41 +0300

    tweak. bench 4706516

--- src/search.cpp
+++ src/search.cpp
@@ -1055,7 +1055,7 @@ moves_loop: // When in check, search starts from here
                   && !(PvNode && abs(bestValue) < 2)
                   && PieceValue[MG][type_of(movedPiece)] >= PieceValue[MG][type_of(pos.piece_on(to_sq(move)))]
                   && !ss->inCheck
-                  && ss->staticEval + 267 + 391 * lmrDepth
+                  && ss->staticEval + 178 + 261 * lmrDepth
                      + PieceValue[MG][type_of(pos.piece_on(to_sq(move)))] <= alpha)
                   continue;
 
