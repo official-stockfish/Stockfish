b4aa7cadf78baa5d50b9055d8ef9aa25f33fa64e
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-08-13 22:54:13 +0200

    Verify SHA of downloaded net file
    check SHA of the available and downloaded file.
    Document the format requirement.
    
    No functional change.

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -79,6 +79,8 @@ void init(OptionsMap& o) {
   o["Syzygy50MoveRule"]      << Option(true);
   o["SyzygyProbeLimit"]      << Option(7, 0, 7);
   o["Use NNUE"]              << Option(false, on_use_NNUE);
+  // The default must follow the format nn-[SHA256 first 12 digits].nnue
+  // for the build process (profile-build and fishtest) to work.
   o["EvalFile"]              << Option("nn-82215d0fd0df.nnue", on_eval_file);
 }
 
