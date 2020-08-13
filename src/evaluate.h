7ef15982d476433157c90ec0e01b53e6b19c97eb
Daniel Uranga <daniel.uranga@gmail.com>
2020-08-13 03:09:23 -0300

    Modify the hybrid condition so it uses alpha-beta window information

--- src/evaluate.h
+++ src/evaluate.h
@@ -28,7 +28,7 @@ class Position;
 namespace Eval {
 
   std::string trace(const Position& pos);
-  Value evaluate(const Position& pos);
+  Value evaluate(const Position& pos, Value alpha, Value beta);
 
   extern bool useNNUE;
   extern std::string eval_file_loaded;
