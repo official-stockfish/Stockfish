33d0acb69b91dc8686413ef3374cb6c2bbf32476
mstembera <MissingEmail@email>
2020-08-14 04:49:33 -0700

    Try to match relative magnitude of NNUE eval to classical. bench: 4165920

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -159,7 +159,7 @@ namespace Eval::NNUE {
 
   // Evaluation function. Perform differential calculation.
   Value evaluate(const Position& pos) {
-    Value v = ComputeScore(pos, false);
+    Value v = ComputeScore(pos, false) * 5 / 4;
     v = Utility::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
 
     return v;
