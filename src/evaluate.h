7135678f71b7f6ee32e92b8dbef2b16b403d8ea9
Sergio Vieri <sergio.vieri.hp@gmail.com>
2020-09-14 17:24:05 +0800

    Update default net to nn-03744f8d56d8.nnue
    Equivalent to 20200914-1520
    
    closes https://github.com/official-stockfish/Stockfish/pull/3123
    
    Bench: 4222126

--- src/evaluate.h
+++ src/evaluate.h
@@ -38,7 +38,7 @@ namespace Eval {
   // The default net name MUST follow the format nn-[SHA256 first 12 digits].nnue
   // for the build process (profile-build and fishtest) to work. Do not change the
   // name of the macro, as it is used in the Makefile.
-  #define EvalFileDefaultName   "nn-308d71810dff.nnue"
+  #define EvalFileDefaultName   "nn-03744f8d56d8.nnue"
 
   namespace NNUE {
 
