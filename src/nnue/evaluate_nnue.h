928965c5e20e80ad332ec662ee04efef8304f48e
Stéphane Nicolet <cassio@free.fr>
2020-09-21 10:07:34 +0200

    Rename TtmemDeleter to LargePageDeleter

--- src/nnue/evaluate_nnue.h
+++ src/nnue/evaluate_nnue.h
@@ -41,7 +41,7 @@ namespace Eval::NNUE {
   };
 
   template <typename T>
-  struct TtmemDeleter {
+  struct LargePageDeleter {
     void operator()(T* ptr) const {
       ptr->~T();
       aligned_large_pages_free(ptr);
@@ -52,7 +52,7 @@ namespace Eval::NNUE {
   using AlignedPtr = std::unique_ptr<T, AlignedDeleter<T>>;
 
   template <typename T>
-  using LargePagePtr = std::unique_ptr<T, TtmemDeleter<T>>;
+  using LargePagePtr = std::unique_ptr<T, LargePageDeleter<T>>;
 
 }  // namespace Eval::NNUE
 
