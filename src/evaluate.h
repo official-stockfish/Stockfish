f07987ffcb3a170a16aa0ed34ec8ac19aaaa2514
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-09-26 17:47:52 +0200

    Update default net to nn-04a843f8932e.nnue
    an optimization of Sergio's nn-03744f8d56d8.nnue tuning the output layer (33 parameters) on game play.
    
    WIP code to make layer parameters tunable is https://github.com/vondele/Stockfish/tree/optionOutput
    Optimization itself is using https://github.com/vondele/nevergrad4sf
    Writing of the modified net using WIP code based on the learner code https://github.com/vondele/Stockfish/tree/evalWrite
    
    Most parameters in the output layer are changed only little (~5 for int8_t).
    
    passed STC:
    https://tests.stockfishchess.org/tests/view/5f716f6b3b22d6afa506941a
    LLR: 2.94 (-2.94,2.94) {-0.25,1.25}
    Total: 15488 W: 1859 L: 1689 D: 11940
    Ptnml(0-2): 79, 1260, 4917, 1388, 100
    
    passed LTC:
    https://tests.stockfishchess.org/tests/view/5f71908e3b22d6afa506942e
    LLR: 2.93 (-2.94,2.94) {0.25,1.25}
    Total: 8728 W: 518 L: 400 D: 7810
    Ptnml(0-2): 7, 338, 3556, 456, 7
    
    Bench: 3789924

--- src/evaluate.h
+++ src/evaluate.h
@@ -36,7 +36,7 @@ namespace Eval {
   // The default net name MUST follow the format nn-[SHA256 first 12 digits].nnue
   // for the build process (profile-build and fishtest) to work. Do not change the
   // name of the macro, as it is used in the Makefile.
-  #define EvalFileDefaultName   "nn-03744f8d56d8.nnue"
+  #define EvalFileDefaultName   "nn-04a843f8932e.nnue"
 
   namespace NNUE {
 
