c9f49644e84137249803328bb971487d45fc9b4b
mstembera <MissingEmail@email>
2020-10-30 13:45:40 -0700

    Result of https://tests.stockfishchess.org/tests/view/5f9a06796a2c112b60691c0f bench: 3517795

--- src/evaluate.h
+++ src/evaluate.h
@@ -36,7 +36,7 @@ namespace Eval {
   // The default net name MUST follow the format nn-[SHA256 first 12 digits].nnue
   // for the build process (profile-build and fishtest) to work. Do not change the
   // name of the macro, as it is used in the Makefile.
-  #define EvalFileDefaultName   "nn-2eb2e0707c2b.nnue"
+  #define EvalFileDefaultName   "nn-cb26f10b1fd9.nnue"
 
   namespace NNUE {
 
