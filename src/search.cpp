a3386896c1fa3917a43677a4ac88c9907831900a
lonfom169 <50217346+lonfom169@users.noreply.github.com>
2020-11-08 23:43:32 -0300

    Bench: 3595418

--- src/search.cpp
+++ src/search.cpp
@@ -1168,6 +1168,9 @@ moves_loop: // When in check, search starts from here
           // Decrease reduction if position is or has been on the PV (~10 Elo)
           if (ss->ttPv)
               r -= 2;
+              
+          if (!PvNode && depth > 10 && thisThread->bestMoveChanges <= 2)
+              r++;
 
           if (moveCountPruning && !formerPv)
               r++;
