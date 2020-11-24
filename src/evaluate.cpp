9fb6383ed804d0bc86d52b07def14352f44eb5b4
Unai Corzo <corzounai@gmail.com>
2020-11-24 17:06:30 +0100

    Assorted search and eval parameter tune
    Search and eval parameter tune.
    
    STC https://tests.stockfishchess.org/tests/view/5fba850a67cbf42301d6b07d
    LLR: 2.94 (-2.94,2.94) {-0.25,1.25}
    Total: 24312 W: 2388 L: 2228 D: 19696
    Ptnml(0-2): 85, 1800, 8241, 1930, 100
    
    LTC https://tests.stockfishchess.org/tests/view/5fbad5ea67cbf42301d6b0fa
    LLR: 2.95 (-2.94,2.94) {0.25,1.25}
    Total: 88376 W: 3619 L: 3351 D: 81406
    Ptnml(0-2): 56, 2977, 37849, 3255, 51
    
    closes https://github.com/official-stockfish/Stockfish/pull/3232
    
    bench: 3600361

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -187,11 +187,11 @@ using namespace Trace;
 namespace {
 
   // Threshold for lazy and space evaluation
-  constexpr Value LazyThreshold1 =  Value(1400);
-  constexpr Value LazyThreshold2 =  Value(1300);
-  constexpr Value SpaceThreshold = Value(12222);
-  constexpr Value NNUEThreshold1 =   Value(550);
-  constexpr Value NNUEThreshold2 =   Value(150);
+  constexpr Value LazyThreshold1 =  Value(1565);
+  constexpr Value LazyThreshold2 =  Value(1102);
+  constexpr Value SpaceThreshold = Value(11551);
+  constexpr Value NNUEThreshold1 =   Value(682);
+  constexpr Value NNUEThreshold2 =   Value(176);
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
   constexpr int KingAttackWeights[PIECE_TYPE_NB] = { 0, 0, 81, 52, 44, 10 };
