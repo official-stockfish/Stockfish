2442ba2b0e8a399b0dbfe9d23a8a2819cb0af987
Unai Corzo <corzounai@gmail.com>
2020-11-29 13:52:36 +0100

    Reductions simplification
    Simplify increase reduction for captures/promotions if late move and at low depth.
    
    STC https://tests.stockfishchess.org/tests/view/5fbff65067cbf42301d6b3ae
    LLR: 2.97 (-2.94,2.94) {-1.25,0.25}
    Total: 49088 W: 4607 L: 4555 D: 39926
    Ptnml(0-2): 177, 3615, 16932, 3619, 201
    
    LTC https://tests.stockfishchess.org/tests/view/5fc0902967cbf42301d6b3fc
    LLR: 2.99 (-2.94,2.94) {-0.75,0.25}
    Total: 160944 W: 6153 L: 6193 D: 148598
    Ptnml(0-2): 90, 5525, 69294, 5461, 102
    
    closes https://github.com/official-stockfish/Stockfish/pull/3248
    
    bench: 3834568

--- src/search.cpp
+++ src/search.cpp
@@ -1222,10 +1222,6 @@ moves_loop: // When in check, search starts from here
           }
           else
           {
-              // Increase reduction for captures/promotions if late move and at low depth
-              if (depth < 8 && moveCount > 2)
-                  r++;
-
               // Unless giving check, this capture is likely bad
               if (   !givesCheck
                   && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha)
