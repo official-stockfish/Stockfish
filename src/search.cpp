86121892d7d4511cc474320fce0405400bf7e3ae
BoredBored <alexiosangeletakis@yahoo.com>
2020-12-08 00:48:39 -0500

    Update search.cpp
    This change is faster than the original version.

--- src/search.cpp
+++ src/search.cpp
@@ -73,7 +73,7 @@ namespace {
 
   Depth reduction(bool i, Depth d, int mn) {
     int r = Reductions[d] * Reductions[mn];
-    return (r + 503) / 1024 + (!i && r > 915);
+    return ((503 + r + (( ~i && r > 915) << 10)) >> 10);
   }
 
   constexpr int futility_move_count(bool improving, Depth depth) {
