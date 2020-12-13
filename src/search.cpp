4ab65e9dfbff1abd5d23d4a4c334b18a28a5e3fe
pb00067 <pb00067@PHXL0356.wp.lan>
2020-12-13 11:26:49 +0100

    Take 8, now I hope without issues
    bench: 4018036

--- src/search.cpp
+++ src/search.cpp
@@ -1358,7 +1358,7 @@ moves_loop: // When in check, search starts from here
           if (captureOrPromotion && captureCount < 32)
               capturesSearched[captureCount++] = move;
 
-          if (quietCount < 64)
+          else if (quietCount < 64)
               quietsSearched[quietCount++] = move;
       }
     }
