ef0d789479afd0f6d9bcc1cd3064030baf850740
bmc4 <bmc4@cin.ufpe.br>
2021-01-16 22:10:41 -0300

    FIx order 2

--- src/position.cpp
+++ src/position.cpp
@@ -563,17 +563,18 @@ bool Position::pseudo_legal(const Move m) const {
   Square to = to_sq(m);
   Piece pc = moved_piece(m);
 
+  // Use a slower but simpler function for uncommon cases
   if (type_of(m) != NORMAL)
       return checkers() ? MoveList<    EVASIONS>(*this).contains(m)
                         : MoveList<NON_EVASIONS>(*this).contains(m);
 
+  // Is not a promotion, so promotion piece must be empty
+  if (promotion_type(m) - KNIGHT != NO_PIECE_TYPE)
+      return false;
+
   // If the 'from' square is not occupied by a piece belonging to the side to
   // move, the move is obviously not legal.
   if (pc == NO_PIECE || color_of(pc) != us)
-      return false;
-
-  // Is not a promotion, so promotion piece must be empty
-  if (promotion_type(m) - KNIGHT != NO_PIECE_TYPE)
       return false;
 
   // The destination square cannot be occupied by a friendly piece
