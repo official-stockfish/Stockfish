65eaf4189a5bfbf83070220819547837eef0f09c
mattginsberg <mlginsberg@gmail.com>
2021-01-18 17:16:38 -0800

    clarified flow in Eval::evaluate()

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1056,27 +1056,44 @@ Value Eval::evaluate(const Position& pos) {
          return NNUE::evaluate(pos) * (679 + mat / 32) / 1024 + Tempo;
       };
 
-      // If there is PSQ imbalance use classical eval, with small probability if it is small
+      // "random" test that has a 1/8 probability of being true
+      // because 0xB has three bits set
+      auto unlikely = [&]() { return !(pos.this_thread()->nodes & 0xB); };
+
+      // Deciding whether to use classical or NNUE evaluation works as
+      // follows:
+      // 1. For low piece endgames, use classical (most critical is
+      // bishop + A/H file pawn vs naked king is a draw),
+      // "strongClassical" below.
+      // 2. If the PSQ imbalance is large:
+      // 2a. If the classical imbalance is small, use NNUE.
+      // 2b. If there are opposite bishops and the classical imbalance
+      //     is modest, use NNUE a small fraction of the time.
+      // 2c. In other large PSQ cases, use the classical result.
+      // 3. If the PSQ imbalance is not large:
+      // 3a. If the PSQ imabalance is not small, use the classical
+      //     valuation a small fraction of the time.
+      // 3b. Otherwise, use NNUE
+      
       Value psq = Value(abs(eg_value(pos.psq_score())));
       int   r50 = 16 + pos.rule50_count();
-      bool  largePsq = psq * 16 > (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50;
-      bool  classical = largePsq || (psq > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
+      Value thresh = (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50;
+      bool  largePsq = (psq * 16 > thresh);
 
-      // Use classical evaluation for really low piece endgames.
-      // The most critical case is a bishop + A/H file pawn vs naked king draw.
-      bool strongClassical = pos.non_pawn_material() < 2 * RookValueMg && pos.count<PAWN>() < 2;
+      bool  lowPieceEG = pos.non_pawn_material() < 2 * RookValueMg && pos.count<PAWN>() < 2;
 
-      v = classical || strongClassical ? Evaluation<NO_TRACE>(pos).value() : adjusted_NNUE();
-
-      // If the classical eval is small and imbalance large, use NNUE nevertheless.
-      // For the case of opposite colored bishops, switch to NNUE eval with
-      // small probability if the classical eval is less than the threshold.
-      if (   largePsq && !strongClassical
-          && (   abs(v) * 16 < NNUEThreshold2 * r50
-              || (   pos.opposite_bishops()
-                  && abs(v) * 16 < (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50
-                  && !(pos.this_thread()->nodes & 0xB))))
+      if (lowPieceEG) v = Evaluation<NO_TRACE>(pos).value();      // case 1
+      else if (largePsq) {
+        v = Evaluation<NO_TRACE>(pos).value();                    // case 2c
+        if (   abs(v) * 16 < NNUEThreshold2 * r50                 // case 2a
+            || (   pos.opposite_bishops()                         // case 2b
+                && abs(v) * 16 < thresh
+                && unlikely()))
           v = adjusted_NNUE();
+      }
+      else if (psq > PawnValueMg / 4 && unlikely())               // case 3a
+        v = Evaluation<NO_TRACE>(pos).value();
+      else v = adjusted_NNUE();                                   // case 3b
   }
 
   // Damp down the evaluation linearly when shuffling
