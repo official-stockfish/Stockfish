930dd12913db5f462adcd229d7033176bda03d55
Matt Ginsberg <ginsberg@dl2.imagreendriver.com>
2021-01-19 16:06:12 -0800

    constants to allow experimentation with # bits in TT generation

--- src/tt.cpp
+++ src/tt.cpp
@@ -132,12 +132,13 @@ TTEntry* TranspositionTable::probe(const Key key, bool& found) const {
   // Find an entry to be replaced according to the replacement strategy
   TTEntry* replace = tte;
   for (int i = 1; i < ClusterSize; ++i)
-      // Due to our packed storage format for generation and its cyclic
-      // nature we add 263 (256 is the modulus plus 7 to keep the unrelated
-      // lowest three bits from affecting the result) to calculate the entry
-      // age correctly even after generation8 overflows into the next cycle.
-      if (  replace->depth8 - ((263 + generation8 - replace->genBound8) & 0xF8)
-          >   tte[i].depth8 - ((263 + generation8 -   tte[i].genBound8) & 0xF8))
+      // Due to our packed storage format for generation and its
+      // cyclic nature we add CYCLE (256 is the modulus plus what's
+      // needed to keep the unrelated lowest n bits from affecting
+      // the result) to calculate the entry age correctly even after
+      // generation8 overflows into the next cycle.
+      if (  replace->depth8 - ((CYCLE + generation8 - replace->genBound8) & MASK)
+          >   tte[i].depth8 - ((CYCLE + generation8 -   tte[i].genBound8) & MASK))
           replace = &tte[i];
 
   return found = false, replace;
@@ -152,7 +153,7 @@ int TranspositionTable::hashfull() const {
   int cnt = 0;
   for (int i = 0; i < 1000; ++i)
       for (int j = 0; j < ClusterSize; ++j)
-          cnt += table[i].entry[j].depth8 && (table[i].entry[j].genBound8 & 0xF8) == generation8;
+          cnt += table[i].entry[j].depth8 && (table[i].entry[j].genBound8 & MASK) == generation8;
 
   return cnt / ClusterSize;
 }
