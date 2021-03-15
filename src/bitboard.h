8505b2b1abee9da6c9a6c3fedb939c619ca22783
bmc4 <bmc4@cin.ufpe.br>
2021-03-15 16:45:19 -0300

    fix description

--- src/bitboard.h
+++ src/bitboard.h
@@ -220,6 +220,7 @@ inline Bitboard line_bb(Square s1, Square s2) {
 /// between the two given squares (including last given square and excluding first).
 /// If the given squares are not on a same file/rank/diagonal, we return 0. For instance,
 /// ray_bb(SQ_C4, SQ_F7) will return a bitboard with squares D5, E6 and F7.
+
 inline Bitboard ray_bb(Square ksq, Square checkersq){
   assert(is_ok(ksq) && is_ok(checkersq));
   return RayBB[ksq][checkersq];
