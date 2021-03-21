ac22a5828710db832b1a985884ddef9432432905
Stéphane Nicolet <cassio@free.fr>
2021-03-21 10:22:32 +0100

    Improve comment

--- src/search.cpp
+++ src/search.cpp
@@ -1252,7 +1252,7 @@ moves_loop: // When in check, search starts from here
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
-              // hence break make_move(). (~2 Elo)
+              // hence break reverse_move() (~2 Elo)
               else if (    type_of(move) == NORMAL
                        && !pos.see_ge(reverse_move(move)))
                   r -= 2 + ss->ttPv - (type_of(movedPiece) == PAWN);
