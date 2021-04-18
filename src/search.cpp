31abd00b1215a0ce5f79dc6df531ea2fafd960c9
xoto10 <buylow001@gmail.com>
2021-04-18 22:20:56 +0100

    More time if losing, 200-400, /4. Bench 4503918

--- src/search.cpp
+++ src/search.cpp
@@ -508,7 +508,7 @@ void Thread::search() {
       {
           double fallingEval = (318 + 6 * (mainThread->bestPreviousScore - bestValue)
                                     + 6 * (mainThread->iterValue[iterIdx] - bestValue)
-                                    + std::clamp(int(-bestValue), 0, 200) / 4
+                                    + std::clamp(int(-bestValue) - 200, 0, 200) / 4
                                ) / 825.0;
           fallingEval =  std::clamp(fallingEval, 0.5, 1.5);
 
