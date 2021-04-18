d6a9a835a54e8e20030cddf8f27d38c77593fe70
xoto10 <buylow001@gmail.com>
2021-04-18 07:48:55 +0100

    More time if losing, 0-25 outside clamp. Bench 4503918

--- src/search.cpp
+++ src/search.cpp
@@ -509,7 +509,7 @@ void Thread::search() {
           double fallingEval = (318 + 6 * (mainThread->bestPreviousScore - bestValue)
                                     + 6 * (mainThread->iterValue[iterIdx] - bestValue)) / 825.0;
           fallingEval =  std::clamp(fallingEval, 0.5, 1.5)
-                       + std::clamp(int(-bestValue) - 100, 0, 200) / 3300.0;
+                       + std::clamp(int(-bestValue) - 100, 0, 200) / 13200.0;
 
           // If the bestMove is stable over several iterations, reduce time accordingly
           timeReduction = lastBestMoveDepth + 9 < completedDepth ? 1.92 : 0.95;
