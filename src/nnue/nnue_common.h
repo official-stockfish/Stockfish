b748b46714d5f8e0acca0a042ede1fc95e4f5190
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2021-04-24 15:08:11 +0200

    Cleanup and simplify NNUE code.
    A lot of optimizations happend since the NNUE was introduced
    and since then some parts of the code were left unused. This
    got to the point where asserts were have to be made just to
    let people know that modifying something will not have any
    effects or may even break everything due to the assumptions
    being made. Removing these parts removes those inexisting
    "false dependencies". Additionally:
    
     * append_changed_indices now takes the king pos and stateinfo
       explicitly, no more misleading pos parameter
     * IndexList is removed in favor of a generic ValueList.
       Feature transformer just instantiates the type it needs.
     * The update cost and refresh requirement is deferred to the
       feature set once again, but now doesn't go through the whole
       FeatureSet machinery and just calls HalfKP directly.
     * accumulator no longer has a singular dimension.
     * The PS constants and the PieceSquareIndex array are made local
       to the HalfKP feature set because they are specific to it and
       DO differ for other feature sets.
     * A few names are changed to more descriptive
    
    Passed STC non-regression:
    https://tests.stockfishchess.org/tests/view/608421dd95e7f1852abd2790
    LLR: 2.95 (-2.94,2.94) <-2.50,0.50>
    Total: 180008 W: 16186 L: 16258 D: 147564
    Ptnml(0-2): 587, 12593, 63725, 12503, 596
    
    closes https://github.com/official-stockfish/Stockfish/pull/3441
    
    No functional change

--- src/nnue/nnue_common.h
+++ src/nnue/nnue_common.h
@@ -71,31 +71,6 @@ namespace Stockfish::Eval::NNUE {
 
   constexpr std::size_t MaxSimdWidth = 32;
 
-  // unique number for each piece type on each square
-  enum {
-    PS_NONE     =  0,
-    PS_W_PAWN   =  1,
-    PS_B_PAWN   =  1 * SQUARE_NB + 1,
-    PS_W_KNIGHT =  2 * SQUARE_NB + 1,
-    PS_B_KNIGHT =  3 * SQUARE_NB + 1,
-    PS_W_BISHOP =  4 * SQUARE_NB + 1,
-    PS_B_BISHOP =  5 * SQUARE_NB + 1,
-    PS_W_ROOK   =  6 * SQUARE_NB + 1,
-    PS_B_ROOK   =  7 * SQUARE_NB + 1,
-    PS_W_QUEEN  =  8 * SQUARE_NB + 1,
-    PS_B_QUEEN  =  9 * SQUARE_NB + 1,
-    PS_NB = 10 * SQUARE_NB + 1
-  };
-
-  constexpr uint32_t PieceSquareIndex[COLOR_NB][PIECE_NB] = {
-    // convention: W - us, B - them
-    // viewed from other side, W and B are reversed
-    { PS_NONE, PS_W_PAWN, PS_W_KNIGHT, PS_W_BISHOP, PS_W_ROOK, PS_W_QUEEN, PS_NONE, PS_NONE,
-      PS_NONE, PS_B_PAWN, PS_B_KNIGHT, PS_B_BISHOP, PS_B_ROOK, PS_B_QUEEN, PS_NONE, PS_NONE },
-    { PS_NONE, PS_B_PAWN, PS_B_KNIGHT, PS_B_BISHOP, PS_B_ROOK, PS_B_QUEEN, PS_NONE, PS_NONE,
-      PS_NONE, PS_W_PAWN, PS_W_KNIGHT, PS_W_BISHOP, PS_W_ROOK, PS_W_QUEEN, PS_NONE, PS_NONE }
-  };
-
   // Type of input feature after conversion
   using TransformedFeatureType = std::uint8_t;
   using IndexType = std::uint32_t;
