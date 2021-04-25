5f1dc85d20edf44190e79f81ab2429b05c3ffc8a
lonfom169 <lomalfredo8@gmail.com>
2021-04-24 21:37:47 -0300

    Bench: 4075325

--- src/search.cpp
+++ src/search.cpp
@@ -1130,6 +1130,8 @@ moves_loop: // When in check, search starts from here
           {
               extension = 1;
               singularQuietLMR = !ttCapture;
+              if (!PvNode && value < singularBeta - 140)
+                  extension = 2;
           }
 
           // Multi-cut pruning
