5ebe470b5fe27002b11cf2945cf2d55a22639dfb
xoto10 <buylow001@gmail.com>
2021-05-27 16:04:47 +0100

    remove "two tempos"

--- src/search.cpp
+++ src/search.cpp
@@ -753,7 +753,6 @@ namespace {
     else
     {
         // In case of null move search use previous static eval with a different sign
-        // and addition of two tempos
         if ((ss-1)->currentMove != MOVE_NULL)
             ss->staticEval = eval = evaluate(pos);
         else
@@ -1441,7 +1440,6 @@ moves_loop: // When in check, search starts here
         }
         else
             // In case of null move search use previous static eval with a different sign
-            // and addition of two tempos
             ss->staticEval = bestValue =
             (ss-1)->currentMove != MOVE_NULL ? evaluate(pos)
                                              : -(ss-1)->staticEval;
