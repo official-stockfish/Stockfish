e90e99f85f540f4109fdc57966fba759923c4eca
bmc4 <bmc4@cin.ufpe.br>
2021-06-06 13:31:57 -0300

    Tweak
    bench: 5173012

--- src/search.cpp
+++ src/search.cpp
@@ -1132,6 +1132,9 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction(improving, depth, moveCount);
 
+          if (PvNode)
+              r--;
+
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
               r--;
