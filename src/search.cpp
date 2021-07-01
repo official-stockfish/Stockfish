f4afd70cdd69724153b48bba55042d8d606afc7b
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-01 19:51:41 +0200

    no cut node reduction for killer moves. bench: 5343478

--- src/search.cpp
+++ src/search.cpp
@@ -1157,8 +1157,8 @@ moves_loop: // When in check, search starts from here
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
-          if (cutNode)
-              r += 1 + !captureOrPromotion;
+          if (cutNode && move != ss->killers[0])
+              r += 2;
 
           if (!captureOrPromotion)
           {
