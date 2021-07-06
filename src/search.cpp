09b6d28391cf582d99897360b225bcbbe38dd1c6
Vizvezdenec <Vizvezdenec@gmail.com>
2021-07-06 20:44:50 +0300

    Remove futility pruning depth limit
    This patch removes futility pruning depth limit for child node futility pruning.
    In current master it was double capped by depth and by futility margin, which is also a function of depth, which didn't make much sense.
    
    passed STC
    https://tests.stockfishchess.org/tests/view/60e2418f9ea99d7c2d693e64
    LLR: 2.95 (-2.94,2.94) <-2.50,0.50>
    Total: 116168 W: 9100 L: 9097 D: 97971
    Ptnml(0-2): 319, 7496, 42476, 7449, 344
    
    passed LTC
    https://tests.stockfishchess.org/tests/view/60e3374f9ea99d7c2d693f20
    LLR: 2.96 (-2.94,2.94) <-2.50,0.50>
    Total: 43304 W: 1282 L: 1231 D: 40791
    Ptnml(0-2): 8, 1126, 19335, 1173, 10
    
    closes https://github.com/official-stockfish/Stockfish/pull/3606
    
    bench 4965493

--- src/search.cpp
+++ src/search.cpp
@@ -780,7 +780,6 @@ namespace {
 
     // Step 7. Futility pruning: child node (~50 Elo)
     if (   !PvNode
-        &&  depth < 9
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
