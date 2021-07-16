a31a8f44f753714f5ab4912079b454b61b0d0a8b
xoto10 <buylow001@gmail.com>
2021-07-16 12:28:25 +0100

    Modify draw_value() to return -3..0. Bench 5206684

--- src/search.cpp
+++ src/search.cpp
@@ -88,7 +88,7 @@ namespace {
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
   Value value_draw(Thread* thisThread) {
-    return VALUE_DRAW + Value(2 * (thisThread->nodes & 1) - 1);
+    return VALUE_DRAW - Value((thisThread->nodes & 3));
   }
 
   // Skill structure is used to implement strength limit
