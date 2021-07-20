d263098b1f090244ee1201aa8394a3cee807e3c7
JackWright347 <86283562+JackWright347@users.noreply.github.com>
2021-07-20 10:32:05 +0200

    make MSVC build compile again after recent master repo changes

--- src/nnue/nnue_feature_transformer.h
+++ src/nnue/nnue_feature_transformer.h
@@ -123,8 +123,10 @@ namespace Stockfish::Eval::NNUE {
       // We use __m* types as template arguments, which causes GCC to emit warnings
       // about losing some attribute information. This is irrelevant to us as we
       // only take their size, so the following pragma are harmless.
+  #ifdef __GNUC__
       #pragma GCC diagnostic push
       #pragma GCC diagnostic ignored "-Wignored-attributes"
+  #endif
 
       template <typename SIMDRegisterType,
                 typename LaneType,
@@ -157,7 +159,9 @@ namespace Stockfish::Eval::NNUE {
       static constexpr int NumRegs     = BestRegisterCount<vec_t, WeightType, TransformedFeatureDimensions, NumRegistersSIMD>();
       static constexpr int NumPsqtRegs = BestRegisterCount<psqt_vec_t, PSQTWeightType, PSQTBuckets, NumRegistersSIMD>();
 
+  #ifdef __GNUC__
       #pragma GCC diagnostic pop
+  #endif
 
   #endif
 
