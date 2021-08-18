1946a675677aee8e485b75525bd06184badc79f2
Torsten Hellwig <torsten.hellwig93@gmail.com>
2021-08-18 09:12:14 +0200

    Update default net to nn-ac5605a608d6.nnue
    This net was created with the nnue-pytorch trainer, it used the previous master net as a starting point.
    
    The training data includes all T60 data (https://drive.google.com/drive/folders/1rzZkgIgw7G5vQMLr2hZNiUXOp7z80613), all T74 data (https://drive.google.com/drive/folders/1aFUv3Ih3-A8Vxw9064Kw_FU4sNhMHZU-) and the wrongNNUE_02_d9.binpack (https://drive.google.com/file/d/1seGNOqcVdvK_vPNq98j-zV3XPE5zWAeq). The Leela data were randomly named and then concatenated. All data was merged into one binpack using interleave_binpacks.py.
    
    python3 train.py \
        ../data/t60_t74_wrong.binpack \
        ../data/t60_t74_wrong.binpack \
        --resume-from-model ../data/nn-e8321e467bf6.pt \
        --gpus 1 \
        --threads 4 \
        --num-workers 1 \
        --batch-size 16384 \
        --progress_bar_refresh_rate 300 \
        --random-fen-skipping 3 \
        --features=HalfKAv2_hm^ \
        --lambda=1.0 \
        --max_epochs=600 \
        --seed $RANDOM \
        --default_root_dir ../output/exp_24
    
    STC:
    LLR: 2.95 (-2.94,2.94) <-0.50,2.50>
    Total: 15320 W: 1415 L: 1257 D: 12648
    Ptnml(0-2): 50, 1002, 5402, 1152, 54
    https://tests.stockfishchess.org/tests/view/611c404a4977aa1525c9c97f
    
    LTC:
    LLR: 2.94 (-2.94,2.94) <0.50,3.50>
    Total: 9440 W: 345 L: 248 D: 8847
    Ptnml(0-2): 3, 222, 4175, 315, 5
    https://tests.stockfishchess.org/tests/view/611c6c7d4977aa1525c9c996
    
    LTC with UHO_XXL_+0.90_+1.19.epd:
    LLR: 2.94 (-2.94,2.94) <0.50,3.50>
    Total: 6232 W: 1638 L: 1459 D: 3135
    Ptnml(0-2): 5, 592, 1744, 769, 6
    https://tests.stockfishchess.org/tests/view/611c9b214977aa1525c9c9cb
    
    closes https://github.com/official-stockfish/Stockfish/pull/3664
    
    Bench: 5375286

--- src/evaluate.h
+++ src/evaluate.h
@@ -39,7 +39,7 @@ namespace Eval {
   // The default net name MUST follow the format nn-[SHA256 first 12 digits].nnue
   // for the build process (profile-build and fishtest) to work. Do not change the
   // name of the macro, as it is used in the Makefile.
-  #define EvalFileDefaultName   "nn-e8321e467bf6.nnue"
+  #define EvalFileDefaultName   "nn-ac5605a608d6.nnue"
 
   namespace NNUE {
 
