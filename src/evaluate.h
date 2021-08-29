2807dcfab671bfc7a1bea79f5639dbbd505703ad
SFisGOD <jonathandumale@gmail.com>
2021-08-30 00:19:46 +0800

    Update default net to nn-735bba95dec0.nnue
    SPSA 1: https://tests.stockfishchess.org/tests/view/61286d8b62d20cf82b5ad1bd
    Parameters: A total of 256 net biases were tuned (hidden layer 2)
    Base net: nn-33495fe25081.nnue
    New net: nn-83e3cf2af92b.nnue
    
    SPSA 2: https://tests.stockfishchess.org/tests/view/6129cf2162d20cf82b5ad25f
    Parameters: A total of 64 net biases were tuned (hidden layer 1)
    Base net: nn-83e3cf2af92b.nnue
    New net: nn-69a528eaef35.nnue
    
    SPSA 3: https://tests.stockfishchess.org/tests/view/612a0dcb62d20cf82b5ad2a0
    Parameters: 256 net weights and 8 net biases (output layer)
    Base net: nn-69a528eaef35.nnue
    New net: nn-735bba95dec0.nnue
    
    STC:
    LLR: 2.94 (-2.94,2.94) <-0.50,2.50>
    Total: 95144 W: 24310 L: 23999 D: 46835
    Ptnml(0-2): 232, 11059, 24748, 11232, 301
    https://tests.stockfishchess.org/tests/view/612bb3be0fdf40644b4b9996
    
    LTC:
    LLR: 2.94 (-2.94,2.94) <0.50,3.50>
    Total: 33632 W: 8522 L: 8271 D: 16839
    Ptnml(0-2): 18, 3511, 9516, 3744, 27
    https://tests.stockfishchess.org/tests/view/612ce5b9bb4956d8b78eb5b3
    
    Closes https://github.com/official-stockfish/Stockfish/pull/3685
    
    Bench: 5600615

--- src/evaluate.h
+++ src/evaluate.h
@@ -39,7 +39,7 @@ namespace Eval {
   // The default net name MUST follow the format nn-[SHA256 first 12 digits].nnue
   // for the build process (profile-build and fishtest) to work. Do not change the
   // name of the macro, as it is used in the Makefile.
-  #define EvalFileDefaultName   "nn-33495fe25081.nnue"
+  #define EvalFileDefaultName   "nn-735bba95dec0.nnue"
 
   namespace NNUE {
 
