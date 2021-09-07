8a8c10435dd4f237062ed76a5480319b6fb6f347
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-09-07 13:22:20 +0200

    Further improve history updates
    Now even double history updates if a search failed low at an expected PV or CUT node.
    
    STC:
    LLR: 2.93 (-2.94,2.94) <-0.50,2.50>
    Total: 30736 W: 7891 L: 7674 D: 15171
    Ptnml(0-2): 90, 3477, 8017, 3694, 90
    https://tests.stockfishchess.org/tests/view/61364ae30cd98ab40c0c9da5
    
    LTC:
    LLR: 2.94 (-2.94,2.94) <0.50,3.50>
    Total: 73600 W: 18684 L: 18326 D: 36590
    Ptnml(0-2): 41, 7734, 20899, 8078, 48
    https://tests.stockfishchess.org/tests/view/6136940f0cd98ab40c0c9df3
    
    Bench: 6030657

--- src/search.cpp
+++ src/search.cpp
@@ -1325,7 +1325,7 @@ moves_loop: // When in check, search starts here
     // Bonus for prior countermove that caused the fail low
     else if (   (depth >= 3 || PvNode)
              && !priorCapture)
-        update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth + (PvNode || cutNode)));
+        update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth) * (1 + (PvNode || cutNode)));
 
     if (PvNode)
         bestValue = std::min(bestValue, maxValue);
