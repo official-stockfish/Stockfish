fd5e77950ea0742fb815b1b92f7ed9ac625d66cf
xoto10 <buylow001@gmail.com>
2021-09-12 09:19:38 +0100

    Update 2 search parameters after tune.
    A tuning run on 3 search parameters was done with 200k games, narrow ranges (50-150%) and a small value for A (3% of total games) :
    https://tests.stockfishchess.org/tests/view/613b5f4b689039fce12e1220
    
    STC 10+0.1 :
    LLR: 2.95 (-2.94,2.94) <-0.50,2.50>
    Total: 73112 W: 18800 L: 18520 D: 35792
    Ptnml(0-2): 205, 8395, 19115, 8597, 244
    https://tests.stockfishchess.org/tests/view/613cb8d2689039fce12e1308
    
    LTC 60+0.6 :
    LLR: 2.95 (-2.94,2.94) <0.50,3.50>
    Total: 45616 W: 11604 L: 11321 D: 22691
    Ptnml(0-2): 24, 4769, 12946, 5038, 31
    https://tests.stockfishchess.org/tests/view/613d07048253e53e97b55b32
    
    closes https://github.com/official-stockfish/Stockfish/pull/3698
    
    Bench 6504816

--- src/search.cpp
+++ src/search.cpp
@@ -793,7 +793,7 @@ namespace {
         && (ss-1)->statScore < 23767
         &&  eval >= beta
         &&  eval >= ss->staticEval
-        &&  ss->staticEval >= beta - 20 * depth - 22 * improving + 168 * ss->ttPv + 159
+        &&  ss->staticEval >= beta - 20 * depth - 22 * improving + 168 * ss->ttPv + 177
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))
@@ -1032,7 +1032,7 @@ moves_loop: // When in check, search starts here
               // Futility pruning: parent node (~5 Elo)
               if (   !ss->inCheck
                   && lmrDepth < 7
-                  && ss->staticEval + 174 + 157 * lmrDepth <= alpha)
+                  && ss->staticEval + 172 + 157 * lmrDepth <= alpha)
                   continue;
 
               // Prune moves with negative SEE (~20 Elo)
@@ -1101,8 +1101,8 @@ moves_loop: // When in check, search starts here
       }
 
       // Capture extensions for PvNodes and cutNodes
-      else if (   (PvNode || cutNode) 
-               && captureOrPromotion 
+      else if (   (PvNode || cutNode)
+               && captureOrPromotion
                && moveCount != 1)
           extension = 1;
 
