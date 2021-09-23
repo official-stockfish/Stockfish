cd519384eaccd4f0c7e07d1cbd75e7d5c5abc6c9
OfekShochat <o@shoch.at>
2021-09-23 23:16:17 +0300

    adding the reduction

--- src/search.cpp
+++ src/search.cpp
@@ -72,9 +72,9 @@ namespace {
   // Reductions lookup table, initialized at startup
   int Reductions[MAX_MOVES]; // [depth or moveNumber]
 
-  Depth reduction(bool i, Depth d, int mn) {
+  Depth reduction(bool i, Depth d, int mn, bool b) {
     int r = Reductions[d] * Reductions[mn];
-    return (r + 534) / 1024 + (!i && r > 904);
+    return (r + 534) / 1024 + (!i && r > 904) + b;
   }
 
   constexpr int futility_move_count(bool improving, Depth depth) {
@@ -918,6 +918,7 @@ moves_loop: // When in check, search starts here
 
     ttCapture = ttMove && pos.capture_or_promotion(ttMove);
 
+    bool b = false;
     // Step 11. A small Probcut idea, when we are in check
     probCutBeta = beta + 409;
     if (   ss->inCheck
@@ -1005,7 +1006,7 @@ moves_loop: // When in check, search starts here
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
-          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), 0);
+          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, b), 0);
 
           if (   captureOrPromotion
               || givesCheck)
@@ -1140,7 +1141,7 @@ moves_loop: // When in check, search starts here
               || !ss->ttPv)
           && (!PvNode || ss->ply > 1 || thisThread->id() % 4 != 3))
       {
-          Depth r = reduction(improving, depth, moveCount);
+          Depth r = reduction(improving, depth, moveCount, b);
 
           if (PvNode)
               r--;
@@ -1227,6 +1228,9 @@ moves_loop: // When in check, search starts here
 
           value = -search<PV>(pos, ss+1, -beta, -alpha,
                               std::min(maxNextDepth, newDepth), false);
+          if (value > ss->staticEval) {
+              b = true;
+          }
       }
 
       // Step 18. Undo move
