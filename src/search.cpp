ff3fa0c664a5799e5531b0908018c59633d761bf
Stéphane Nicolet <cassio@free.fr>
2021-09-23 11:20:03 +0200

    Tweak doubly singular condition (Topo's patch)
    This patch relax a little bit the condition for doubly singular moves
    (ie moves that are so forced that we think that they deserve a local
    double extension of the search). We lower the margin and allow up to
    six such double extensions in the path between the root and the critical
    node.
    
    Original idea by Siad Daboul (@TopoIogist) in PR #3709
    
    Tested with the previous commit:
    
    passed STC:
    LLR: 2.94 (-2.94,2.94) <-0.50,2.50>
    Total: 33048 W: 8458 L: 8236 D: 16354
    Ptnml(0-2): 120, 3701, 8660, 3923, 120
    https://tests.stockfishchess.org/tests/view/614b24347bdc23e77ceb88fe
    
    passed LTC:
    LLR: 2.95 (-2.94,2.94) <0.50,3.50>
    Total: 54176 W: 13712 L: 13406 D: 27058
    Ptnml(0-2): 36, 5653, 15399, 5969, 31
    https://tests.stockfishchess.org/tests/view/614b3b727bdc23e77ceb8911
    
    closes https://github.com/official-stockfish/Stockfish/pull/3714
    
    Bench: 5792377

--- src/search.cpp
+++ src/search.cpp
@@ -1107,8 +1107,8 @@ moves_loop: // When in check, search starts here
 
               // Avoid search explosion by limiting the number of double extensions
               if (   !PvNode
-                  && value < singularBeta - 93
-                  && ss->doubleExtensions < 3)
+                  && value < singularBeta - 75
+                  && ss->doubleExtensions <= 6)
               {
                   extension = 2;
                   noLMRExtension = true;
