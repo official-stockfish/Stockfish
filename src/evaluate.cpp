c968e7c5e0e046dd408c6e69bff81fb31f5d6188
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2021-10-19 23:02:57 +0200

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1084,10 +1084,10 @@ Value Eval::evaluate(const Position& pos) {
   // Deciding between classical and NNUE eval: for high PSQ imbalance we use classical,
   // but we switch to NNUE during long shuffling or with high material on the board.
 
-  if (  !useNNUE
-      || abs(eg_value(pos.psq_score())) * 5 > (850 + pos.non_pawn_material() / 64) * (5 + pos.rule50_count()))
-      v = Evaluation<NO_TRACE>(pos).value();          // classical
-  else
+  //if (  !useNNUE
+  //    || abs(eg_value(pos.psq_score())) * 5 > (850 + pos.non_pawn_material() / 64) * (5 + pos.rule50_count()))
+  //    v = Evaluation<NO_TRACE>(pos).value();          // classical
+  //else
   {
       int scale =   883
                   + 32 * pos.count<PAWN>()
