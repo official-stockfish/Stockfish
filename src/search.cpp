8557f35aa5c0f1ea45b2ccba5ebfd0165dbd33da
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-24 16:07:00 +0200

    Double extend search even more via LMR
    Allow now for the first five moves a two plies deeper LMR search.
    
    STC:
    LLR: 2.96 (-2.94,2.94) <-2.50,0.50>
    Total: 99608 W: 25143 L: 25115 D: 49350
    Ptnml(0-2): 291, 11444, 26328, 11428, 313
    https://tests.stockfishchess.org/tests/view/61718c9438cb9784038af8d7
    
    LTC:
    LLR: 2.95 (-2.94,2.94) <-2.50,0.50>
    Total: 52064 W: 13234 L: 13145 D: 25685
    Ptnml(0-2): 35, 5431, 15014, 5514, 38
    https://tests.stockfishchess.org/tests/view/6171e13e38cb9784038af928
    
    closes https://github.com/official-stockfish/Stockfish/pull/3760
    
    Bench: 7222293

--- src/search.cpp
+++ src/search.cpp
@@ -1210,8 +1210,7 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1             ? 0
-                       : moveCount <= 3      ? 2
-                       : moveCount <= 5      ? 1
+                       : moveCount <= 5      ? 2
                        : PvNode && depth > 6 ? 1
                        :                       0;
 
