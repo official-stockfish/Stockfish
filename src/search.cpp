4c35960d8427316f0123e39bfb351233540c52a5
xefoci7612 <empty>
2021-11-07 10:32:53 +0100

    Weakness from 160

--- src/search.cpp
+++ src/search.cpp
@@ -1775,7 +1775,7 @@ moves_loop: // When in check, search starts here
     Value topScore = rootMoves[0].score;
     int delta = std::min(topScore - rootMoves[multiPV - 1].score, PawnValueMg);
     int maxScore = -VALUE_INFINITE;
-    double weakness = 120 - 2 * level;
+    double weakness = 160 - 4 * level;
 
     float mean = delta * weakness / 2;
     float stddev = mean * 0.3;
