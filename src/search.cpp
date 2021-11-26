282644f1413db0ccb58e2e1793d5007c58ff8ce2
pb00067 <pb00067@PHXL0356.wp.lan>
2021-11-26 08:03:15 +0100

    Remove depth dependence and use same limit (2000) as stat_bonus
    STC:
    https://tests.stockfishchess.org/tests/view/619df59dc0a4ea18ba95a424
    LLR: 2.96 (-2.94,2.94) <-2.25,0.25>
    Total: 83728 W: 21329 L: 21242 D: 41157
    Ptnml(0-2): 297, 9669, 21847, 9752, 299
    
    LTC:
    https://tests.stockfishchess.org/tests/view/619e64d7c0a4ea18ba95a475
    LLR: 2.94 (-2.94,2.94) <-2.25,0.25>
    Total: 79888 W: 20238 L: 20155 D: 39495
    Ptnml(0-2): 57, 8391, 22980, 8444, 73
    
    closes https://github.com/official-stockfish/Stockfish/pull/3806
    
    bench: 6792010

--- src/search.cpp
+++ src/search.cpp
@@ -801,7 +801,7 @@ namespace {
     // Use static evaluation difference to improve quiet move ordering
     if (is_ok((ss-1)->currentMove) && !(ss-1)->inCheck && !priorCapture)
     {
-        int bonus = std::clamp(-depth * 4 * int((ss-1)->staticEval + ss->staticEval), -1000, 1000);
+        int bonus = std::clamp(-16 * int((ss-1)->staticEval + ss->staticEval), -2000, 2000);
         thisThread->mainHistory[~us][from_to((ss-1)->currentMove)] << bonus;
     }
 
