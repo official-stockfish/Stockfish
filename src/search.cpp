95a2ac1e073a6f7c3af8aeef2fc4fe8cbd6650cf
bmc4 <bmc4@cin.ufpe.br>
2021-11-29 09:30:07 -0300

    Simplify reduction on rootNode when bestMoveChanges is high
    The reduction introduced in #3736 also consider on rootNode, so we don't have to reduce again.
    
    STC:
    LLR: 2.94 (-2.94,2.94) <-2.25,0.25>
    Total: 28736 W: 7494 L: 7329 D: 13913
    Ptnml(0-2): 95, 3247, 7503, 3444, 79
    https://tests.stockfishchess.org/tests/view/61a3abe01b7fdf52228e74d8
    
    LTC:
    LLR: 2.94 (-2.94,2.94) <-2.25,0.25>
    Total: 47816 W: 12434 L: 12308 D: 23074
    Ptnml(0-2): 37, 4972, 13755, 5116, 28
    https://tests.stockfishchess.org/tests/view/61a3c3e39f0c43dae1c71d71
    
    closes https://github.com/official-stockfish/Stockfish/pull/3817
    
    bench: 6331638

--- src/search.cpp
+++ src/search.cpp
@@ -1185,8 +1185,8 @@ moves_loop: // When in check, search starts here
               && !likelyFailLow)
               r -= 2;
 
-          // Increase reduction at root and non-PV nodes when the best move does not change frequently
-          if (   (rootNode || !PvNode)
+          // Increase reduction at non-PV nodes when the best move does not change frequently
+          if (  !PvNode
               && thisThread->bestMoveChanges <= 2)
               r++;
 
