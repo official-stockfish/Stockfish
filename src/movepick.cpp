c15d2d1177c16f4f6169166d3b6593db2ffb41cc
pb00067 <pb00067@PHXL0356.wp.lan>
2021-12-15 08:49:44 +0100

    Remove lowPly history
    Seems that after pull #3731 (Capping stat bonus at 2000) this
    heuristic is no more useful.
    
    STC:
    https://tests.stockfishchess.org/tests/view/61b8d0e2dffbe89a35815444
    LLR: 2.94 (-2.94,2.94) <-2.25,0.25>
    Total: 30672 W: 7974 L: 7812 D: 14886
    Ptnml(0-2): 106, 3436, 8072, 3634, 88
    
    LTC:
    https://tests.stockfishchess.org/tests/view/61b8e90cdffbe89a35815a67
    LLR: 2.94 (-2.94,2.94) <-2.25,0.25>
    Total: 42448 W: 10884 L: 10751 D: 20813
    Ptnml(0-2): 23, 4394, 12267, 4507, 33
    
    bench: 4801890

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -56,10 +56,10 @@ namespace {
 /// ordering is at the current node.
 
 /// MovePicker constructor for the main search
-MovePicker::MovePicker(const Position& p, Move ttm, Depth d, const ButterflyHistory* mh, const LowPlyHistory* lp,
-                       const CapturePieceToHistory* cph, const PieceToHistory** ch, Move cm, const Move* killers, int pl)
-           : pos(p), mainHistory(mh), lowPlyHistory(lp), captureHistory(cph), continuationHistory(ch),
-             ttMove(ttm), refutations{{killers[0], 0}, {killers[1], 0}, {cm, 0}}, depth(d), ply(pl) {
+MovePicker::MovePicker(const Position& p, Move ttm, Depth d, const ButterflyHistory* mh,
+                       const CapturePieceToHistory* cph, const PieceToHistory** ch, Move cm, const Move* killers)
+           : pos(p), mainHistory(mh), captureHistory(cph), continuationHistory(ch),
+             ttMove(ttm), refutations{{killers[0], 0}, {killers[1], 0}, {cm, 0}}, depth(d) {
 
   assert(d > 0);
 
@@ -110,8 +110,7 @@ void MovePicker::score() {
                    + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
-                   +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
-                   + (ply < MAX_LPH ? 6 * (*lowPlyHistory)[ply][from_to(m)] : 0);
+                   +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)];
 
       else // Type == EVASIONS
       {
--- src/thread.cpp
+++ src/thread.cpp
@@ -59,7 +59,6 @@ void Thread::clear() {
 
   counterMoves.fill(MOVE_NONE);
   mainHistory.fill(0);
-  lowPlyHistory.fill(0);
   captureHistory.fill(0);
 
   for (bool inCheck : { false, true })
