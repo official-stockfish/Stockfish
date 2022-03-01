6903ef5dd42f2d15b29830405e36f3c22ec653cb
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2022-03-01 09:34:40 +0100

    Sort captures
    This patch (partially) sort captures in analogy to quiet moves. All
    three movepickers are affected, hence `depth` is added as an argument in
    probcut's.
    
    Passed STC:
    https://tests.stockfishchess.org/tests/view/621a4576da649bba32ef6fd4
    LLR: 2.95 (-2.94,2.94) <0.00,2.50>
    Total: 103848 W: 27884 L: 27473 D: 48491
    Ptnml(0-2): 587, 11691, 26974, 12068, 604
    
    Passed LTC:
    https://tests.stockfishchess.org/tests/view/621aaa5bda649bba32ef7c2d
    LLR: 2.96 (-2.94,2.94) <0.50,3.00>
    Total: 212032 W: 56420 L: 55739 D: 99873
    Ptnml(0-2): 198, 21310, 62348, 21933, 227
    
    Bench: 6833580

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -87,8 +87,8 @@ MovePicker::MovePicker(const Position& p, Move ttm, Depth d, const ButterflyHist
 
 /// MovePicker constructor for ProbCut: we generate captures with SEE greater
 /// than or equal to the given threshold.
-MovePicker::MovePicker(const Position& p, Move ttm, Value th, const CapturePieceToHistory* cph)
-           : pos(p), captureHistory(cph), ttMove(ttm), threshold(th)
+MovePicker::MovePicker(const Position& p, Move ttm, Value th, Depth d, const CapturePieceToHistory* cph)
+           : pos(p), captureHistory(cph), ttMove(ttm), threshold(th), depth(d)
 {
   assert(!pos.checkers());
 
@@ -169,11 +169,12 @@ top:
       endMoves = generate<CAPTURES>(pos, cur);
 
       score<CAPTURES>();
+      partial_insertion_sort(cur, endMoves, -3000 * depth);
       ++stage;
       goto top;
 
   case GOOD_CAPTURE:
-      if (select<Best>([&](){
+      if (select<Next>([&](){
                        return pos.see_ge(*cur, Value(-69 * cur->value / 1024)) ?
                               // Move losing capture to endBadCaptures to be tried later
                               true : (*endBadCaptures++ = *cur, false); }))
@@ -241,10 +242,10 @@ top:
       return select<Best>([](){ return true; });
 
   case PROBCUT:
-      return select<Best>([&](){ return pos.see_ge(*cur, threshold); });
+      return select<Next>([&](){ return pos.see_ge(*cur, threshold); });
 
   case QCAPTURE:
-      if (select<Best>([&](){ return   depth > DEPTH_QS_RECAPTURES
+      if (select<Next>([&](){ return   depth > DEPTH_QS_RECAPTURES
                                     || to_sq(*cur) == recaptureSquare; }))
           return *(cur - 1);
 
