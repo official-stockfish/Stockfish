e31f97e3baa52042fe60d6f4eeb50fe0d9e61013
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-03-15 16:53:59 +0100

    Remove ttPv tree shrinking.
    Via the ttPv flag an implicit tree of current and former PV nodes is maintained. In addition this tree is grown or shrinked at the leafs dependant on the search results. But now the shrinking step has been removed.
    
    As the frequency of ttPv nodes decreases with depth the shown scaling behavior (STC barely passed but LTC scales well) of the tests was expected.
    
    STC:
    LLR: 2.93 (-2.94,2.94) <-2.25,0.25>
    Total: 270408 W: 71593 L: 71785 D: 127030
    Ptnml(0-2): 1339, 31024, 70630, 30912, 1299
    https://tests.stockfishchess.org/tests/view/622fbf9dc9e950cbfc2376d6
    
    LTC:
    LLR: 2.96 (-2.94,2.94) <-2.25,0.25>
    Total: 34368 W: 9135 L: 8992 D: 16241
    Ptnml(0-2): 28, 3423, 10135, 3574, 24
    https://tests.stockfishchess.org/tests/view/62305257c9e950cbfc238964
    
    closes https://github.com/official-stockfish/Stockfish/pull/3963
    
    Bench: 7044203

--- src/search.cpp
+++ src/search.cpp
@@ -1359,10 +1359,6 @@ moves_loop: // When in check, search starts here
     // opponent move is probably good and the new position is added to the search tree.
     if (bestValue <= alpha)
         ss->ttPv = ss->ttPv || ((ss-1)->ttPv && depth > 3);
-    // Otherwise, a counter move has been found and if the position is the last leaf
-    // in the search tree, remove the position from the search tree.
-    else if (depth > 3)
-        ss->ttPv = ss->ttPv && (ss+1)->ttPv;
 
     // Write gathered information in transposition table
     if (!excludedMove && !(rootNode && thisThread->pvIdx))
