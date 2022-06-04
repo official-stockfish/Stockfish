bc4660548702a80fc1664bf31d0e16db003867ca
movepicker <106845693+movepicker@users.noreply.github.com>
2022-06-04 03:30:16 -0700

    Update thread.cpp

--- src/thread.cpp
+++ src/thread.cpp
@@ -171,13 +171,14 @@ void ThreadPool::clear() {
 /// returns immediately. Main thread will wake up other threads and start the search.
 
 void ThreadPool::start_thinking(Position& pos, StateListPtr& states,
-                                const Search::LimitsType& limits, bool ponderMode) {
+                                const Search::LimitsType& limits, bool ponderMode,  bool brainmode) {
 
   main()->wait_for_search_finished();
 
   main()->stopOnPonderhit = stop = false;
   increaseDepth = true;
   main()->ponder = ponderMode;
+  main()->brain = brainmode;
   Search::Limits = limits;
   Search::RootMoves rootMoves;
 
