330b6aea664273b071038548b8100f3dc523b7a2
johndoknjas <jdoknjas@sfu.ca>
2022-06-23 23:54:00 -0700

    Only use the pragma if GCC is the compiler being used.

--- src/nnue/nnue_feature_transformer.h
+++ src/nnue/nnue_feature_transformer.h
@@ -283,8 +283,9 @@ namespace Stockfish::Eval::NNUE {
           - psqtAccumulation[perspectives[1]][bucket]
         ) / 2;
 
-
+#if (defined(__GNUC__) && !defined(__clang__) && !defined(__INTEL_COMPILER))
 #pragma GCC unroll 2
+#endif
       for (IndexType p = 0; p < 2; ++p)
       {
           const IndexType offset = (HalfDimensions / 2) * p;
@@ -302,7 +303,9 @@ namespace Stockfish::Eval::NNUE {
           const vec_t* in1 = reinterpret_cast<const vec_t*>(&(accumulation[perspectives[p]][HalfDimensions / 2]));
                 vec_t* out = reinterpret_cast<      vec_t*>(output + offset);
 
-          #pragma GCC unroll NumOutputChunks
+#if (defined(__GNUC__) && !defined(__clang__) && !defined(__INTEL_COMPILER))
+#pragma GCC unroll NumOutputChunks
+#endif
           for (IndexType j = 0; j < NumOutputChunks; j += 1)
           {
               const vec_t sum0a = vec_max_16(vec_min_16(in0[j * 2 + 0], One), Zero);
