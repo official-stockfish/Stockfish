5337539803b461bee989b15bac7c757be2d9618f
Michael Chaly <Vizvezdenec@gmail.com>
2022-08-07 13:09:21 +0300

    bench 5921315

--- src/search.cpp
+++ src/search.cpp
@@ -1059,7 +1059,6 @@ moves_loop: // When in check, search starts here
           if (   !rootNode
               &&  depth >= 4 - (thisThread->previousDepth > 27) + 2 * (PvNode && tte->is_pv())
               &&  move == ttMove
-              && !excludedMove // Avoid recursive singular search
            /* &&  ttValue != VALUE_NONE Already implicit in the next condition */
               &&  abs(ttValue) < VALUE_KNOWN_WIN
               && (tte->bound() & BOUND_LOWER)
