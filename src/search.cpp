8c3d4c2745bcd7efd436509b06b6cc249f28be4b
Michael Chaly <Vizvezdenec@gmail.com>
2022-08-15 08:52:55 +0300

    bench 5868987

--- src/search.cpp
+++ src/search.cpp
@@ -636,7 +636,7 @@ namespace {
     // At non-PV nodes we check for an early TT cutoff
     if (  !PvNode
         && ss->ttHit
-        && tte->depth() > depth - ((int)thisThread->id() & 0x1) - (tte->bound() == BOUND_EXACT)
+        && tte->depth() > depth - (tte->bound() == BOUND_EXACT)
         && ttValue != VALUE_NONE // Possible in case of TT access race
         && (tte->bound() & (ttValue >= beta ? BOUND_LOWER : BOUND_UPPER)))
     {
