9fa258ee1d36d5a432a7a3e857e0723d439528b0
Dubslow <bunslow@gmail.com>
2022-08-19 22:49:29 -0500

    Razor also on PV nodes
    Simplification introduced by xoto10
    
    blue LTC vs new master:
    https://tests.stockfishchess.org/tests/view/631ad4ef9cfa5e9b648d1b4e
    LLR: 2.93 (-2.94,2.94) <-1.75,0.25>
    Total: 59184 W: 16002 L: 15828 D: 27354
    Ptnml(0-2): 65, 5777, 17747, 5925, 78
    
    blue STC vs old master:
    https://tests.stockfishchess.org/tests/view/6306b87b902a848543334c25
    LLR: 2.94 (-2.94,2.94) <-1.75,0.25>
    Total: 213944 W: 57184 L: 57159 D: 99601
    Ptnml(0-2): 877, 23448, 58331, 23405, 911
    
    blue LTC vs old master:
    https://tests.stockfishchess.org/tests/view/63070e6b902a8485433357e7
    LLR: 2.94 (-2.94,2.94) <-1.75,0.25>
    Total: 192080 W: 52050 L: 52006 D: 88024
    Ptnml(0-2): 232, 18981, 57611, 18943, 273
    
    closes https://github.com/official-stockfish/Stockfish/pull/4147
    
    bench 4208975

--- src/search.cpp
+++ src/search.cpp
@@ -776,8 +776,7 @@ namespace {
     // Step 7. Razoring.
     // If eval is really low check with qsearch if it can exceed alpha, if it can't,
     // return a fail low.
-    if (   !PvNode
-        && depth <= 7
+    if (   depth <= 7
         && eval < alpha - 369 - 254 * depth * depth)
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
