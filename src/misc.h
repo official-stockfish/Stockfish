5b35cdeb208c6c7451432ce15c934233c7fef276
hongzhi-cheng <52321104@qq.com>
2022-09-16 23:08:20 +0800

    Simplify mul_hi64
    Simplify mul_hi64

--- src/misc.h
+++ src/misc.h
@@ -197,7 +197,6 @@ inline uint64_t mul_hi64(uint64_t a, uint64_t b) {
     uint64_t aL = (uint32_t)a, aH = a >> 32;
     uint64_t bL = (uint32_t)b, bH = b >> 32;
     uint64_t c1 = (aL * bL) >> 32;
-    uint64_t c2 = aH * bL + c1;
     uint64_t c3 = aL * bH + (uint32_t)c2;
     return aH * bH + (c2 >> 32) + (c3 >> 32);
 #endif
