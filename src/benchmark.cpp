45820aa385e3784e6dc6db89f227a7b832335e6e
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2022-11-24 02:39:34 +0100

    Update benchmark.cpp

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -6,7 +6,7 @@
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-
+ 
   Stockfish is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
