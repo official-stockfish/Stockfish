c620886181b5c0fb4a0c067b4173143a81819199
Linmiao Xu <linmiao.xu@gmail.com>
2022-12-19 16:10:22 -0500

    Update default net to nn-335a9b2d8a80.nnue
    Created by retraining the master net with a combination of:
    
        the previous best dataset (Leela-dfrc_n5000.binpack), with about half the dataset filtered using depth6 multipv2 search to throw away positions where either of the 2 best moves are captures
        Leela T80 Oct and Nov training data rescored with best moves, adding ~9.5 billion positions
    
    Trained effectively the same way as the previous master net:
    
    python3 easy_train.py \
      --experiment-name=leela-dfrc-filtered-T80-oct-nov \
      --training-dataset=/data/leela-dfrc-filtered-T80-oct-nov.binpack \
      --start-from-engine-test-net True \
      --gpus="0," \
      --start-lambda=1.0 \
      --end-lambda=0.75 \
      --gamma=0.995 \
      --lr=4.375e-4 \
      --tui=False \
      --seed=$RANDOM \
      --max_epoch=800 \
      --auto-exit-timeout-on-training-finished=900 \
      --network-testing-threads 20 \
      --num-workers 6
    
    Local testing at a fixed 25k nodes:
    experiments/experiment_leela-dfrc-filtered-T80-oct-nov/training/run_0/nn-epoch779.nnue
    localElo: run_0/nn-epoch779.nnue : 4.7 +/- 3.1
    
    The new Leela T80 part of the dataset was prepared by downloading test80 training data from all of Oct 2022 and Nov 2022, rescoring with syzygy 6-piece tablebases and ~600 GB of 7-piece tablebases, saving best moves to exported .plain files, removing all positions with castling flags, then converting to binpacks and using interleave_binpacks.py to merge them together. Scripts used in this data conversion process are available at:
    https://github.com/linrock/lc0-data-converter
    
    Filtering binpack data using depth6 multipv2 search was done by modifying transform.cpp in the tools branch:
    https://github.com/linrock/Stockfish/tree/tools-filter-multipv2-no-rescore
    
    Links for downloading the training data (total size: 338 GB) are available at:
    https://robotmoon.com/nnue-training-data/
    
    Passed STC:
    LLR: 2.94 (-2.94,2.94) <0.00,2.00>
    Total: 30544 W: 8244 L: 7947 D: 14353
    Ptnml(0-2): 93, 3243, 8302, 3542, 92
    https://tests.stockfishchess.org/tests/view/63a0d377264a0cf18f86f82b
    
    Passed LTC:
    LLR: 2.95 (-2.94,2.94) <0.50,2.50>
    Total: 32464 W: 8866 L: 8573 D: 15025
    Ptnml(0-2): 19, 3054, 9794, 3345, 20
    https://tests.stockfishchess.org/tests/view/63a10bc9fb452d3c44b1e016
    
    closes https://github.com/official-stockfish/Stockfish/pull/4295
    
    Bench 3554904

--- src/evaluate.h
+++ src/evaluate.h
@@ -39,7 +39,7 @@ namespace Eval {
   // The default net name MUST follow the format nn-[SHA256 first 12 digits].nnue
   // for the build process (profile-build and fishtest) to work. Do not change the
   // name of the macro, as it is used in the Makefile.
-  #define EvalFileDefaultName   "nn-ad9b42354671.nnue"
+  #define EvalFileDefaultName   "nn-335a9b2d8a80.nnue"
 
   namespace NNUE {
 
