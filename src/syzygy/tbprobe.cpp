b5cd44c3622d0dff4559ecae0ea3f244da0c3dd5
Jake Senne <26696846+w1wwwwww@users.noreply.github.com>
2023-01-03 23:54:41 -0600

    Undo last commit (it broke everything)

--- src/syzygy/tbprobe.cpp
+++ src/syzygy/tbprobe.cpp
@@ -201,12 +201,8 @@ public:
 
     // Memory map the file and check it.
     uint8_t* map(void** baseAddress, uint64_t* mapping, TBType type) {
-        struct stat buffer; // Needed for file existence check
-
         if (is_open())
             close(); // Need to re-open to get native file descriptor
-        else if (!stat(fname.c_str(), &buffer) == 0)
-            return;
 
 #ifndef _WIN32
         struct stat statbuf;
