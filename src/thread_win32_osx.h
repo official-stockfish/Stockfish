4cb0c68094f4dbfd8b71e29f0685be21ab4840c9
Stefano Di Martino <stefano.d@posteo.de>
2023-01-07 01:08:30 +0100

    Modernize code base a little bit
    Removed sprintf() which generated a warning, because of security reasons.
    Replace NULL with nullptr
    Replace typedef with using
    Do not inherit from std::vector. Use composition instead.
    optimize mutex-unlocking
    
    closes https://github.com/official-stockfish/Stockfish/pull/4327
    
    No functional change

--- src/thread_win32_osx.h
+++ src/thread_win32_osx.h
@@ -41,7 +41,7 @@ void* start_routine(void* ptr)
    P* p = reinterpret_cast<P*>(ptr);
    (p->first->*(p->second))(); // Call member function pointer
    delete p;
-   return NULL;
+   return nullptr;
 }
 
 class NativeThread {
@@ -56,7 +56,7 @@ public:
     pthread_attr_setstacksize(attr, TH_STACK_SIZE);
     pthread_create(&thread, attr, start_routine<T>, new P(obj, fun));
   }
-  void join() { pthread_join(thread, NULL); }
+  void join() { pthread_join(thread, nullptr); }
 };
 
 } // namespace Stockfish
@@ -65,7 +65,7 @@ public:
 
 namespace Stockfish {
 
-typedef std::thread NativeThread;
+using NativeThread = std::thread;
 
 } // namespace Stockfish
 
