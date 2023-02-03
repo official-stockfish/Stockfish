16371b4aff1264b8cdb4fde1fae75f2f17ec1d82
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-02-03 18:54:30 +0800

    Remove reduced LMR capture bonus Bench: 3982060

--- src/search.cpp
+++ src/search.cpp
@@ -1214,9 +1214,6 @@ moves_loop: // When in check, search starts here
               int bonus = value > alpha ?  stat_bonus(newDepth)
                                         : -stat_bonus(newDepth);
 
-              if (capture)
-                  bonus /= 6;
-
               update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
           }
       }
