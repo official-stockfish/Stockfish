996ab340d0f9f20032968c7c4b2dd0740fd083de
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2023-04-05 21:18:32 +0200

    Revert "Restore conflict"
    This reverts commit fd2caf703e4de90837883167c87ac4f21ffed1ac.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1063,7 +1063,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   // We use the much less accurate but faster Classical eval when the NNUE
   // option is set to false. Otherwise we use the NNUE eval unless the
   // PSQ advantage is decisive. (~4 Elo at STC, 1 Elo at LTC)
-  bool useClassical = !pureNNUE && (!useNNUE || abs(psq) > 2048);
+  bool useClassical = !useNNUE || abs(psq) > 2048;
 
   if (useClassical)
       v = Evaluation<NO_TRACE>(pos).value();
