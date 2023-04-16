c09d9c003b9c385c4c4d2169fe5a6deb98cebb60
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-04-16 08:15:53 +0800

    Simplify verification search Bench: 3334480

--- src/search.cpp
+++ src/search.cpp
@@ -824,9 +824,6 @@ namespace {
             if (nullValue >= VALUE_TB_WIN_IN_MAX_PLY)
                 nullValue = beta;
 
-            if (thisThread->nmpMinPly || (abs(beta) < VALUE_KNOWN_WIN && depth < 14))
-                return nullValue;
-
             assert(!thisThread->nmpMinPly); // Recursive verification is not allowed
 
             // Do verification search at high depths, with null move pruning disabled
