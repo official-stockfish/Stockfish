72d542f00026fd8437e6033c95802714e4cd45d1
candirufish <38038147+candirufish@users.noreply.github.com>
2023-04-28 19:05:56 +0200

    Adjust reductions
    Decrease further on cutNodes with tte->depth() >= depth + 3 condition.
    
    LTC: https://tests.stockfishchess.org/tests/view/644dc84bccf5e93df5e50c13
    LLR: 2.94 (-2.94,2.94) <0.50,2.50>
    Total: 155346 W: 42184 L: 41660 D: 71502
    Ptnml(0-2): 59, 14765, 47504, 15283, 62
    
    STC: https://tests.stockfishchess.org/tests/view/644d05de68e01d8194cd9bbb
    LLR: 2.93 (-2.94,2.94) <0.00,2.00>
    Total: 121888 W: 32868 L: 32444 D: 56576
    Ptnml(0-2): 332, 13273, 33343, 13631, 365
    
    closes https://github.com/official-stockfish/Stockfish/pull/4552
    
    bench: 3739675

--- src/search.cpp
+++ src/search.cpp
@@ -884,7 +884,7 @@ namespace {
     // Use qsearch if depth is equal or below zero (~9 Elo)
     if (    PvNode
         && !ttMove)
-        depth -= 2 + 2 * (ss->ttHit &&  tte->depth() >= depth);
+        depth -= 2 + 2 * (ss->ttHit && tte->depth() >= depth);
 
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
@@ -1141,9 +1141,10 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction if position is or has been on the PV
       // and node is not likely to fail low. (~3 Elo)
+      // Decrease further on cutNodes. (~1 Elo)
       if (   ss->ttPv
           && !likelyFailLow)
-          r -= 2;
+          r -= cutNode && tte->depth() >= depth + 3 ? 3 : 2;
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
       if ((ss-1)->moveCount > 7)
