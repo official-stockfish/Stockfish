21d6b69f7c8d0c0a71fe627714913a59d39a3b57
Linmiao Xu <linmiao.xu@gmail.com>
2023-04-29 15:45:22 -0400

    Update 7 eval and optimism params
    Params found using spsa at 30+0.3 with this tuning config:
    
    ```
    // evaluate.cpp
    int nnueOptScaleBase = 1001;
    int nnueComplexityMult = 406;
    int nnueComplexityOptOffset = 424;
    int evalOptComplexityOffset = 272;
    int evalOptScaleOffset = 748;
    TUNE(SetRange(801, 1201), nnueOptScaleBase);
    TUNE(SetRange(306, 506), nnueComplexityMult);
    TUNE(SetRange(324, 524), nnueComplexityOptOffset);
    TUNE(SetRange(172, 372), evalOptComplexityOffset);
    TUNE(SetRange(648, 848), evalOptScaleOffset);
    
    // search.cpp
    int searchOptBase = 120;
    int searchOptDenom = 161;
    TUNE(SetRange(20, 220), searchOptBase);
    TUNE(SetRange(111, 211), searchOptDenom);
    ```
    
    Passed STC:
    https://tests.stockfishchess.org/tests/view/644dda8accf5e93df5e50cbe
    LLR: 2.93 (-2.94,2.94) <0.00,2.00>
    Total: 136800 W: 36682 L: 36237 D: 63881
    Ptnml(0-2): 353, 14910, 37492, 15229, 416
    
    Passed LTC:
    https://tests.stockfishchess.org/tests/view/644eaedb3f31c3bbe4a3d345
    LLR: 2.94 (-2.94,2.94) <0.50,2.50>
    Total: 64548 W: 17624 L: 17272 D: 29652
    Ptnml(0-2): 33, 6112, 19631, 6466, 32
    
    closes https://github.com/official-stockfish/Stockfish/pull/4550
    
    bench 3670343

--- src/search.cpp
+++ src/search.cpp
@@ -353,7 +353,7 @@ void Thread::search() {
           beta  = std::min(prev + delta, VALUE_INFINITE);
 
           // Adjust optimism based on root move's previousScore
-          int opt = 120 * prev / (std::abs(prev) + 161);
+          int opt = 102 * prev / (std::abs(prev) + 147);
           optimism[ us] = Value(opt);
           optimism[~us] = -optimism[us];
 
