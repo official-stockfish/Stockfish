b36af07758178ac7b38ab6d51a8900c8d1be65e3
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-23 00:35:13 +0200

    s20 d-2 d > 3 and d < 12

--- src/search.cpp
+++ src/search.cpp
@@ -1320,7 +1320,7 @@ moves_loop: // When in check, search starts here
                   if (   depth > 1
                       && beta  <  14001
                       && value > -12754)
-                      depth -= depth > 3 && depth < 8 ? 2 : 1;
+                      depth -= depth > 3 && depth < 12 ? 2 : 1;
 
                   assert(depth > 0);
                   alpha = value; // Update alpha! Always alpha < beta
