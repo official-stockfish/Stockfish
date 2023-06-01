49c179039c8bde89e34aa7ee82dd7d3340defd98
Michael Chaly <Vizvezdenec@gmail.com>
2023-06-01 23:32:50 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -822,7 +822,7 @@ namespace {
         }
     }
 
-    // Step 10. If the position is not in TT, decrease depth by 2 
+    // Step 10. If the position doesn't have ttMove, decrease depth by 2 
     // (or by 4 if the TT entry for the current position was hit and the stored depth is greater than or equal to the current depth).
     // Use qsearch if depth is equal or below zero (~9 Elo)
     if (    PvNode
