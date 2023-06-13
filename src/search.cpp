8cfea0ed2effe59fd8db87860ca4ae8828657dc2
Viren6 <94880762+Viren6@users.noreply.github.com>
2023-06-13 22:26:20 +0100

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1464,7 +1464,6 @@ moves_loop: // When in check, search starts here
     // Step 4. Static evaluation of the position
     if (ss->inCheck)
     {
-        ss->staticEval = VALUE_NONE;
         bestValue = futilityBase = -VALUE_INFINITE;
     }
     else
