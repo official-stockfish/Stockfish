c59ba7b32f1d0904351509663637804072d7b84d
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2023-07-14 17:43:56 +0200

    Build and test more binaries in CI
    use a fixed compiler on Linux and Windows (right now gcc 11).
    build avxvnni on Windows (Linux needs updated core utils)
    build x86-32 on Linux (Windows needs other mingw)
    fix a Makefile issue where a failed PGOBENCH would not stop the build
    reuse the WINE_PATH for SDE as we do for QEMU
    use WINE_PATH variable also for the signature
    verify the bench for each of the binaries
    do not build x86-64-avx2 on macos
    
    No functional change

--- tests/signature.sh
+++ tests/signature.sh
@@ -11,7 +11,7 @@ trap 'error ${LINENO}' ERR
 
 # obtain
 
-signature=`./stockfish bench 2>&1 | grep "Nodes searched  : " | awk '{print $4}'`
+signature=`eval "$WINE_PATH ./stockfish bench 2>&1" | grep "Nodes searched  : " | awk '{print $4}'`
 
 if [ $# -gt 0 ]; then
    # compare to given reference
