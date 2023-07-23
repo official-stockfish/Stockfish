78e3d2ad78f1835d627ec40b9228e8b7dbb676ef
windfishballad <windfishballadtcec@gmail.com>
2023-07-22 20:35:40 -0400

    Simplify some qsearch conditions
    Use the assert which ensures that beta == alpha+1 at PVNodes
    to simplify a little bit the conditions further down in the code.
    
    passed STC:
    LLR: 2.95 (-2.94,2.94) <-1.75,0.25>
    Total: 56160 W: 14370 L: 14173 D: 27617
    Ptnml(0-2): 210, 6192, 15076, 6395, 207
    https://tests.stockfishchess.org/tests/view/64bc769cdc56e1650abb2e26
    
    closes https://tests.stockfishchess.org/tests/view/64bc769cdc56e1650abb2e26
    
    No functional change

--- src/search.cpp
+++ src/search.cpp
@@ -1496,7 +1496,7 @@ moves_loop: // When in check, search starts here
             return bestValue;
         }
 
-        if (PvNode && bestValue > alpha)
+        if (bestValue > alpha)
             alpha = bestValue;
 
         futilityBase = bestValue + 200;
@@ -1608,7 +1608,7 @@ moves_loop: // When in check, search starts here
                 if (PvNode) // Update pv even in fail-high case
                     update_pv(ss->pv, move, (ss+1)->pv);
 
-                if (PvNode && value < beta) // Update alpha here!
+                if (value < beta) // Update alpha here!
                     alpha = value;
                 else
                     break; // Fail high
