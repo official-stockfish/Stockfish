5d1008be2703862a58e5c0831f4e986fa9c6ecd8
KasparovChess <121145620+KasparovChess@users.noreply.github.com>
2023-08-04 19:33:19 +0200

    Revert uci.cpp

--- src/uci.cpp
+++ src/uci.cpp
@@ -68,13 +68,9 @@ namespace {
     else
         return;
 
-#if __APPLE__
-    false;
-#else
     string rfen = fen;
     regex c("\\s*([rnbqkpRNBQKP1-8]+\\/){7}([rnbqkpRNBQKP1-8]+)\\s[bw-]\\s(([a-hkqA-HKQ]{1,4})|(-))\\s(([a-h][3-6])|(-))\\s\\d+\\s\\d+ \\s*");
-    if (regex_match(rfen, c));  else { sync_cout << "Invaild fen" << sync_endl; return; }
-#endif
+    if (regex_match(rfen, c));  else { cout << "Invaild fen" << endl; return; }
 
     states = StateListPtr(new std::deque<StateInfo>(1)); // Drop the old state and create a new one
     pos.set(fen, Options["UCI_Chess960"], &states->back(), Threads.main());
