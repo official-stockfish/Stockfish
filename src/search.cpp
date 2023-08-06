ddcdbf68787abfdf107cb1b6d86bc8189542d35c
Michael Chaly <Vizvezdenec@gmail.com>
2023-08-07 02:32:38 +0300

    bench 1573024

--- src/search.cpp
+++ src/search.cpp
@@ -1495,7 +1495,7 @@ moves_loop: // When in check, search starts here
         if (bestValue > alpha)
             alpha = bestValue;
 
-        futilityBase = bestValue + 200;
+        futilityBase = std::min(ss->staticEval, bestValue) + 200;
     }
 
     const PieceToHistory* contHist[] = { (ss-1)->continuationHistory, (ss-2)->continuationHistory,
