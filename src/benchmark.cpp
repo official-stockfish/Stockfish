2bba973b297fc79a5f2092d134925759c196a9f4
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2023-09-03 14:11:43 +0200

    bench 1393714 OK (Workflow build)

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -14,6 +14,7 @@
 
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+bench 1393714 checked with Workflow build
 */
 
 #include "benchmark.h"
