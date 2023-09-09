6699e970ecc972ac81468d3ce21336d751f5405f
FauziAkram <fauzi.dabat@hotmail.com>
2023-09-10 00:22:19 +0300

    bench: 1393714
    bench: 1393714

--- src/search.cpp
+++ src/search.cpp
@@ -908,7 +908,6 @@ moves_loop: // When in check, search starts here
         && (tte->bound() & BOUND_LOWER)
         && tte->depth() >= depth - 4
         && ttValue >= probCutBeta
-        && abs(ttValue) <= VALUE_KNOWN_WIN
         && abs(beta) <= VALUE_KNOWN_WIN)
         return probCutBeta;
 
