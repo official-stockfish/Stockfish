bd6890a703c3aa580bd9d2296e84cb08b2faf9b0
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-09-19 18:06:12 +0800

    Make casting styles consistent
    Make casting styles consistent with the rest of the code.
    
    closes https://github.com/official-stockfish/Stockfish/pull/4793
    
    No functional change

--- src/misc.h
+++ src/misc.h
@@ -133,13 +133,13 @@ public:
 inline uint64_t mul_hi64(uint64_t a, uint64_t b) {
 #if defined(__GNUC__) && defined(IS_64BIT)
     __extension__ using uint128 = unsigned __int128;
-    return ((uint128)a * (uint128)b) >> 64;
+    return (uint128(a) * uint128(b)) >> 64;
 #else
-    uint64_t aL = (uint32_t)a, aH = a >> 32;
-    uint64_t bL = (uint32_t)b, bH = b >> 32;
+    uint64_t aL = uint32_t(a), aH = a >> 32;
+    uint64_t bL = uint32_t(b), bH = b >> 32;
     uint64_t c1 = (aL * bL) >> 32;
     uint64_t c2 = aH * bL + c1;
-    uint64_t c3 = aL * bH + (uint32_t)c2;
+    uint64_t c3 = aL * bH + uint32_t(c2);
     return aH * bH + (c2 >> 32) + (c3 >> 32);
 #endif
 }
