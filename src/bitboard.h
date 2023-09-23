81f4853cda41c33da0c9a5cb6cb268566cc953ca
FauziAkram <fauzi.dabat@hotmail.com>
2023-09-23 16:36:36 +0300

    Update bitboard.h

--- src/bitboard.h
+++ src/bitboard.h
@@ -209,7 +209,7 @@ template<> inline int distance<Square>(Square x, Square y) { return SquareDistan
 
 inline int edge_distance(File f) { return std::min(f, File(FILE_H - f)); }
 
-/// attacks_bb(Square) returns the pseudo attacks of the give piece type
+/// attacks_bb(Square) returns the pseudo attacks of the given piece type
 /// assuming an empty board.
 
 template<PieceType Pt>
