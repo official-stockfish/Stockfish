1ecf1e27aa89350210a8d93b9792aa056684a9ce
mstembera <m_stembera@yahoo.com>
2023-10-05 11:30:43 -0700

    Update default hash size to 64MB. bench: 1453057

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -68,7 +68,7 @@ void init(OptionsMap& o) {
 
   o["Debug Log File"]        << Option("", on_logger);
   o["Threads"]               << Option(1, 1, 1024, on_threads);
-  o["Hash"]                  << Option(16, 1, MaxHashMB, on_hash_size);
+  o["Hash"]                  << Option(64, 1, MaxHashMB, on_hash_size);
   o["Clear Hash"]            << Option(on_clear_hash);
   o["Ponder"]                << Option(false);
   o["MultiPV"]               << Option(1, 1, 500);
