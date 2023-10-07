25d444ed60e3873c02a70525776b145f03833103
candirufish <38038147+candirufish@users.noreply.github.com>
2023-10-07 14:46:03 +0200

    Razor more if ss+1 cutoffCnt > 3
    STC:
    LLR: 2.95 (-2.94,2.94) <0.00,2.00>
    Total: 221760 W: 56726 L: 56144 D: 108890
    Ptnml(0-2): 655, 25453, 58123, 25953, 696
    https://tests.stockfishchess.org/tests/view/651d34dbcff46e538ee05d91
    
    LTC:
    LLR: 2.95 (-2.94,2.94) <0.50,2.50>
    Total: 130326 W: 33188 L: 32681 D: 64457
    Ptnml(0-2): 69, 13949, 36620, 14456, 69
    https://tests.stockfishchess.org/tests/view/651f844eac577114367273d5
    
    closes https://github.com/official-stockfish/Stockfish/pull/4822
    
    bench: 1291708

--- src/search.cpp
+++ src/search.cpp
@@ -766,7 +766,8 @@ namespace {
     // Step 7. Razoring (~1 Elo)
     // If eval is really low check with qsearch if it can exceed alpha, if it can't,
     // return a fail low.
-    if (eval < alpha - 456 - 252 * depth * depth)
+    // Adjust razor margin according to cutoffCnt. (~1 Elo)
+    if (eval < alpha - 456 - (252 - 200 * ((ss+1)->cutoffCnt > 3)) * depth * depth)
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha)
