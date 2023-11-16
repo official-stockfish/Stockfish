256fe52d3fac9bb8c7023d8a3682bf6a44c18e49
FauziAkram <fauzi.dabat@hotmail.com>
2023-11-16 21:10:30 +0300

    bench: 1334248
    bench: 1334248

--- src/uci.h
+++ src/uci.h
@@ -36,7 +36,7 @@ namespace UCI {
 // to the UCI centipawn result used in output. This value is derived from
 // the win_rate_model() such that Stockfish outputs an advantage of
 // "100 centipawns" for a position if the engine has a 50% probability to win
-// from this position in selfplay at fishtest LTC time control.
+// from this position in self-play at fishtest LTC time control.
 const int NormalizeToPawnValue = 328;
 
 class Option;
