504bf0e8b8cf2dd818deb623c5ad7e428e504cd8
FauziAkram <fauzi.dabat@hotmail.com>
2023-11-20 18:56:34 +0100

    Change depth - 1 to newDepth
    Replacing 'depth - 1' with 'newDepth' in the singularbeta formula
    utilizes existing variables more succinctly.
    
    closes https://github.com/official-stockfish/Stockfish/pull/4876
    
    No functional change

--- src/search.cpp
+++ src/search.cpp
@@ -1045,7 +1045,7 @@ moves_loop:  // When in check, search starts here
                 && tte->depth() >= depth - 3)
             {
                 Value singularBeta  = ttValue - (64 + 57 * (ss->ttPv && !PvNode)) * depth / 64;
-                Depth singularDepth = (depth - 1) / 2;
+                Depth singularDepth = newDepth / 2;
 
                 ss->excludedMove = move;
                 value =
