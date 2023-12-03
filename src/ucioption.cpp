eac74d6caf2b26a6c6b73582c5ab57c54f1c5afa
mstembera <m_stembera@yahoo.com>
2023-12-02 21:16:38 -0800

    Change EvalFileBig back to EvalFile to make fishtest happy bench: 1380121

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -84,7 +84,7 @@ void init(OptionsMap& o) {
     o["SyzygyProbeDepth"] << Option(1, 1, 100);
     o["Syzygy50MoveRule"] << Option(true);
     o["SyzygyProbeLimit"] << Option(7, 0, 7);
-    o["EvalFileBig"] << Option(EvalFileDefaultNameBig, on_eval_file);
+    o["EvalFile"] << Option(EvalFileDefaultNameBig, on_eval_file);
     o["EvalFileSmall"] << Option(EvalFileDefaultNameSmall, on_eval_file);
 }
 
