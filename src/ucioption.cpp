c3808150466e93e45fb5794161c74cfce769d1b6
FauziAkram <fauzi.dabat@hotmail.com>
2023-12-12 16:39:14 +0300

    bench: 1283323
    bench: 1283323

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -73,7 +73,6 @@ void init(OptionsMap& o) {
     o["MultiPV"] << Option(1, 1, 500);
     o["Skill Level"] << Option(20, 0, 20);
     o["Move Overhead"] << Option(10, 0, 5000);
-    o["Slow Mover"] << Option(100, 10, 1000);
     o["nodestime"] << Option(0, 0, 10000);
     o["UCI_Chess960"] << Option(false);
     o["UCI_AnalyseMode"] << Option(false);
