22558dc13dac5384c0254e2eab6539699e1acec0
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-12-13 19:05:37 +0800

    Remove UCI_AnalyseMode option

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -76,7 +76,6 @@ void init(OptionsMap& o) {
     o["Slow Mover"] << Option(100, 10, 1000);
     o["nodestime"] << Option(0, 0, 10000);
     o["UCI_Chess960"] << Option(false);
-    o["UCI_AnalyseMode"] << Option(false);
     o["UCI_LimitStrength"] << Option(false);
     o["UCI_Elo"] << Option(1320, 1320, 3190);
     o["UCI_ShowWDL"] << Option(false);
