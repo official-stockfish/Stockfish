3e67902b027a59548c7e38a4157384d16f00c99d
FauziAkram <fauzi.dabat@hotmail.com>
2023-12-18 16:22:54 +0300

    Update thread.cpp

--- src/thread.cpp
+++ src/thread.cpp
@@ -235,7 +235,7 @@ Thread* ThreadPool::get_best_thread() const {
         votes[th->rootMoves[0].pv[0]] += thread_value(th);
 
     for (Thread* th : threads)
-        if (abs(bestThread->rootMoves[0].score) >= VALUE_TB_WIN_IN_MAX_PLY)
+        if (std::abs(bestThread->rootMoves[0].score) >= VALUE_TB_WIN_IN_MAX_PLY)
         {
             // Make sure we pick the shortest mate / TB conversion or stave off mate the longest
             if (th->rootMoves[0].score > bestThread->rootMoves[0].score)
