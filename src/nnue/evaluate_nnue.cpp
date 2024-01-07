7c5e3f28655607288a980645e6b2ce600a627b11
Disservin <disservin.social@gmail.com>
2024-01-07 21:41:52 +0100

    Prefix abs with std::

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -178,7 +178,7 @@ static bool write_parameters(std::ostream& stream, NetSize netSize) {
 void hint_common_parent_position(const Position& pos) {
 
     int simpleEval = simple_eval(pos, pos.side_to_move());
-    if (abs(simpleEval) > 1050)
+    if (std::abs(simpleEval) > 1050)
         featureTransformerSmall->hint_common_access(pos);
     else
         featureTransformerBig->hint_common_access(pos);
