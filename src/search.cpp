7bd9dd7e2993dc272502e98612925025778594a4
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-02-02 13:24:36 +0000

    Fix double extension cap
    bench 1394701

--- src/search.cpp
+++ src/search.cpp
@@ -1044,7 +1044,7 @@ moves_loop:  // When in check, search starts here
                     extension        = 1;
 
                     // Avoid search explosion by limiting the number of double extensions
-                    if (!PvNode && value < singularBeta - 2 && ss->doubleExtensions <= 12)
+                    if (!PvNode && value < singularBeta - 2 && ss->doubleExtensions <= 15)
                     {
                         extension = 2 + (value < singularBeta - 200 && !ttCapture);
                         depth += depth < 15;
