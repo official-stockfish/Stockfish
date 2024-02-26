03d60bfac8d890d9bfc172264ef3ae37b88cf576
FauziAkram <fauzi.dabat@hotmail.com>
2024-02-26 21:38:26 +0300

    bench: 1303971
    bench: 1303971

--- src/search.cpp
+++ src/search.cpp
@@ -57,7 +57,8 @@ namespace {
 // Futility margin
 Value futility_margin(Depth d, bool noTtCutNode, bool improving) {
     Value futilityMult = 117 - 44 * noTtCutNode;
-    return (futilityMult * d - 3 * futilityMult / 2 * improving);
+    return (improving? futilityMult * d - 3 * futilityMult / 2
+                      :futilityMult * d);
 }
 
 constexpr int futility_move_count(bool improving, Depth depth) {
