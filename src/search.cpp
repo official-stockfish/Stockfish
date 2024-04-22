3d414c9c3701af7933603883d34015f4d33dc7e3
FauziAkram <fauzi.dabat@hotmail.com>
2024-04-22 14:42:26 +0300

    bench: 1836777
    bench: 1836777

--- src/search.cpp
+++ src/search.cpp
@@ -12,7 +12,7 @@
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License 
+  You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
