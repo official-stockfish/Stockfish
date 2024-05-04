b2696dfd2d43ab61d77ffe7556473ff940a5e991
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-04 10:33:26 +0300

    bench 1600075

--- src/search.cpp
+++ src/search.cpp
@@ -1336,7 +1336,8 @@ moves_loop:  // When in check, search starts here
     {
         int bonus = (depth > 5) + (PvNode || cutNode) + ((ss - 1)->statScore < -14761)
                   + ((ss - 1)->moveCount > 11)
-                  + (!ss->inCheck && bestValue <= ss->staticEval - 142);
+                  + (!ss->inCheck && bestValue <= ss->staticEval - 142)
+                  + (!(ss-1)->inCheck && bestValue <= -(ss-1)->staticEval - 77);
         update_continuation_histories(ss - 1, pos.piece_on(prevSq), prevSq,
                                       stat_bonus(depth) * bonus);
         thisThread->mainHistory[~us][((ss - 1)->currentMove).from_to()]
