9d6dab06a8274c4e09b437110f86bdb1ea7edb0f
FauziAkram <fauzi.dabat@hotmail.com>
2024-05-05 03:10:26 +0300

    simplify moveCountPruning
    no (significant) speedup upon renewed testing
    
    Passed stc:
    LLR: 2.94 (-2.94,2.94) <-1.75,0.25>
    Total: 88992 W: 22779 L: 22633 D: 43580
    Ptnml(0-2): 137, 8706, 26681, 8818, 154
    https://tests.stockfishchess.org/tests/view/6636c4844b68b70d85800dae
    
    closes https://github.com/official-stockfish/Stockfish/pull/5213
    
    No functional change.

--- src/search.cpp
+++ src/search.cpp
@@ -960,8 +960,7 @@ moves_loop:  // When in check, search starts here
         if (!rootNode && pos.non_pawn_material(us) && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
         {
             // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
-            if (!moveCountPruning)
-                moveCountPruning = moveCount >= futility_move_count(improving, depth);
+            moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
             // Reduced depth of the next LMR search
             int lmrDepth = newDepth - r;
