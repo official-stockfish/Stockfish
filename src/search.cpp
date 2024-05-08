300decd38113b955f19f3968808d9df3fb57e0fe
rn5f107s2 <clemens.lerchl@gmail.com>
2024-05-08 22:08:56 +0200

    IIR on cutnodes if there is a ttMove but the ttBound is upper If there is an upper bound stored in the transposition table, but we still have a ttMove, the upperbound indicates that the last time the ttMove was tried, it failed low. This fail low indicates that the ttMove may not be good, so this patch introduces a depth reduction of one for cutnodes with such ttMoves.
    Passed STC:
    https://tests.stockfishchess.org/tests/view/663be4d1ca93dad645f7f45f
    LLR: 2.93 (-2.94,2.94) <0.00,2.00>
    Total: 139424 W: 35900 L: 35433 D: 68091
    Ptnml(0-2): 425, 16357, 35743, 16700, 487
    
    Passed LTC:
    https://tests.stockfishchess.org/tests/view/663bec95ca93dad645f7f5c8
    LLR: 2.94 (-2.94,2.94) <0.50,2.50>
    Total: 129690 W: 32902 L: 32390 D: 64398
    Ptnml(0-2): 63, 14304, 35610, 14794, 74
    
    bench 2299424

--- src/search.cpp
+++ src/search.cpp
@@ -828,8 +828,8 @@ Value Search::Worker::search(
         return qsearch<PV>(pos, ss, alpha, beta);
 
     // For cutNodes without a ttMove, we decrease depth by 2 if depth is high enough.
-    if (cutNode && depth >= 8 && !ttMove)
-        depth -= 2;
+    if (cutNode && depth >= 8 && (!ttMove || tte->bound() == BOUND_UPPER))
+        depth -= 1 + !ttMove;
 
     // Step 11. ProbCut (~10 Elo)
     // If we have a good enough capture (or queen promotion) and a reduced search returns a value
