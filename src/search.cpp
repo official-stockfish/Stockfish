b8812138e8e4e6ebd9d1c46ca9da15ddab1eb1ae
xu-shawn <50402888+xu-shawn@users.noreply.github.com>
2024-05-09 00:11:09 -0700

    Fix usage of abs vs std::abs
    closes https://github.com/official-stockfish/Stockfish/pull/5229
    
    no functional change

--- src/search.cpp
+++ src/search.cpp
@@ -1284,7 +1284,7 @@ moves_loop:  // When in check, search starts here
                 else
                 {
                     // Reduce other moves if we have found at least one score improvement (~2 Elo)
-                    if (depth > 2 && depth < 13 && abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
+                    if (depth > 2 && depth < 13 && std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
                         depth -= 2;
 
                     assert(depth > 0);
