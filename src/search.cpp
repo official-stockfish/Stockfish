285f1d2a663fb111f7124272403923eab4251982
Shawn Xu <xu107288696@gmail.com>
2024-05-15 22:26:15 -0700

    Tweak NMP Formula
    Passed STC:
    LLR: 2.99 (-2.94,2.94) <0.00,2.00>
    Total: 241728 W: 62440 L: 61811 D: 117477
    Ptnml(0-2): 914, 28467, 61458, 29126, 899
    https://tests.stockfishchess.org/tests/live_elo/6645992993ce6da3e93b5b99
    
    Passed LTC:
    LLR: 2.96 (-2.94,2.94) <0.50,2.50>
    Total: 167850 W: 42620 L: 42030 D: 83200
    Ptnml(0-2): 82, 18412, 46354, 18988, 89
    https://tests.stockfishchess.org/tests/live_elo/6647c5726dcff0d1d6b05dd3
    
    closes https://github.com/official-stockfish/Stockfish/pull/5257
    
    Bench: 1636018

--- src/search.cpp
+++ src/search.cpp
@@ -787,7 +787,7 @@ Value Search::Worker::search(
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and eval
-        Depth R = std::min(int(eval - beta) / 144, 6) + depth / 3 + 4;
+        Depth R = std::min(int(eval - beta) / 144, 6) + depth / 3 + 5;
 
         ss->currentMove         = Move::null();
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
