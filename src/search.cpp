c14297a483f7905d61e6f22068d33b199916257a
Shawn Xu <shawn@shawnxu.org>
2024-05-27 01:21:32 -0700

    Tune Fail Low Bonus
    Fractional bonus idea is from @Ergodice on
    [discord](https://discord.com/channels/435943710472011776/735707599353151579/1244039134499180614).
    Values are tuned for 149k games at LTC.
    
    SPSA tune:
    https://tests.stockfishchess.org/tests/view/6652d5d5a86388d5e27da9d6
    
    Failed STC:
    LLR: -2.95 (-2.94,2.94) <0.00,2.00>
    Total: 67424 W: 17364 L: 17528 D: 32532
    Ptnml(0-2): 238, 8043, 17299, 7909, 223
    https://tests.stockfishchess.org/tests/view/66551e1ba86388d5e27db9f9
    
    Passed LTC:
    LLR: 2.94 (-2.94,2.94) <0.00,2.00>
    Total: 146910 W: 37141 L: 36695 D: 73074
    Ptnml(0-2): 84, 16201, 40441, 16643, 86
    https://tests.stockfishchess.org/tests/view/66559949a86388d5e27dcc5d
    
    Passed VLTC:
    LLR: 2.95 (-2.94,2.94) <0.50,2.50>
    Total: 27248 W: 6924 L: 6633 D: 13691
    Ptnml(0-2): 5, 2744, 7835, 3035, 5
    https://tests.stockfishchess.org/tests/view/66563f4da86388d5e27dd27a
    
    closes https://github.com/official-stockfish/Stockfish/pull/5299
    
    Bench: 1390709

--- src/search.cpp
+++ src/search.cpp
@@ -1341,18 +1341,19 @@ moves_loop:  // When in check, search starts here
     // Bonus for prior countermove that caused the fail low
     else if (!priorCapture && prevSq != SQ_NONE)
     {
-        int bonus = (depth > 4) + (depth > 5) + (PvNode || cutNode) + ((ss - 1)->statScore < -14144)
-                  + ((ss - 1)->moveCount > 9) + (!ss->inCheck && bestValue <= ss->staticEval - 115)
-                  + (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 81);
+        int bonus = (54 * (depth > 4) + 62 * (depth > 5) + 115 * (PvNode || cutNode)
+                     + 186 * ((ss - 1)->statScore < -14144) + 121 * ((ss - 1)->moveCount > 9)
+                     + 64 * (!ss->inCheck && bestValue <= ss->staticEval - 115)
+                     + 137 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 81));
         update_continuation_histories(ss - 1, pos.piece_on(prevSq), prevSq,
-                                      stat_bonus(depth) * bonus);
+                                      stat_bonus(depth) * bonus / 100);
         thisThread->mainHistory[~us][((ss - 1)->currentMove).from_to()]
-          << stat_bonus(depth) * bonus / 2;
+          << stat_bonus(depth) * bonus / 200;
 
 
         if (type_of(pos.piece_on(prevSq)) != PAWN && ((ss - 1)->currentMove).type_of() != PROMOTION)
             thisThread->pawnHistory[pawn_structure_index(pos)][pos.piece_on(prevSq)][prevSq]
-              << stat_bonus(depth) * bonus * 4;
+              << stat_bonus(depth) * bonus / 25;
     }
 
     if (PvNode)
