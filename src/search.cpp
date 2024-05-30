4c7073a8614c43eb65e8ef6cefe9c670a3935065
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-05-30 08:18:04 +0100

    Add extension condition to cutoffCnt
    bench 1340462

--- src/search.cpp
+++ src/search.cpp
@@ -1291,7 +1291,7 @@ moves_loop:  // When in check, search starts here
 
                 if (value >= beta)
                 {
-                    ss->cutoffCnt += 1 + !ttMove;
+                    ss->cutoffCnt += 1 + !ttMove - (extension >= 2);
                     assert(value >= beta);  // Fail high
                     break;
                 }
