c3c7daba8308bf89dd18b1d546c6440956ce3334
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2024-06-02 20:39:25 +0200

    `GetProcessGroupAffinity` appears to require 4 byte alignment for `GroupArray` memory.
    See https://stackoverflow.com/q/78567676 for further information

--- src/numa.h
+++ src/numa.h
@@ -774,7 +774,7 @@ class NumaConfig {
 
             // We are expecting a single group.
             USHORT GroupCount = 1;
-            USHORT GroupArray[1];
+            alignas(4) USHORT GroupArray[1];
             status = GetProcessGroupAffinity_f(GetCurrentProcess(), &GroupCount, GroupArray);
             if (status == 0 || GroupCount != 1)
                 return std::nullopt;
