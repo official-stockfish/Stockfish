28a909e21f849fff691302f2ec68e20e8c59432b
Shahin M. Shahin <peregrineshahin@gmail.com>
2024-06-25 01:57:35 +0300

    An alternative to fix mate issues by no progress cycles
    An alternative to https://github.com/official-stockfish/Stockfish/pull/5407
    using only original algorithm according to the paper http://web.archive.org/web/20201107002606/https://marcelk.net/2013-04-06/paper/upcoming-rep-v2.pdf
    
    passed non-regression STC:
    https://tests.stockfishchess.org/tests/view/6679fa1d0c2db3fa2dcecbf2
    LLR: 2.96 (-2.94,2.94) <-1.75,0.25>
    Total: 63584 W: 16666 L: 16472 D: 30446
    Ptnml(0-2): 186, 7552, 16146, 7698, 210
    
    Passed non-regression LTC:
    https://tests.stockfishchess.org/tests/view/667ac965e439ed1c7a9ca042
    LLR: 2.94 (-2.94,2.94) <-1.75,0.25>
    Total: 464574 W: 117493 L: 117729 D: 229352
    Ptnml(0-2): 311, 52468, 126974, 52214, 320
    
    bench: 1323798

--- src/position.h
+++ src/position.h
@@ -156,7 +156,7 @@ class Position {
     int   game_ply() const;
     bool  is_chess960() const;
     bool  is_draw(int ply) const;
-    bool  has_game_cycle(int ply) const;
+    bool  upcoming_repetition(int ply) const;
     bool  has_repeated() const;
     int   rule50_count() const;
     Value non_pawn_material(Color c) const;
