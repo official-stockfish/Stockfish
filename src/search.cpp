a62c95cb93c22dd5913021ceb54e1fc00d1d65a9
Daniel Monroe <39802758+Ergodice@users.noreply.github.com>
2024-06-24 11:50:40 -0400

    50 bonus

--- src/search.cpp
+++ src/search.cpp
@@ -1022,6 +1022,9 @@ moves_loop:  // When in check, search starts here
                 Value futilityValue =
                   ss->staticEval + (bestValue < ss->staticEval - 51 ? 138 : 54) + 140 * lmrDepth;
 
+                if (bestValue < ss->staticEval - 150)
+                    futilityValue += 50;
+
                 // Futility pruning: parent node (~13 Elo)
                 if (!ss->inCheck && lmrDepth < 12 && futilityValue <= alpha)
                 {
