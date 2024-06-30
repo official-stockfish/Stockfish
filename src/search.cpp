815ff9f33f87d8b45d1c54cd92c7bc6a76b76fe7
FauziAkram <fauzi.dabat@hotmail.com>
2024-06-30 22:24:28 +0300

    bench: 1222242
    bench: 1222242

--- src/search.cpp
+++ src/search.cpp
@@ -1245,7 +1245,7 @@ moves_loop:  // When in check, search starts here
             rm.effort += nodes - nodeCount;
 
             rm.averageScore =
-              rm.averageScore != -VALUE_INFINITE ? (2 * value + rm.averageScore) / 3 : value;
+              rm.averageScore != -VALUE_INFINITE ? (value + rm.averageScore) / 2 : value;
 
             // PV move or new best move?
             if (moveCount == 1 || value > alpha)
