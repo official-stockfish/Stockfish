a5cf8632f8a9d40bfb6fb9ecd55ad745f128f26a
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-07-09 12:18:16 -0500

    refactored refutations in movepicker
    bench 1097251

--- src/movepick.h
+++ src/movepick.h
@@ -185,7 +185,7 @@ class MovePicker {
     const PieceToHistory**       continuationHistory;
     const PawnHistory*           pawnHistory;
     Move                         ttMove;
-    ExtMove refutations[2], *cur, *endMoves, *endBadCaptures, *beginBadQuiets, *endBadQuiets;
+    ExtMove refutation, *cur, *endMoves, *endBadCaptures, *beginBadQuiets, *endBadQuiets;
     int     stage;
     int     threshold;
     Depth   depth;
