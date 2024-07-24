c9882a34a6b405c9cd7e427e1d53d943719353e3
Michael Chaly <Vizvezdenec@gmail.com>
2024-07-24 18:25:08 +0300

    bench 1371485

--- src/search.h
+++ src/search.h
@@ -291,7 +291,7 @@ class Worker {
 
     // Quiescence search function, which is called by the main search
     template<NodeType nodeType>
-    Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth = 0);
+    Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta);
 
     Depth reduction(bool i, Depth d, int mn, int delta) const;
 
