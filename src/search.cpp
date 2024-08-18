cfb5f536e34020aba40cbcca8e136f1d0e883b49
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2024-08-18 16:11:06 +0200

    Update search.cpp
    bench: 1420582

--- src/search.cpp
+++ src/search.cpp
@@ -1163,7 +1163,7 @@ moves_loop:  // When in check, search starts here
             // beyond the first move depth.
             // To prevent problems when the max value is less than the min value,
             // std::clamp has been replaced by a more robust implementation.
-            Depth d = std::max(1, std::min(newDepth - r, newDepth + 1));
+            Depth d = std::max(1, std::min(newDepth - r, newDepth + (PvNode || cutNode)));
 
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, d, true);
 
