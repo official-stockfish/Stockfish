6de25872361de9515bdb25bf1d0391311d074012
MinetaS <skdty87@gmail.com>
2024-08-31 16:35:17 +0900

    Remove statScore condition in NMP
    Eliminate the condition that is nearly 100% likelihood of being true.
    
    Passed non-regression STC:
    LLR: 2.94 (-2.94,2.94) <-1.75,0.25>
    Total: 208832 W: 54053 L: 54022 D: 100757
    Ptnml(0-2): 753, 24987, 52901, 25026, 749
    https://tests.stockfishchess.org/tests/view/66cddb50bf8c9d8780fdabaf
    
    Passed non-regression LTC:
    LLR: 2.94 (-2.94,2.94) <-1.75,0.25>
    Total: 154344 W: 39132 L: 39047 D: 76165
    Ptnml(0-2): 115, 17231, 42403, 17300, 123
    https://tests.stockfishchess.org/tests/view/66cfafe39de3e7f9b33d1050
    
    closes https://github.com/official-stockfish/Stockfish/pull/5558
    
    Bench: 1393697

--- src/search.cpp
+++ src/search.cpp
@@ -773,10 +773,9 @@ Value Search::Worker::search(
         return beta + (eval - beta) / 3;
 
     // Step 9. Null move search with verification search (~35 Elo)
-    if (cutNode && (ss - 1)->currentMove != Move::null() && (ss - 1)->statScore < 14389
-        && eval >= beta && ss->staticEval >= beta - 21 * depth + 390 && !excludedMove
-        && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly
-        && beta > VALUE_TB_LOSS_IN_MAX_PLY)
+    if (cutNode && (ss - 1)->currentMove != Move::null() && eval >= beta
+        && ss->staticEval >= beta - 21 * depth + 390 && !excludedMove && pos.non_pawn_material(us)
+        && ss->ply >= thisThread->nmpMinPly && beta > VALUE_TB_LOSS_IN_MAX_PLY)
     {
         assert(eval - beta >= 0);
 
