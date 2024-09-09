f4ba7ce67a0a4b77d9d641b4010dfc73f3b534b2
Disservin <disservin.social@gmail.com>
2024-09-09 17:07:36 +0200

    Restore development
    closes https://github.com/official-stockfish/Stockfish/pull/5580
    
    No functional change

--- src/misc.cpp
+++ src/misc.cpp
@@ -38,7 +38,7 @@ namespace Stockfish {
 namespace {
 
 // Version number or dev.
-constexpr std::string_view version = "17";
+constexpr std::string_view version = "dev";
 
 // Our fancy logging facility. The trick here is to replace cin.rdbuf() and
 // cout.rdbuf() with two Tie objects that tie cin and cout to a file stream. We
