1746beda7c106e7e18e776c0231a8efeb016d692
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2024-09-30 18:58:48 +0200

    Update search.cpp
    bench: 1269487

--- src/search.cpp
+++ src/search.cpp
@@ -1157,7 +1157,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if ttMove is a capture but the current move is not a capture (~3 Elo)
         if (ttCapture && !capture)
-            r++;
+            r += 1 + (depth < 8);
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
