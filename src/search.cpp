449dd0ec1763a57e1399d8a669fc840c56c455c3
Michael Chaly <Vizvezdenec@gmail.com>
2024-10-19 12:01:11 +0300

    bench 1172274

--- src/search.cpp
+++ src/search.cpp
@@ -1237,7 +1237,7 @@ moves_loop:  // When in check, search starts here
             (ss + 1)->pv[0] = Move::none();
 
             // Extend move from transposition table if we are about to dive into qsearch.
-            if (move == ttData.move && ss->statScore > -10000 && ss->ply <= thisThread->rootDepth * 2)
+            if (move == ttData.move && ss->statScore > -25000 && ss->ply <= thisThread->rootDepth * 2)
                 newDepth = std::max(newDepth, 1);
 
             value = -search<PV>(pos, ss + 1, -beta, -alpha, newDepth, false);
