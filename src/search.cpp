328663e55d42337e4bbc37c3fa24248aa7f6ff18
Michael Chaly <Vizvezdenec@gmail.com>
2024-10-20 18:15:08 +0300

    bench 1193496

--- src/search.cpp
+++ src/search.cpp
@@ -660,7 +660,7 @@ Value Search::Worker::search(
             return ttData.value;
     }
 
-    probCutBeta = beta + 379;
+    probCutBeta = beta + 359;
     if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
         && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         && std::abs(ttData.value) < VALUE_TB_WIN_IN_MAX_PLY)
