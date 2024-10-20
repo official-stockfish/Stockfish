3eb0db7f76961a5fe27a704306417cd63c6dce94
Michael Chaly <Vizvezdenec@gmail.com>
2024-10-20 12:36:07 +0300

    bench 1360014

--- src/search.cpp
+++ src/search.cpp
@@ -1172,8 +1172,8 @@ moves_loop:  // When in check, search starts here
         if (ttCapture && !capture)
             r += 1 + (depth < 8);
 
-        if (ss->staticEval - unadjustedStaticEval < -80)
-            r++;
+        if (ss->staticEval - unadjustedStaticEval > -50)
+            r--;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
