ba8f82eefffba02e82344b832bae3eb8ceb7feb1
Linmiao Xu <linmiao.xu@gmail.com>
2024-11-03 22:48:42 -0500

    Update default main net to nn-1c0000000000.nnue
    Found by updating 489 L2 weights with values found from around
    31k / 60k spsa games.
    
    Spsa was configured to use 60k games, down from 120k games in:
    https://github.com/official-stockfish/Stockfish/pull/5459
    
    623 spsa params:
    L2 weights from `nn-1cedc0ffeeee.nnue` where 24 <= |value| <= 30
    A: 3000, alpha: 0.602, gamma: 0.101
    weights: [-127, 127], c_end = 6
    
    Passed STC:
    https://tests.stockfishchess.org/tests/view/6728d61e86d5ee47d953dcaf
    LLR: 2.93 (-2.94,2.94) <0.00,2.00>
    Total: 187168 W: 48642 L: 48107 D: 90419
    Ptnml(0-2): 558, 21888, 48213, 22311, 614
    
    Passed LTC:
    https://tests.stockfishchess.org/tests/view/672b018f86d5ee47d953de98
    LLR: 2.94 (-2.94,2.94) <0.50,2.50>
    Total: 235074 W: 59924 L: 59202 D: 115948
    Ptnml(0-2): 131, 25467, 65610, 26207, 122
    
    bench 1207568

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,7 +33,7 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultNameBig "nn-1cedc0ffeeee.nnue"
+#define EvalFileDefaultNameBig "nn-1c0000000000.nnue"
 #define EvalFileDefaultNameSmall "nn-37f18f62d772.nnue"
 
 namespace NNUE {
