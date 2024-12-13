ba145332c9f0b8126bd940ec5afbc7769ef43174
Daniel Monroe <github.com@danielmonroe.net>
2024-12-13 13:45:54 -0800

    Remove time reduction for recaptures
    Passed simplification STC
    LLR: 2.96 (-2.94,2.94) <-1.75,0.25>
    Total: 54016 W: 14098 L: 13902 D: 26016
    Ptnml(0-2): 165, 5797, 14919, 5931, 196
    https://tests.stockfishchess.org/tests/view/6758a90486d5ee47d954201e
    
    Passed simplification LTC
    LLR: 2.94 (-2.94,2.94) <-1.75,0.25>
    Total: 296940 W: 75631 L: 75689 D: 145620
    Ptnml(0-2): 145, 28928, 90384, 28866, 147
    https://tests.stockfishchess.org/tests/view/6758df7a86d5ee47d9542091
    
    closes https://github.com/official-stockfish/Stockfish/pull/5719
    
    Bench: 1148169

--- src/search.cpp
+++ src/search.cpp
@@ -453,10 +453,9 @@ void Search::Worker::iterative_deepening() {
             timeReduction    = lastBestMoveDepth + 8 < completedDepth ? 1.495 : 0.687;
             double reduction = (1.48 + mainThread->previousTimeReduction) / (2.17 * timeReduction);
             double bestMoveInstability = 1 + 1.88 * totBestMoveChanges / threads.size();
-            double recapture           = limits.capSq == rootMoves[0].pv[0].to_sq() ? 0.955 : 1.005;
 
             double totalTime =
-              mainThread->tm.optimum() * fallingEval * reduction * bestMoveInstability * recapture;
+              mainThread->tm.optimum() * fallingEval * reduction * bestMoveInstability;
 
             // Cap used time in case of a single legal move for a better viewer experience
             if (rootMoves.size() == 1)
