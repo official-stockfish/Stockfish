c4dd32834bc3326b87f4b4730372538b65938b69
Daniel Monroe <github.com@danielmonroe.net>
2024-12-24 01:15:59 -0500

    harder futility pruning if corrhist large bench 1294909

--- src/search.cpp
+++ src/search.cpp
@@ -787,6 +787,7 @@ Value Search::Worker::search(
     if (!ss->ttPv && depth < 14
         && eval - futility_margin(depth, cutNode && !ss->ttHit, improving, opponentWorsening)
                - (ss - 1)->statScore / 290
+               + (ss->staticEval == eval) * (40 - std::abs(correctionValue) / 131072)
              >= beta
         && eval >= beta && (!ttData.move || ttCapture) && !is_loss(beta) && !is_win(eval))
         return beta + (eval - beta) / 3;
