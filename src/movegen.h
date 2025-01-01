5488dd2f91226f73d93ab8f892d4aaa9b2582eed
Shawn Xu <xu107288696@gmail.com>
2024-12-31 22:28:16 -0800

    Update Copyright Year
    closes https://github.com/official-stockfish/Stockfish/pull/5747
    
    No functional change

--- src/movegen.h
+++ src/movegen.h
@@ -1,6 +1,6 @@
 /*
   Stockfish, a UCI chess playing engine derived from Glaurung 2.1
-  Copyright (C) 2004-2024 The Stockfish developers (see AUTHORS file)
+  Copyright (C) 2004-2025 The Stockfish developers (see AUTHORS file)
 
   Stockfish is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
