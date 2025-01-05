c76c1793615b17b97dd504f8c81c1ff2c63c232a
Daniel Monroe <39802758+Ergodice@users.noreply.github.com>
2025-01-05 15:18:09 -0800

    Remove non-functional std::min()
    closes https://github.com/official-stockfish/Stockfish/pull/5749
    
    No functional change

--- src/search.cpp
+++ src/search.cpp
@@ -1160,7 +1160,7 @@ moves_loop:  // When in check, search starts here
 
         r += 330;
 
-        r -= std::min(std::abs(correctionValue) / 32768, 2048);
+        r -= std::abs(correctionValue) / 32768;
 
         // Increase reduction for cut nodes (~4 Elo)
         if (cutNode)
