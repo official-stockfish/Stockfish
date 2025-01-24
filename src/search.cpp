489819d56ef6a4aa59552a2206547d295e145484
Daniel Monroe <github.com@danielmonroe.net>
2025-01-23 20:02:24 -0800

    simp futility bench 1569382

--- src/search.cpp
+++ src/search.cpp
@@ -1030,7 +1030,7 @@ moves_loop:  // When in check, search starts here
                 lmrDepth += history / 3459;
 
                 Value futilityValue =
-                  ss->staticEval + (bestValue < ss->staticEval - 47 ? 137 : 47) + 142 * lmrDepth;
+                  ss->staticEval + (bestMove ? 47 : 137) + 142 * lmrDepth;
 
                 // Futility pruning: parent node (~13 Elo)
                 if (!ss->inCheck && lmrDepth < 12 && futilityValue <= alpha)
