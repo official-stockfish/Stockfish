9f0844c101fda0526637cb8468b80e67699ea451
Kenneth Lee <71492754+kennethlee33@users.noreply.github.com>
2025-01-25 20:14:08 -0800

    Simplify extensions depth increase condition
    Passed STC:
    LLR: 2.95 (-2.94,2.94) <-1.75,0.25>
    Total: 42784 W: 11198 L: 10979 D: 20607
    Ptnml(0-2): 166, 5024, 10822, 5185, 195
    https://tests.stockfishchess.org/tests/view/6795b6f8f6281b7d7b1869d6
    
    Failed LTC:
    LLR: -2.95 (-2.94,2.94) <-1.75,0.25>
    Total: 283614 W: 72046 L: 72587 D: 138981
    Ptnml(0-2): 241, 32097, 77647, 31606, 216
    https://tests.stockfishchess.org/tests/view/6795cbb6f6281b7d7b186a07
    
    Passed VLTC:
    LLR: 2.94 (-2.94,2.94) <-1.75,0.25>
    Total: 59678 W: 15387 L: 15211 D: 29080
    Ptnml(0-2): 23, 6169, 17273, 6357, 17
    https://tests.stockfishchess.org/tests/view/6795c6dbf6281b7d7b1869f9
    
    closes https://github.com/official-stockfish/Stockfish/pull/5827
    
    Bench: 3088494

--- src/search.cpp
+++ src/search.cpp
@@ -1107,7 +1107,7 @@ moves_loop:  // When in check, search starts here
                     extension = 1 + (value < singularBeta - doubleMargin)
                               + (value < singularBeta - tripleMargin);
 
-                    depth += ((!PvNode) && (depth < 15));
+                    depth += (depth < 15);
                 }
 
                 // Multi-cut pruning
