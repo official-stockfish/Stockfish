dc3ed2c38619c2329624713e22780420e871cc29
Kenneth Lee <71492754+kennethlee33@users.noreply.github.com>
2025-01-25 20:14:08 -0800

    Update search.cpp
    bench 2009497

--- src/search.cpp
+++ src/search.cpp
@@ -1086,7 +1086,7 @@ moves_loop:  // When in check, search starts here
                               + (value < singularBeta - tripleMargin)
                               + (value < singularBeta - quadMargin);
 
-                    depth += ((!PvNode) && (depth < 15));
+                    depth += (depth < 15);
                 }
 
                 // Multi-cut pruning
