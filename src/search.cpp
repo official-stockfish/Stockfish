9336ccf82b629537d0216cbb2942ed561a4e9153
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-01-30 22:36:00 +0100

    Tweak correction history factors.
    The values are taken from this tuning https://tests.stockfishchess.org/tests/view/679c4e150774dfd78deaf376 which added also a new material correction history.
    The full tune doesn't work but ignoring the new history results in this changes.
    
    Passed STC:
    LLR: 2.94 (-2.94,2.94) <0.00,2.00>
    Total: 102368 W: 27057 L: 26638 D: 48673
    Ptnml(0-2): 394, 12031, 25949, 12382, 428
    https://tests.stockfishchess.org/tess/view/679d2ca70774dfd78deaf796
    
    Passed LTC:
    LLR: 2.96 (-2.94,2.94) <0.50,2.50>
    Total: 55044 W: 14215 L: 13855 D: 26974
    Ptnml(0-2): 43, 5956, 15172, 6300, 51
    https://tests.stockfishchess.org/tests/view/679d30be0774dfd78deafda2
    
    Bench: 2239499

--- src/search.cpp
+++ src/search.cpp
@@ -94,7 +94,7 @@ int correction_value(const Worker& w, const Position& pos, const Stack* ss) {
       m.is_ok() ? (*(ss - 2)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
                  : 0;
 
-    return (7000 * pcv + 6300 * micv + 7550 * (wnpcv + bnpcv) + 6320 * cntcv);
+    return 7037 * pcv + 6671 * micv + 7631 * (wnpcv + bnpcv) + 6362 * cntcv;
 }
 
 // Add correctionHistory value to raw staticEval and guarantee evaluation
@@ -1430,20 +1430,21 @@ moves_loop:  // When in check, search starts here
             || (bestValue > ss->staticEval && bestMove)))     // positive correction & no fail low
     {
         const auto    m             = (ss - 1)->currentMove;
-        constexpr int nonPawnWeight = 165;
+        constexpr int nonPawnWeight = 159;
 
         auto bonus = std::clamp(int(bestValue - ss->staticEval) * depth / 8,
                                 -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
         thisThread->pawnCorrectionHistory[us][pawn_structure_index<Correction>(pos)]
-          << bonus * 114 / 128;
-        thisThread->minorPieceCorrectionHistory[us][minor_piece_index(pos)] << bonus * 146 / 128;
+          << bonus * 104 / 128;
+        thisThread->minorPieceCorrectionHistory[us][minor_piece_index(pos)] << bonus * 145 / 128;
         thisThread->nonPawnCorrectionHistory[WHITE][non_pawn_index<WHITE>(pos)][us]
           << bonus * nonPawnWeight / 128;
         thisThread->nonPawnCorrectionHistory[BLACK][non_pawn_index<BLACK>(pos)][us]
           << bonus * nonPawnWeight / 128;
 
         if (m.is_ok())
-            (*(ss - 2)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()] << bonus;
+            (*(ss - 2)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
+              << bonus * 146 / 128;
     }
 
     assert(bestValue > -VALUE_INFINITE && bestValue < VALUE_INFINITE);
