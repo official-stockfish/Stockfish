21c84afe9b79a8ddcd2d5df03b124a40b38ba107
Shawn Xu <xu107288696@gmail.com>
2025-01-31 17:36:55 -0800

    more simplification
    bench 2688175

--- src/search.cpp
+++ src/search.cpp
@@ -1107,7 +1107,7 @@ moves_loop:  // When in check, search starts here
                     extension = 1 + (value < singularBeta - doubleMargin)
                               + (value < singularBeta - tripleMargin);
 
-                    depth += (depth < 15);
+                    depth++;
                 }
 
                 // Multi-cut pruning
