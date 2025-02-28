c3c00c0d9fa2347d9101da9e51e6c3e1b262a229
Crazed-png <vdogeland@gmail.com>
2025-02-28 18:36:30 +0800

    Update tt.cpp
    tt experiment

--- src/tt.cpp
+++ src/tt.cpp
@@ -224,12 +224,14 @@ std::tuple<bool, TTData, TTWriter> TranspositionTable::probe(const Key key) cons
 
     TTEntry* const tte   = first_entry(key);
     const uint16_t key16 = uint16_t(key);  // Use the low 16 bits as key inside the cluster
+	const uint8_t key8 = uint8_t(key);
 
     for (int i = 0; i < ClusterSize; ++i)
-        if (tte[i].key16 == key16)
+        if (uint8_t(tte[i].key16) == key8 || tte[i].key16 == key16) {
             // This gap is the main place for read races.
             // After `read()` completes that copy is final, but may be self-inconsistent.
             return {tte[i].is_occupied(), tte[i].read(), TTWriter(&tte[i])};
+		}
 
     // Find an entry to be replaced according to the replacement strategy
     TTEntry* replace = tte;
