891cb878f4cc71ea78e6649597737505c146866a
Myself <63040919+AliceRoselia@users.noreply.github.com>
2025-03-02 05:13:46 +0700

    Update movepick.cpp
    bench: 1843842

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -166,7 +166,7 @@ void MovePicker::score() {
             m.value += (*continuationHistory[5])[pc][to];
 
             // bonus for checks
-            m.value += bool(pos.check_squares(pt) & to) * 16384;
+            m.value += pos.gives_check(m) * 16384;
 
             // bonus for escaping from capture
             m.value += threatenedPieces & from ? (pt == QUEEN && !(to & threatenedByRook)   ? 51700
