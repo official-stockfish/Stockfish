4f847c8e2132edbcc4b6e9fb55d3a2356a38b626
Shawn Xu <xu107288696@gmail.com>
2025-03-02 01:29:38 -0800

    simplify
    bench 1889431

--- src/search.cpp
+++ src/search.cpp
@@ -1408,8 +1408,7 @@ moves_loop:  // When in check, search starts here
     assert(moveCount || !ss->inCheck || excludedMove || !MoveList<LEGAL>(pos).size());
 
     // Adjust best value for fail high cases at non-pv nodes
-    if (!PvNode && bestValue >= beta && !is_decisive(bestValue) && !is_decisive(beta)
-        && !is_decisive(alpha))
+    if (bestValue >= beta && !is_decisive(bestValue) && !is_decisive(beta) && !is_decisive(alpha))
         bestValue = (bestValue * depth + beta) / (depth + 1);
 
     if (!moveCount)
