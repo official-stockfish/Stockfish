7e6a0c464bd0fce5b5d94f7d6bd4af3d4d282504
Myself <63040919+AliceRoselia@users.noreply.github.com>
2025-04-26 22:00:36 +0700

    Check only if good see.
    Passed STC:
    LLR: 2.95 (-2.94,2.94) <0.00,2.00>
    Total: 106400 W: 27863 L: 27444 D: 51093
    Ptnml(0-2): 320, 12431, 27275, 12858, 316
    https://tests.stockfishchess.org/tests/view/6806239498cd372e3aea5949
    
    Passed LTC:
    LLR: 2.96 (-2.94,2.94) <0.50,2.50>
    Total: 420534 W: 107594 L: 106494 D: 206446
    Ptnml(0-2): 197, 45541, 117722, 46579, 228
    https://tests.stockfishchess.org/tests/view/6806b4b3878abf56f9a0d4fc
    
    closes https://github.com/official-stockfish/Stockfish/pull/6020
    
    bench: 1675758

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -170,7 +170,7 @@ void MovePicker::score() {
             m.value += (*continuationHistory[5])[pc][to];
 
             // bonus for checks
-            m.value += bool(pos.check_squares(pt) & to) * 16384;
+            m.value += (bool(pos.check_squares(pt) & to) && pos.see_ge(m, -75)) * 16384;
 
             // bonus for escaping from capture
             m.value += threatenedPieces & from ? (pt == QUEEN && !(to & threatenedByRook)   ? 51700
