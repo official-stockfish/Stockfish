b0a7a34d3fd9b7024abd64150d47405d1be1dd69
Shawn Xu <xu107288696@gmail.com>
2025-04-26 14:38:21 -0700

    Simplify malus calculation
    closes https://github.com/official-stockfish/Stockfish/pull/6024
    
    No functional change

--- src/search.cpp
+++ src/search.cpp
@@ -1910,7 +1910,7 @@ void update_all_stats(const Position&      pos,
     PieceType              captured;
 
     int bonus = std::min(141 * depth - 89, 1613) + 311 * (bestMove == ttMove);
-    int malus = std::min(695 * depth - 215, 2808) - 31 * (moveCount - 1);
+    int malus = std::min(695 * depth - 184, 2839) - 31 * moveCount;
 
     if (!pos.capture_stage(bestMove))
     {
