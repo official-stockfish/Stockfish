e695b9537ec1d7b19306deefe24d889db563dbb0
FauziAkram <fauzi.dabat@hotmail.com>
2025-06-14 16:15:15 +0300

    Remove eval & beta diff from NM reduction
    Passed STC:
    LLR: 2.94 (-2.94,2.94) <-1.75,0.25>
    Total: 43456 W: 11178 L: 10966 D: 21312
    Ptnml(0-2): 114, 5078, 11114, 5326, 96
    https://tests.stockfishchess.org/tests/view/6849ae13e84567164b5c9de9
    
    Passed LTC:
    LLR: 2.94 (-2.94,2.94) <-1.75,0.25>
    Total: 63090 W: 16302 L: 16125 D: 30663
    Ptnml(0-2): 37, 6837, 17603, 7048, 20
    https://tests.stockfishchess.org/tests/view/684ab516e84567164b5ca02f
    
    closes https://github.com/official-stockfish/Stockfish/pull/6134
    
    Bench: 2249459

--- src/search.cpp
+++ src/search.cpp
@@ -859,8 +859,8 @@ Value Search::Worker::search(
     {
         assert(eval - beta >= 0);
 
-        // Null move dynamic reduction based on depth and eval
-        Depth R = std::min(int(eval - beta) / 213, 6) + depth / 3 + 5;
+        // Null move dynamic reduction based on depth
+        Depth R = 7 + depth / 3;
 
         ss->currentMove                   = Move::null();
         ss->continuationHistory           = &thisThread->continuationHistory[0][0][NO_PIECE][0];
