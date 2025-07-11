0d2f19df2adffc04e4578bdef5e31036a08a7da5
Daniel Monroe <github.com@danielmonroe.net>
2025-07-10 23:46:30 -0400

    simp razoring bench 2657751

--- src/search.cpp
+++ src/search.cpp
@@ -826,7 +826,7 @@ Value Search::Worker::search(
     // Step 7. Razoring
     // If eval is really low, skip search entirely and return the qsearch value.
     // For PvNodes, we must have a guard against mates being returned.
-    if (!PvNode && eval < alpha - 486 - 325 * depth * depth)
+    if (!PvNode && eval < alpha - 800 && depth == 1)
         return qsearch<NonPV>(pos, ss, alpha, beta);
 
     // Step 8. Futility pruning: child node
