26e700ca3a77ca05850bbc4909b2216f3712e0c8
DANUT39 <gagadanutvasile@gmail.com>
2025-07-13 15:50:44 +0300

    Tweak: Adjusted smallnet threshold from 962 to 960.
    Bench: 2249459

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -46,7 +46,7 @@ int Eval::simple_eval(const Position& pos) {
          + (pos.non_pawn_material(c) - pos.non_pawn_material(~c));
 }
 
-bool Eval::use_smallnet(const Position& pos) { return std::abs(simple_eval(pos)) > 962; }
+bool Eval::use_smallnet(const Position& pos) { return std::abs(simple_eval(pos)) > 960; }
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
 // of the position from the point of view of the side to move.
