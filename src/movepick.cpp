0e6efeee8973097be63edd4cc6f16d69e169406e
FauziAkram <fauzi.dabat@hotmail.com>
2025-07-21 18:41:38 +0300

    No functional change
    No functional change

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -153,8 +153,8 @@ void MovePicker::score() {
         else if constexpr (Type == QUIETS)
         {
             // histories
-            m.value = 2 * (*mainHistory)[us][m.from_to()];
-            m.value += 2 * (*pawnHistory)[pawn_structure_index(pos)][pc][to];
+            m.value = 2 * ((*mainHistory)[us][m.from_to()] +
+                       (*pawnHistory)[pawn_structure_index(pos)][pc][to]);
             m.value += (*continuationHistory[0])[pc][to];
             m.value += (*continuationHistory[1])[pc][to];
             m.value += (*continuationHistory[2])[pc][to];
