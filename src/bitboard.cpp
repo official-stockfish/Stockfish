8d9cc51ab5ec74ab183efd5b0f914279c37ad2f2
FauziAkram <fauzi.dabat@hotmail.com>
2025-07-21 19:26:11 +0300

    No functional change
    No functional change

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -42,11 +42,11 @@ Bitboard BishopTable[0x1480];  // To store bishop attacks
 
 void init_magics(PieceType pt, Bitboard table[], Magic magics[][2]);
 
-// Returns the bitboard of target square for the given step
-// from the given square. If the step is off the board, returns empty bitboard.
+// Returns the bitboard of target square for the given step from the
+// given square. If the step is off the board, it returns an empty bitboard.
 Bitboard safe_destination(Square s, int step) {
     Square to = Square(s + step);
-    return is_ok(to) && distance(s, to) <= 2 ? square_bb(to) : Bitboard(0);
+    return (is_ok(to) && distance(s, to) <= 2) * square_bb(to);
 }
 }
 
