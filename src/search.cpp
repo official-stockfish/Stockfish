9bbd4f20209b5ca4afa620948cdb1c2ca57d50e1
Gabe <66077254+MrBrain295@users.noreply.github.com>
2025-08-14 08:54:20 -0500

    Remove Extra Variable
    On my computer running ./stockfish bench I got 
    Total time (ms) : 3302
    Nodes searched  : 3629755
    Nodes/second    : 1099259
    for base Stockfish and 
    Total time (ms) : 3276
    Nodes searched  : 3629755
    Nodes/second    : 1107983
    for my change.

--- src/search.cpp
+++ src/search.cpp
@@ -464,8 +464,7 @@ void Search::Worker::iterative_deepening() {
 
             // If the bestMove is stable over several iterations, reduce time accordingly
             double k      = 0.527;
-            double center = lastBestMoveDepth + 11;
-            timeReduction = 0.8 + 0.84 / (1.077 + std::exp(-k * (completedDepth - center)));
+            timeReduction = 0.8 + 0.84 / (1.077 + std::exp(-k * (completedDepth - lastBestMoveDepth + 11)));
             double reduction =
               (1.4540 + mainThread->previousTimeReduction) / (2.1593 * timeReduction);
             double bestMoveInstability = 0.9929 + 1.8519 * totBestMoveChanges / threads.size();
