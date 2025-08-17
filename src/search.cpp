6fe17c8efc9c54ce879de91b3e6dba831486a8d3
Daniel Monroe <github.com@danielmonroe.net>
2025-08-17 17:20:08 -0400

    Simplify depth condition in prior reduction bench 2055071

--- src/search.cpp
+++ src/search.cpp
@@ -824,7 +824,7 @@ Value Search::Worker::search(
     improving         = ss->staticEval > (ss - 2)->staticEval;
     opponentWorsening = ss->staticEval > -(ss - 1)->staticEval;
 
-    if (priorReduction >= (depth < 10 ? 2 : 3) && !opponentWorsening)
+    if (priorReduction >= 3 && !opponentWorsening)
         depth++;
     if (priorReduction >= 2 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 173)
         depth--;
