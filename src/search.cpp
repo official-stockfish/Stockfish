678d503d8f9c36e4087d7506f5d443c08f859c36
Shawn Xu <xu107288696@gmail.com>
2025-08-23 06:26:40 -0700

    Simplify LMR Extensions
    Passed Non-regression STC:
    LLR: 2.94 (-2.94,2.94) <-1.75,0.25>
    Total: 236896 W: 61683 L: 61683 D: 113530
    Ptnml(0-2): 730, 28057, 60901, 28003, 757
    https://tests.stockfishchess.org/tests/view/68a9c20475da51a345a5a6a0
    
    Passed Non-regression LTC:
    LLR: 2.94 (-2.94,2.94) <-1.75,0.25>
    Total: 73644 W: 18936 L: 18770 D: 35938
    Ptnml(0-2): 33, 7951, 20685, 8123, 30
    https://tests.stockfishchess.org/tests/view/68aa95c575da51a345a5a88a
    
    closes https://github.com/official-stockfish/Stockfish/pull/6265
    
    Bench:  2693376

--- src/search.cpp
+++ src/search.cpp
@@ -1218,8 +1218,7 @@ moves_loop:  // When in check, search starts here
             // beyond the first move depth.
             // To prevent problems when the max value is less than the min value,
             // std::clamp has been replaced by a more robust implementation.
-            Depth d =
-              std::max(1, std::min(newDepth - r / 1024, newDepth + (PvNode ? 2 : 1))) + PvNode;
+            Depth d = std::max(1, std::min(newDepth - r / 1024, newDepth + 2)) + PvNode;
 
             ss->reduction = newDepth - d;
             value         = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, d, true);
