7d213afd37ff90bf1f6ca6deba5533f3f7fc51ff
Shawn Xu <xu107288696@gmail.com>
2025-08-24 15:03:34 -0700

    Non-functional simplifications
    closes https://github.com/official-stockfish/Stockfish/pull/6267
    
    No functional change
    
    Co-authored-by: Daniel Monroe <github.com@danielmonroe.net>

--- src/search.cpp
+++ src/search.cpp
@@ -929,8 +929,6 @@ Value Search::Worker::search(
 
             assert(pos.capture_stage(move));
 
-            movedPiece = pos.moved_piece(move);
-
             do_move(pos, move, st, ss);
 
             // Perform a preliminary qsearch to verify that the move holds
@@ -1079,9 +1077,8 @@ moves_loop:  // When in check, search starts here
 
                 lmrDepth += history / 3220;
 
-                Value baseFutility = (bestMove ? 47 : 218);
-                Value futilityValue =
-                  ss->staticEval + baseFutility + 134 * lmrDepth + 90 * (ss->staticEval > alpha);
+                Value futilityValue = ss->staticEval + 47 + 171 * !bestMove + 134 * lmrDepth
+                                    + 90 * (ss->staticEval > alpha);
 
                 // Futility pruning: parent node
                 // (*Scaler): Generally, more frequent futility pruning
@@ -1353,7 +1350,7 @@ moves_loop:  // When in check, search starts here
 
                 if (value >= beta)
                 {
-                    // (* Scaler) Especially if they make cutoffCnt increment more often.
+                    // (*Scaler) Especially if they make cutoffCnt increment more often.
                     ss->cutoffCnt += (extension < 2) || PvNode;
                     assert(value >= beta);  // Fail high
                     break;
