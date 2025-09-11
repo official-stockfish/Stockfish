e8a4e27cb82033dce1dcc3b57262bf6c7881c5b8
FauziAkram <fauzi.dabat@hotmail.com>
2025-09-11 03:05:22 +0300

    bench: 2652761
    bench: 2652761

--- src/search.cpp
+++ src/search.cpp
@@ -1882,7 +1882,7 @@ void update_quiet_histories(
 
     int pIndex = pawn_history_index(pos);
     workerThread.pawnHistory[pIndex][pos.moved_piece(move)][move.to_sq()]
-      << (bonus * (bonus > 0 ? 704 : 439) / 1024) + 70;
+      << (bonus * (bonus > 0 ? 800 : 500) / 1024) + 70;
 }
 
 }
