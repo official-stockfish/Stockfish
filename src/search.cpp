4f4f78f86e8d44df2c13cfd6671f777dad516067
Michael Chaly <Vizvezdenec@gmail.com>
2025-09-20 09:29:19 +0300

    Extend nodes pre qsearch only with deep enough tt entries
    Modification of current pre qsearch extensions - allowing it only for deep enough tt entries.
    
    Passed STC:
    https://tests.stockfishchess.org/tests/view/68c954d302c43c969fe7eea5
    LLR: 2.93 (-2.94,2.94) <0.00,2.00>
    Total: 35872 W: 9548 L: 9236 D: 17088
    Ptnml(0-2): 101, 4075, 9295, 4341, 124
    
    Passed LTC:
    https://tests.stockfishchess.org/tests/view/68ca4e4f02c43c969fe7ef5f
    LLR: 2.94 (-2.94,2.94) <0.50,2.50>
    Total: 107754 W: 27784 L: 27324 D: 52646
    Ptnml(0-2): 47, 11528, 30300, 11922, 80
    
    closes https://github.com/official-stockfish/Stockfish/pull/6324
    
    bench: 2462792

--- src/search.cpp
+++ src/search.cpp
@@ -1260,7 +1260,7 @@ moves_loop:  // When in check, search starts here
             (ss + 1)->pv[0] = Move::none();
 
             // Extend move from transposition table if we are about to dive into qsearch.
-            if (move == ttData.move && rootDepth > 8)
+            if (move == ttData.move && ttData.depth > 1 && rootDepth > 8)
                 newDepth = std::max(newDepth, 1);
 
             value = -search<PV>(pos, ss + 1, -beta, -alpha, newDepth, false);
