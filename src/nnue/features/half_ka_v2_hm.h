43cd2390a01a8d0277a2100e0e4cbeeb2ce3d8ae
Timothy Herchen <timothy.herchen@gmail.com>
2025-10-12 11:03:33 -0700

    move up

--- src/nnue/features/half_ka_v2_hm.h
+++ src/nnue/features/half_ka_v2_hm.h
@@ -65,8 +65,7 @@ class HalfKAv2_hm {
     static constexpr IndexType Dimensions =
       static_cast<IndexType>(SQUARE_NB) * static_cast<IndexType>(PS_NB) / 2;
 
-#define B(v) (v * PS_NB)
-    static constexpr int16_t PieceSquareIndex[COLOR_NB][PIECE_NB] = {
+    static constexpr std::int16_t PieceSquareIndex[COLOR_NB][PIECE_NB] = {
       // Convention: W - us, B - them
       // Viewed from other side, W and B are reversed
       {PS_NONE, PS_W_PAWN, PS_W_KNIGHT, PS_W_BISHOP, PS_W_ROOK, PS_W_QUEEN, PS_KING, PS_NONE,
@@ -74,8 +73,10 @@ class HalfKAv2_hm {
       {PS_NONE, PS_B_PAWN, PS_B_KNIGHT, PS_B_BISHOP, PS_B_ROOK, PS_B_QUEEN, PS_KING, PS_NONE,
        PS_NONE, PS_W_PAWN, PS_W_KNIGHT, PS_W_BISHOP, PS_W_ROOK, PS_W_QUEEN, PS_KING, PS_NONE}};
 
+#define B(v) (v * PS_NB)
+
     // clang-format off
-    static constexpr int16_t KingBuckets[COLOR_NB][SQUARE_NB] = {
+    static constexpr std::int16_t KingBuckets[COLOR_NB][SQUARE_NB] = {
       { B(28), B(29), B(30), B(31), B(31), B(30), B(29), B(28),
         B(24), B(25), B(26), B(27), B(27), B(26), B(25), B(24),
         B(20), B(21), B(22), B(23), B(23), B(22), B(21), B(20),
@@ -97,7 +98,7 @@ class HalfKAv2_hm {
 #undef B
     // clang-format off
     // Orient a square according to perspective (rotates by 180 for black)
-    static constexpr int16_t OrientTBL[COLOR_NB][SQUARE_NB] = {
+    static constexpr std::int16_t OrientTBL[COLOR_NB][SQUARE_NB] = {
       { SQ_H1, SQ_H1, SQ_H1, SQ_H1, SQ_A1, SQ_A1, SQ_A1, SQ_A1,
         SQ_H1, SQ_H1, SQ_H1, SQ_H1, SQ_A1, SQ_A1, SQ_A1, SQ_A1,
         SQ_H1, SQ_H1, SQ_H1, SQ_H1, SQ_A1, SQ_A1, SQ_A1, SQ_A1,
