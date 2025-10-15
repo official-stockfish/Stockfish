eca3aeba4ce0e4006fc5f7377b8b8f0a414f64d8
Shawn Xu <xu107288696@gmail.com>
2025-10-14 17:08:38 -0700

    Update default network to nn-bf4519f857f4.nnue
    Bench: 2126801

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,7 +33,7 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultNameBig "nn-a878500a97a8.nnue"
+#define EvalFileDefaultNameBig "nn-bf4519f857f4.nnue"
 #define EvalFileDefaultNameSmall "nn-37f18f62d772.nnue"
 
 namespace NNUE {
