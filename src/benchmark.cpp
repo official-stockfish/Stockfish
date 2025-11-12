b956cdc896603c3481490e44b35f826979c617b6
Shawn Xu <xu107288696@gmail.com>
2025-11-11 23:07:59 -0800

    .
    Bench: 2626086

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -65,7 +65,7 @@ const std::vector<std::string> Defaults = {
   "3Qb1k1/1r2ppb1/pN1n2q1/Pp1Pp1Pr/4P2p/4BP2/4B1R1/1R5K b - - 11 40",
   "4k3/3q1r2/1N2r1b1/3ppN2/2nPP3/1B1R2n1/2R1Q3/3K4 w - - 5 1",
 
-  // positions with high numbers of changed threats
+  // Positions with high numbers of changed threats
   "k7/2n1n3/1nbNbn2/2NbRBn1/1nbRQR2/2NBRBN1/3N1N2/7K w - - 0 1",
   "K7/8/8/BNQNQNB1/N5N1/R1Q1q2r/n5n1/bnqnqnbk w - - 0 1",
 
--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -43,6 +43,12 @@ Bitboard BishopTable[0x1480];  // To store bishop attacks
 
 void init_magics(PieceType pt, Bitboard table[], Magic magics[][2]);
 
+// Returns the bitboard of target square for the given step
+// from the given square. If the step is off the board, returns empty bitboard.
+inline Bitboard safe_destination(Square s, int step) {
+    Square to = Square(s + step);
+    return is_ok(to) && distance(s, to) <= 2 ? square_bb(to) : Bitboard(0);
+}
 }
 
 // Returns an ASCII representation of a bitboard suitable
