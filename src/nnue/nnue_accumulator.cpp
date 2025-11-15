ca98f78959ecaf452954e150c6a65f3bff94e571
Timothy Herchen <timothy.herchen@gmail.com>
2025-11-14 22:33:25 -0800

    move the case in

--- src/nnue/nnue_accumulator.cpp
+++ src/nnue/nnue_accumulator.cpp
@@ -577,26 +577,27 @@ void update_accumulator_incremental(
         FeatureSet::template append_changed_indices<Perspective>(ksq, computed.diff, added,
                                                                  removed);
 
-    if (!added.size() && !removed.size())
-    {
-        auto&       targetAcc = target_state.template acc<TransformedFeatureDimensions>();
-        const auto& sourceAcc = computed.template acc<TransformedFeatureDimensions>();
-
-        std::memcpy(targetAcc.accumulation[Perspective], sourceAcc.accumulation[Perspective],
-                    sizeof(targetAcc.accumulation[Perspective]));
-        std::memcpy(targetAcc.psqtAccumulation[Perspective],
-                    sourceAcc.psqtAccumulation[Perspective],
-                    sizeof(targetAcc.psqtAccumulation[Perspective]));
-
-        targetAcc.computed[Perspective] = true;
-        return;
-    }
-
     auto updateContext =
       make_accumulator_update_context<Perspective>(featureTransformer, computed, target_state);
 
     if constexpr (std::is_same_v<FeatureSet, ThreatFeatureSet>)
+    {
+        if (!added.size() && !removed.size())
+        {
+            auto&       targetAcc = target_state.template acc<TransformedFeatureDimensions>();
+            const auto& sourceAcc = computed.template acc<TransformedFeatureDimensions>();
+
+            std::memcpy(targetAcc.accumulation[Perspective], sourceAcc.accumulation[Perspective],
+                        sizeof(targetAcc.accumulation[Perspective]));
+            std::memcpy(targetAcc.psqtAccumulation[Perspective],
+                        sourceAcc.psqtAccumulation[Perspective],
+                        sizeof(targetAcc.psqtAccumulation[Perspective]));
+
+            targetAcc.computed[Perspective] = true;
+            return;
+        }
         updateContext.apply(added, removed);
+    }
     else
     {
         assert(added.size() == 1 || added.size() == 2);
