bee0a4f41a4aeaa4ec9aa973fc40d23b26901417
Daniel Monroe <github.com@danielmonroe.net>
2025-12-23 23:26:58 -0500

    take 1 bench 2970339

--- src/search.cpp
+++ src/search.cpp
@@ -1241,7 +1241,7 @@ moves_loop:  // When in check, search starts here
             {
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
-                const bool doDeeperSearch    = d < newDepth && value > (bestValue + newDepth + 44);
+                const bool doDeeperSearch    = d < newDepth && value > bestValue + 50;
                 const bool doShallowerSearch = value < bestValue + 9;
 
                 newDepth += doDeeperSearch - doShallowerSearch;
