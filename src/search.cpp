7af60d79210715c4bf5c11b938e9bd6c51599bcd
FauziAkram <fauzi.dabat@hotmail.com>
2026-01-09 22:25:30 +0300

    bench: 2050811
    bench: 2050811

--- src/search.cpp
+++ src/search.cpp
@@ -1206,8 +1206,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if next ply has a lot of fail high
         if ((ss + 1)->cutoffCnt > 1)
-            r += 120 + 1024 * ((ss + 1)->cutoffCnt > 2) + 100 * ((ss + 1)->cutoffCnt > 3)
-               + 1024 * allNode;
+            r += 256 + 1024 * ((ss + 1)->cutoffCnt > 2) + 1024 * allNode;
 
         // For first picked move (ttMove) reduce reduction
         if (move == ttData.move)
