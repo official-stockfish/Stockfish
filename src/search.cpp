305d538b65f76990c6350ee3d7e50e3c7f51b767
Ryan Hirsch <hirsch28@purdue.edu>
2026-02-25 09:22:26 -0500

    bench 2534375

--- src/search.cpp
+++ src/search.cpp
@@ -888,7 +888,7 @@ Value Search::Worker::search(
     }
 
     // Step 9. Null move search with verification search
-    if (cutNode && ss->staticEval >= beta - 17 * depth + 359 && !excludedMove
+    if (cutNode && ss->staticEval >= beta - 17 * depth - 50 * improving + 359 && !excludedMove
         && pos.non_pawn_material(us) && ss->ply >= nmpMinPly && !is_loss(beta))
     {
         assert((ss - 1)->currentMove != Move::null());
