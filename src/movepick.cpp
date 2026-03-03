c0242e4877fc0754403ab0c48f1c279ba5a8b43c
vvilim4-cmd <vvilim4@gmail.com>
2026-03-03 19:47:36 +0100

    Adjust goodQuietThreshold value in MovePicker

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -207,7 +207,7 @@ Move MovePicker::select(Pred filter) {
 // picking the move with the highest score from a list of generated moves.
 Move MovePicker::next_move() {
 
-    constexpr int goodQuietThreshold = -14000;
+    constexpr int goodQuietThreshold = -14010;
 top:
     switch (stage)
     {
