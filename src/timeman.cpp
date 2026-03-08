5e871a29c5b464262fb35bbc41f1995afc4b92eb
MARLIN-Tools <jubilantjanitorthrowaway@gmail.com>
2026-03-08 04:24:06 -0500

    timeman: ensure minimum 1ms optimum and max>=optimum

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -129,9 +129,10 @@ void TimeManagement::init(Search::LimitsType& limits,
     }
 
     // Limit the maximum possible time for this move
-    optimumTime = TimePoint(optScale * timeLeft);
-    maximumTime =
-      TimePoint(std::min(0.825179 * limits.time[us] - moveOverhead, maxScale * optimumTime));
+    optimumTime = TimePoint(std::max(1.0, optScale * timeLeft));
+    maximumTime = TimePoint(std::max(
+      double(optimumTime),
+      std::min(0.825179 * limits.time[us] - moveOverhead, maxScale * optimumTime)));
 
     if (options["Ponder"])
         optimumTime += optimumTime / 4;
