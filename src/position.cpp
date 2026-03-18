f5cf458072033931ed6a0650e053b74d4bf5a817
mstembera <5421953+mstembera@users.noreply.github.com>
2026-03-18 14:46:21 -0700

    Simplify HalfKAv2_hm::write_indices() No functional change bench: 2926703

--- src/position.cpp
+++ src/position.cpp
@@ -1185,11 +1185,6 @@ void write_multiple_dirties(const Position& p,
     static_assert(sizeof(DirtyThreat) == 4);
 
     const __m512i board      = _mm512_loadu_si512(p.piece_array().data());
-    const __m512i AllSquares = _mm512_set_epi8(
-      63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41,
-      40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18,
-      17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
-
     const int dt_count = popcount(mask);
     assert(dt_count <= 16);
 
