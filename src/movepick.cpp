cc4637b52b23ecd26b653de254bbd55b3ade2d3c
FauziAkram <fauzi.dabat@hotmail.com>
2026-04-19 07:17:57 +0200

    Remove Redundant Cast in MovePicker
    closes https://github.com/official-stockfish/Stockfish/pull/6735
    
    No functional change

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -167,7 +167,7 @@ ExtMove* MovePicker::score(const MoveList<Type>& ml) {
             m.value += (*continuationHistory[5])[pc][to];
 
             // bonus for checks
-            m.value += (bool(pos.check_squares(pt) & to) && pos.see_ge(m, -75)) * 16384;
+            m.value += ((pos.check_squares(pt) & to) && pos.see_ge(m, -75)) * 16384;
 
             // penalty for moving to a square threatened by a lesser piece
             // or bonus for escaping an attack by a lesser piece.
