d5b5176cd3a9e59567ae70db685cb9a10d613e87
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2026-06-19 10:55:18 +0800

    net with bt4 data
    Bench: 2709045

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,7 +33,7 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultName "nn-71d6d32cb962.nnue"
+#define EvalFileDefaultName "nn-89224b88daf1.nnue"
 
 namespace NNUE {
 class Network;
