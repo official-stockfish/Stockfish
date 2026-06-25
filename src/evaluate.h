12eb6e6cfb517b839649169ae968fe898c7827a5
Shawn Xu <xu107288696@gmail.com>
2026-06-25 16:40:43 -0700

    Update main network to nn-1b6a82263149.nnue
    Bench: 2703604

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,7 +33,7 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultName "nn-f8a759c05f9f.nnue"
+#define EvalFileDefaultName "nn-1b6a82263149.nnue"
 
 namespace NNUE {
 class Network;
