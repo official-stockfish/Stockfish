4488343cf67b39b68c8dc41c91f26796b98df673
Shawn Xu <xu107288696@gmail.com>
2026-06-29 06:32:19 +0200

    Update main network to nn-af1339a6dea3.nnue
    This network is trained by adding the following binpacks, relabeled by @vondele and totaling 74B positions, to the distillation fine-tuning stage:
    ```
      - vondele/from_kaggle_1_relabel/leela96-filt-v2.min.split_0.relabel-BT4-tf13tune.binpack
      - vondele/from_kaggle_1_relabel/leela96-filt-v2.min.split_1.relabel-BT4-tf13tune.binpack
      - vondele/from_kaggle_1_relabel/leela96-filt-v2.min.split_2.relabel-BT4-tf13tune.binpack
      - vondele/from_kaggle_1_relabel/leela96-filt-v2.min.split_3.relabel-BT4-tf13tune.binpack
      - vondele/from_kaggle_1_relabel/leela96-filt-v2.min.split_4.relabel-BT4-tf13tune.binpack
      - vondele/from_kaggle_2_relabel/T60T70wIsRightFarseerT60T74T75T76.split_0.relabel-BT4-tf13tune.binpack
      - vondele/from_kaggle_2_relabel/T60T70wIsRightFarseerT60T74T75T76.split_1.relabel-BT4-tf13tune.binpack
      - vondele/from_kaggle_2_relabel/T60T70wIsRightFarseerT60T74T75T76.split_2.relabel-BT4-tf13tune.binpack
      - vondele/from_kaggle_2_relabel/T60T70wIsRightFarseerT60T74T75T76.split_3.relabel-BT4-tf13tune.binpack
      - vondele/from_kaggle_2_relabel/T60T70wIsRightFarseerT60T74T75T76.split_4.relabel-BT4-tf13tune.binpack
    ```
    
    The relabeling effort has been completed during the testing of this patch, and a full training run is on the way. Thanks to @vondele, @anematode, @Disservin, @yl25946, and all who've contributed to the process.
    
    Passed STC:
    LLR: 2.95 (-2.94,2.94) <0.00,2.00>
    Total: 24512 W: 6500 L: 6201 D: 11811
    Ptnml(0-2): 69, 2772, 6300, 3021, 94
    https://tests.stockfishchess.org/tests/view/6a40b7083036e45021aebbd6
    
    Passed LTC:
    LLR: 2.94 (-2.94,2.94) <0.50,2.50>
    Total: 15708 W: 4232 L: 3960 D: 7516
    Ptnml(0-2): 8, 1572, 4420, 1848, 6
    https://tests.stockfishchess.org/tests/view/6a413d293036e45021aebc84
    
    nettest PR: https://github.com/vondele/nettest/pull/388
    
    closes https://github.com/official-stockfish/Stockfish/pull/6932
    
    Bench: 2102535
    
    Co-authored-by: Joost VandeVondele <Joost.VandeVondele@gmail.com>

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,7 +33,7 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultName "nn-1b6a82263149.nnue"
+#define EvalFileDefaultName "nn-af1339a6dea3.nnue"
 
 namespace NNUE {
 class Network;
