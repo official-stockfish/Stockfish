054f6196b22840c842c5ba63849bea8b67b59417
Dubslow <bunslow@gmail.com>
2026-07-01 15:08:39 -0500

    Rename RootMove inexact flags to match search `Bound` terminology
    Namely, in search we use `enum Bound`, and every score is by defintion a bound,
    it just may be a one-sided bound or two-sided bound. So the RootMove flags track
    inexactness, not the kind of bounds. Rename them accordingly
    
    Also constexpr and an assert
    
    no functional change

--- src/thread.cpp
+++ src/thread.cpp
@@ -372,10 +372,10 @@ Thread* ThreadPool::get_best_thread() const {
         // Aborted (d1) searches may lead to inexact win (or loss) scores.
         const bool bestThreadDecisive = bestThreadMove.score != -VALUE_INFINITE
                                      && is_decisive(bestThreadMove.score)
-                                     && !bestThreadMove.score_is_bound();
+                                     && !bestThreadMove.is_inexact();
         const bool newThreadDecisive = newThreadMove.score != -VALUE_INFINITE
                                     && is_decisive(newThreadMove.score)
-                                    && !newThreadMove.score_is_bound();
+                                    && !newThreadMove.is_inexact();
 
         if (bestThreadDecisive)
         {
