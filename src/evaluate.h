b9fa08ec02d001beafc9187db22aa4e7f55f2f80
tony <35334345+TonyCongqianWang@users.noreply.github.com>
2026-07-12 00:46:02 +0200

    Using net with mate optimized training
    Bench 2786872

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,7 +33,7 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultName "nn-0ee0657fb25e.nnue"
+#define EvalFileDefaultName "nn-45ab3bb38b12.nnue"
 
 namespace NNUE {
 class Network;
