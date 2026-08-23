7a410ab69ab2d0bb702f32a545df76c1d50cb880
Suleiman Sheikh <syzygy930@users.noreply.github.com>
2026-08-22 20:38:29 -0400

    Reduce ProbCut depth near balanced scores
    Use a shallower ProbCut verification search when beta is within 1000 centipawns of a balanced score.
    
    Bench: 2415259

--- src/search.cpp
+++ src/search.cpp
@@ -1074,7 +1074,7 @@ Value Search::Worker::search(
         assert(probCutBeta < VALUE_INFINITE && probCutBeta > beta);
 
         MovePicker mp(pos, ttData.move, probCutBeta - ss->staticEval, &captureHistory);
-        Depth      probCutDepth = depth - (improving ? 5 : 3);
+        Depth      probCutDepth = depth - (improving ? 5 : 3) - (std::abs(beta) < 1000);
 
         while ((move = mp.next_move()) != Move::none())
         {
