2c88244c9f1aa5da2d8e5142b281e09a9c1b6fea
Disservin <disservin.social@gmail.com>
2026-09-05 18:54:31 +0200

    Restore development

--- src/misc.cpp
+++ src/misc.cpp
@@ -54,7 +54,7 @@ namespace fs = std::filesystem;
 namespace {
 
 // Version number or dev.
-constexpr std::string_view version = "19";
+constexpr std::string_view version = "dev";
 
 // Our fancy logging facility. The trick here is to replace cin.rdbuf() and
 // cout.rdbuf() with two Tie objects that tie cin and cout to a file stream. We
